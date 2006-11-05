5b7d2ba0f804b059b493c1be7fe29c7d629182f5
j <j@127d096d-3646-0410-84c9-11b0184650a1>
2006-11-05 18:22:01 +0000

    Included the axes2pix function

--- utils/aux_funs.m
+++ utils/aux_funs.m
@@ -17,6 +17,8 @@ switch opt
         [varargout{1}] = strip_bg_color(varargin{:});
     case 'adjust_lims'
         [varargout{1} varargout{2}] = adjust_lims(varargin{:});
+    case 'axes2pix'
+        varargout{1} = axes2pix(varargin{:});
     case 'cleanGRDappdata'
         clean_GRDappdata(varargin{:})
     case 'min_max_single'
@@ -194,3 +196,24 @@ try
     rmappdata(handles.figure1,'dem_x');     rmappdata(handles.figure1,'dem_y');     rmappdata(handles.figure1,'dem_z');
     rmappdata(handles.figure1,'GMThead');   rmappdata(handles.figure1,'Zmin_max');
 end
+
+%-----------------------------------------------------------------------------------------
+function pixelx = axes2pix(dim, x, axesx)
+%AXES2PIX Convert axes coordinates to pixel coordinates.
+%   PIXELX = AXES2PIX(DIM, X, AXESX) converts axes coordinates
+%   (as returned by get(gca, 'CurrentPoint'), for example) into
+%   pixel coordinates.  X should be the vector returned by
+%   X = get(image_handle, 'XData') (or 'YData').  DIM is the
+%   number of image columns for the x coordinate, or the number
+%   of image rows for the y coordinate.
+
+%   Copyright 1993-2002 The MathWorks, Inc.  
+%   $Revision: 5.12 $  $Date: 2002/03/15 15:57:01 $
+
+if (max(size(dim)) ~= 1);   error('First argument must be a scalar.');  end
+if (min(size(x)) > 1);      error('X must be a vector.');               end
+xfirst = x(1);      xlast = x(max(size(x)));
+if (dim == 1);      pixelx = axesx - xfirst + 1;    return;     end
+xslope = (dim - 1) / (xlast - xfirst);
+if ((xslope == 1) && (xfirst == 1));     pixelx = axesx;
+else    pixelx = xslope * (axesx - xfirst) + 1;         end
