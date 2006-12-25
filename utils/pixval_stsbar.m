6e3fab8b0477ee9423003d650f4799cd6e004d17
j <j@127d096d-3646-0410-84c9-11b0184650a1>
2006-12-25 02:32:59 +0000

    Logical representations of grids don't report idiot base ten Zs

--- utils/pixval_stsbar.m
+++ utils/pixval_stsbar.m
@@ -266,7 +266,7 @@ case 'normal'   % Just display Z (or intensity) information
     else      % intensity
         if isa(img, 'uint8'),         pixval_str = sprintf([form_xy ' %3d'],x,y,pixel(1));
         elseif isa(img, 'uint16'),    pixval_str = sprintf([form_xy ' %5d'],x,y,pixel(1));
-        elseif islogical(img),        pixval_str = sprintf([form_xy ' %1d'],x,y,pixel(1));
+        elseif islogical(img),        pixval_str = sprintf([form_xy ' %g'],x,y,pixel(1));
         else                          pixval_str = sprintf([form_xy ' %6.4f'],x,y,pixel(1));
         end
     end
