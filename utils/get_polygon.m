e8d1179eb5ed6eba6489f0b3c74add4a6f783f35
j <j@127d096d-3646-0410-84c9-11b0184650a1>
2007-01-26 02:02:34 +0000

    Replaced calls to uisuspend_j to uisuspend_fig, and the same fore restore

--- utils/get_polygon.m
+++ utils/get_polygon.m
@@ -25,7 +25,7 @@ ud.GETLINE_FIG = varargin{1};       ud.GETLINE_AX = get(ud.GETLINE_FIG,'CurrentA
     
 % Remember initial figure state
 old_db = get(ud.GETLINE_FIG, 'DoubleBuffer');
-state= uisuspend_j(ud.GETLINE_FIG);
+state= uisuspend_fig(ud.GETLINE_FIG);
 
 % Set up initial callbacks for initial stage
 set(ud.GETLINE_FIG, 'Pointer', 'crosshair', 'WindowButtonDownFcn', {@FirstButtonDown,ud.GETLINE_FIG}, ...
@@ -73,7 +73,7 @@ end
 
 % Restore the figure's initial state
 if (ishandle(ud.GETLINE_FIG))
-   uirestore_j(state);
+   uirestore_fig(state);
    set(ud.GETLINE_FIG, 'DoubleBuffer', old_db);
    try, rmappdata(ud.GETLINE_FIG,'FromGetPolygon');     end
 end
