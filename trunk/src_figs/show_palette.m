25301ac6f60575cd8e2ddd8e8c50c6d05acc7a4b
j <j@127d096d-3646-0410-84c9-11b0184650a1>
2007-08-27 01:24:54 +0000

    Completely rewritten. Now also adds a colorbar on the Mirone figure

--- trunk/src_figs/show_palette.m
+++ trunk/src_figs/show_palette.m
@@ -1,132 +1,123 @@
 function varargout = show_palette(varargin)
-% M-File changed by desGUIDE  
+	%	Copyright (c) 2004-2006 by J. Luis
+	%
+	%	This program is free software; you can redistribute it and/or modify
+	%	it under the terms of the GNU General Public License as published by
+	%	the Free Software Foundation; version 2 of the License.
+	%
+	%	This program is distributed in the hope that it will be useful,
+	%	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	%	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	%	GNU General Public License for more details.
+	%
+	%	Contact info: w3.ualg.pt/~jluis/mirone
+	% --------------------------------------------------------------------
+  
+    handMir = varargin{1};
+    tipo = varargin{2};
+    
+    % Some tests
+	if (handMir.no_file),     return;      end
+	if (ndims(get(handMir.hImg,'CData')) == 3)
+        msgbox('True color images do not use color palettes. Bye','Warning');    return
+	end
+    
+    cmap = get(handMir.figure1,'Colormap');
+	axUnits = get(handMir.axes1, 'Units');			set(handMir.axes1, 'Units', 'pixels');
+	posAxParent = get(handMir.axes1,'Pos');
+	posFigParent = get(handMir.figure1,'Pos');
+	screen = get(0,'ScreenSize');           % In some cases we need this
+    
+    if (strcmp(tipo,'Float'))            % Create a new Figure with the colorbar
+        
+        figPos = [100 100 100 min(420,posAxParent(4)+10)];
+		hObject = figure('Units','Pixels',...
+			'Position',figPos, 'PaperUnits','centimeters',...
+			'Color',get(0,'factoryUicontrolBackgroundColor'),...
+			'KeyPressFcn',@figure1_KeyPressFcn,...
+			'MenuBar','none','Resize','off',...
+			'Name','','NumberTitle','off',...
+            'Colormap',cmap,'Visible','off');
+    
+		% Try to position this figure glued to the right of calling figure
+		ecran = get(0,'ScreenSize');
+		xLL = posFigParent(1) + posFigParent(3) + 6;
+		xLR = xLL + figPos(3);
+		if (xLR > ecran(3))         % If figure is partially out, bring totally into screen
+			xLL = ecran(3) - figPos(3);
+		end
+		yLL = (posFigParent(2) + posFigParent(4)/2+12) - figPos(4) / 2;
+		set(hObject,'Pos',[xLL yLL figPos(3:4)])
 
-%	Copyright (c) 2004-2006 by J. Luis
-%
-%	This program is free software; you can redistribute it and/or modify
-%	it under the terms of the GNU General Public License as published by
-%	the Free Software Foundation; version 2 of the License.
-%
-%	This program is distributed in the hope that it will be useful,
-%	but WITHOUT ANY WARRANTY; without even the implied warranty of
-%	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-%	GNU General Public License for more details.
-%
-%	Contact info: w3.ualg.pt/~jluis/mirone
-% --------------------------------------------------------------------
- 
-hObject = figure('Tag','figure1','Visible','off');
-handles = guihandles(hObject);
-guidata(hObject, handles);
-show_palette_LayoutFcn(hObject,handles);
-handles = guihandles(hObject);
- 
-global home_dir
-if (isempty(home_dir))        % Case when this function was called directly
-    handles.home_dir = pwd;
-else
-    handles.home_dir = home_dir;
-end
+		% Create the color bar axes and image object
+        axPos = [10 5 40 figPos(4)-10];
+		hAx = axes('Units','Pixels','pos',axPos,'Parent',hObject);
+		image([1 10],handMir.head(5:6),[1:size(cmap,1)]');
+		set(hAx,'XTick',[],'YAxisLocation','right','YDir','normal')
+		set(hObject,'Visible','on');
+		if (nargout),   varargout{1} = hObject;     end
 
-h_m_gmt = findobj('Name','m_gmt');
-if ishandle(h_m_gmt)
-    handles.work_dir = getappdata(h_m_gmt,'current_dir');
-    handles.last_dir = handles.work_dir;
-else
-    handles.work_dir = pwd;
-    handles.last_dir = pwd;
-end
+    elseif (strcmp(tipo,'At'))              % Create a colorbar right next to image's right side
 
-movegui(hObject,'northeast');     % Reposition the window on screen
-cmaplim = varargin{1};      datalim = varargin{2};
-datainc = varargin{3};      cmap = varargin{4};
+		hUict = handMir.PalAt;
+		% If we already have a colorbar, remove it
+		if (strcmp(get(hUict,'Check'),'on'))
+			ud = get(hUict,'Userdata');
+			posFigParent(3) = posAxParent(1) + posAxParent(3) + 9;  % 9 is for the slider + 2 
+			if (posFigParent(3) == screen(3)),      posFigParent(3) = posFigParent(3)-1;    end     % The elastic
+			set(handMir.figure1,'pos',posFigParent)
+			delete(ud)
+			set(hUict,'Checked','off')
+			return
+		end
 
-set(hObject,'Name','');     set(handles.figure1,'Colormap',cmap)
-% Determine tick labels
-tickvalues = datainc * [ceil(datalim(1)/datainc) : floor(datalim(2)/datainc)];
-for i = 1 : length(tickvalues)
-  ticklabel{i} = num2str(tickvalues(i));
-end
+		% Create the color bar axes at the right side of the main image axes
+        barW = 20;      % Colorbar width
+        marg = 8;       % Margin between Image and colorbar (must be enoug for slider width)
+        axPos = [posAxParent(1)+posAxParent(3)+marg posAxParent(2) barW min(420,posAxParent(4))];
+		hAx = axes('Units','Pixels','pos',axPos,'Parent',handMir.figure1,'vis','off');
+		image([1 10],handMir.head(5:6),[1:size(cmap,1)]');
+		set(hAx,'XTick',[],'YAxisLocation','right','YDir','normal','HandleVisibility','off')
 
-% Determine tick positions and image array
-if cmaplim(1) < cmaplim(2)
-    tick = cmaplim(1) + (tickvalues - datalim(1)) * diff(cmaplim) / diff(datalim);
-    I = cmaplim(1):cmaplim(2);    ilim = cmaplim;
-else
-    tick = cmaplim(2) - (tickvalues - datalim(1)) * diff(cmaplim) / diff(datalim);
-    I = cmaplim(1):-1:cmaplim(2);    ilim = [cmaplim(2) cmaplim(1)];
-end
+		h_Ylabel = get(hAx,'Ylabel');	set(h_Ylabel,'units','pixels')
+		Ylabel_pos = get(h_Ylabel,'Extent');
 
-% Create the color bar axes and image object
-h = axes('Units','Pixels','Visible','off');
-image(I','CDataMapping','direct','YData',ilim);
-set(h,'YTick',tick,'XTick',[],'YTickLabel',ticklabel);
+        gutterRight = posFigParent(3) - (posAxParent(1) + posAxParent(3));
+		if (gutterRight < Ylabel_pos(1)+marg)           % Grow Figure's width to acomodate the colorbar
+			posFigParent(3) = posFigParent(3) + (Ylabel_pos(1)+marg - gutterRight - 3); % -3 is empiric
+			if (posFigParent(3) == screen(3)),      posFigParent(3) = posFigParent(3)-1;    end     % The elastic
+			set(handMir.figure1, 'Position', posFigParent);
+		end
+		set(hUict,'Userdata',hAx)        % Save it so that we can restore upon colorbar deletion
+		set(hAx,'Units','normalized','Vis','on')
+		set(hUict,'Checked','on')
 
-pos = get(h,'Position');
-% Set axes additional properties
-set(h,'YDir','normal'); % Override the default YDir set by IMAGE
-set(h,'Position', pos+[55 -30 -45 +40]); % Separate in case we have {...,'YDir','reverse',...}
-set(h,'Visible','On');  % Always make it visible
+    elseif (strcmp(tipo,'In'))                  % Create a colorbar inside the image, at it's right side
 
-set(hObject,'Visible','on');
+		hUict = handMir.PalIn;
+		% If we already have a colorbar, remove it
+		if (strcmp(get(hUict,'Check'),'on'))
+			ud = get(hUict,'Userdata');
+			delete(ud)
+			set(hUict,'Checked','off')
+			return
+		end
 
-% Choose default command line output for show_palette_export
-handles.output = hObject;
-guidata(hObject, handles);
+		% Create the color bar axes at the right (in)side of the main image axes
+        barW = 20;      % Colorbar width
+        axPos = [posAxParent(1)+posAxParent(3)-barW posAxParent(2) barW posAxParent(4)];
+		hAx = axes('Units','Pixels','pos',axPos,'Parent',handMir.figure1,'Vis','off');
+		image([1 10],handMir.head(5:6),[1:size(cmap,1)]');
+		set(hAx,'XTick',[],'YDir','normal','HandleVisibility','off','Units','normalized','Vis','on')
+		set(hUict,'Userdata',hAx)            % Save it so that we can restore upon colorbar deletion
+		set(hUict,'Checked','on')
 
-% UIWAIT makes shading_params wait for user response (see UIRESUME)
-% uiwait(handles.figure1);
-
-% NOTE: If you make uiwait active you have also to uncomment the next three lines
-% handles = guidata(hObject);
-% out = show_palette_OutputFcn(hObject, [], handles);
-% varargout{1} = out;
-
-% --- Outputs from this function are returned to the command line.
-function varargout = show_palette_OutputFcn(hObject, eventdata, handles)
-varargout{1} = handles.output;
-
-% --- Executes when user attempts to close figure1.
-function figure1_CloseRequestFcn(hObject, eventdata, handles)
-delete(hObject);
+    end
+    
+	set(handMir.axes1, 'Units', axUnits);   % Do it here because the "At side" case needs it in Pixels
 
 % --- Executes on key press over figure1 with no controls selected.
-function figure1_KeyPressFcn(hObject, eventdata, handles)
-if isequal(get(hObject,'CurrentKey'),'escape')      % Check for "escape"
-    delete(hObject);
-end
-
-
-% --- Creates and returns a handle to the GUI figure. 
-function show_palette_LayoutFcn(h1,handles);
-
-set(h1,...
-'PaperUnits','centimeters',...
-'CloseRequestFcn',{@figure1_CloseRequestFcn,handles},...
-'Color',get(0,'factoryUicontrolBackgroundColor'),...
-'KeyPressFcn',{@figure1_KeyPressFcn,handles},...
-'MenuBar','none',...
-'Name','show_palette',...
-'NumberTitle','off',...
-'Position',[100 100 106 420],...
-'Resize','off',...
-'Tag','figure1',...
-'UserData',[]);
-
-setappdata(h1, 'GUIDEOptions',struct(...
-'active_h', 102.003295898438, ...
-'taginfo', struct(...
-'figure', 2), ...
-'override', 0, ...
-'release', 13, ...
-'resize', 'none', ...
-'accessibility', 'callback', ...
-'mfile', 1, ...
-'callbacks', 1, ...
-'singleton', 1, ...
-'syscolorfig', 1, ...
-'lastSavedFile', 'D:\m_gmt\show_palette.m'));
-
-function show_palette_uicallback(hObject, eventdata, h1, callback_name)
-% This function is executed by the callback and than the handles is allways updated.
-feval(callback_name,hObject,[],guidata(h1));
+function figure1_KeyPressFcn(hObject, eventdata)
+	if isequal(get(hObject,'CurrentKey'),'escape')      % Check for "escape"
+        delete(hObject);
+	end
