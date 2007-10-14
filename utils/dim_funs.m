72e7e9b251259ea396337437b04fe7ff01e0e634
j <j@127d096d-3646-0410-84c9-11b0184650a1>
2007-10-14 19:33:35 +0000

    Compute format for printing limits/inc such as number of decimals >= 8

--- utils/dim_funs.m
+++ utils/dim_funs.m
@@ -1,6 +1,11 @@
 function  dim_funs(opt, varargin)
     % Functions to deal with -R -I (Ivan the Terrible) stuff of Windows that need it
     % There are outputs. All changes are saved by guidata.
+	
+	% handles must have these fields
+	% handles.x_min  handles.x_max handles.y_min  handles.y_max
+	% handles.x_min_or handles.x_max_or handles.y_min_or  handles.y_max_or
+	% handles.dms_xinc handles.dms_yinc	handles.one_or_zero
 
 switch opt
     case 'xMin'
@@ -26,13 +31,16 @@ function edit_x_min(hObject, handles)
 	x_min_or = handles.x_min_or;
 	xx = get(hObject,'String');     val = test_dms(xx);
 	if ~isempty(val)            % when dd:mm or dd:mm:ss was given
+		% Calculate printing format
+		nDigit = round( log10(abs(x_min_or)) );		% Number of digits of the integer part
+		frmt = sprintf('%%.%dg',nDigit+8);			% it will be of the type '%.Ng'
         x_min = 0;
         if str2double(val{1}) > 0
             for i = 1:numel(val)   x_min = x_min + str2double(val{i}) / (60^(i-1));    end
         else
             for i = 1:numel(val)   x_min = x_min - abs(str2double(val{i})) / (60^(i-1));   end
         end
-        if (x_min < x_min_or);  set(hObject,'String',sprintf('%.8g',x_min_or));    return;     end; 
+        if (x_min < x_min_or);  set(hObject,'String',sprintf(frmt,x_min_or));    return;     end; 
         handles.x_min = x_min;
         if ~isempty(handles.x_max) && x_min >= handles.x_max
             errordlg('West Longitude >= East Longitude ','Error in Longitude limits')
@@ -43,11 +51,11 @@ function edit_x_min(hObject, handles)
             % Compute Ncols, but first must recompute x_inc
             x_inc = ivan_the_terrible((handles.x_max - x_min),round(abs(str2double(nc))),1, handles.one_or_zero);
             xx = floor((handles.x_max - str2double(xx)) / (str2double(get(handles.edit_x_inc,'String')))+0.5) + handles.one_or_zero;
-            set(handles.edit_x_inc,'String',sprintf('%.12g',x_inc))
+            set(handles.edit_x_inc,'String',sprintf(frmt,x_inc))
             guidata(hObject, handles);
         elseif ~isempty(handles.x_max)      % x_max box is filled but ncol is not, so put to the default (100)
             x_inc = ivan_the_terrible((handles.x_max - x_min),100,1, handles.one_or_zero);
-            set(handles.edit_x_inc,'String',sprintf('%.12g',x_inc))
+            set(handles.edit_x_inc,'String',sprintf(frmt,x_inc))
             set(handles.edit_Ncols,'String','100')
             guidata(hObject, handles);
         end
@@ -61,13 +69,16 @@ function edit_x_max(hObject, handles)
 	x_max_or = handles.x_max_or;
 	xx = get(hObject,'String');     val = test_dms(xx);
 	if ~isempty(val)
+		% Calculate printing format
+		nDigit = round( log10(abs(x_max_or)) );		% Number of digits of the integer part
+		frmt = sprintf('%%.%dg',nDigit+8);			% it will be of the type '%.Ng'
         x_max = 0;
         if str2double(val{1}) > 0
             for i = 1:numel(val)   x_max = x_max + str2double(val{i}) / (60^(i-1));    end
         else
             for i = 1:numel(val)   x_max = x_max - abs(str2double(val{i})) / (60^(i-1));   end
         end
-        if (x_max > x_max_or);  set(hObject,'String',sprintf('%.8g',x_max_or));    return;     end; 
+        if (x_max > x_max_or);  set(hObject,'String',sprintf(frmt,x_max_or));    return;     end; 
         handles.x_max = x_max;
         if ~isempty(handles.x_min) && x_max <= handles.x_min 
             errordlg('East Longitude <= West Longitude','Error in Longitude limits')
@@ -78,11 +89,11 @@ function edit_x_max(hObject, handles)
             % Compute Ncols, but first must recompute x_inc
             x_inc = ivan_the_terrible((x_max - handles.x_min),round(abs(str2double(nc))),1, handles.one_or_zero);
             xx = floor((handles.x_min - str2double(xx)) / (str2double(get(handles.edit_x_inc,'String')))+0.5) + handles.one_or_zero;
-            set(handles.edit_x_inc,'String',sprintf('%.12g',x_inc))
+            set(handles.edit_x_inc,'String',sprintf(frmt,x_inc))
             guidata(hObject, handles);    
         elseif ~isempty(handles.x_min)      % x_min box is filled but ncol is not, so put to the default (100)
             x_inc = ivan_the_terrible((x_max - handles.x_min),100,1, handles.one_or_zero);
-            set(handles.edit_x_inc,'String',sprintf('%.12g',x_inc))
+            set(handles.edit_x_inc,'String',sprintf(frmt,x_inc))
             set(handles.edit_Ncols,'String','100')
             guidata(hObject, handles);
         end
@@ -97,13 +108,16 @@ function edit_y_min(hObject, handles)
 	y_min_or = handles.y_min_or;
 	xx = get(hObject,'String');     val = test_dms(xx);
 	if ~isempty(val)
+		% Calculate printing format
+		nDigit = round( log10(abs(y_min_or)) );		% Number of digits of the integer part
+		frmt = sprintf('%%.%dg',nDigit+8);			% it will be of the type '%.Ng'
         y_min = 0;
         if str2double(val{1}) > 0
             for i = 1:numel(val)   y_min = y_min + str2double(val{i}) / (60^(i-1));    end
         else
             for i = 1:numel(val)   y_min = y_min - abs(str2double(val{i})) / (60^(i-1));   end
         end
-        if (y_min < y_min_or);  set(hObject,'String',sprintf('%.8g',y_min_or));    return;     end; 
+        if (y_min < y_min_or);  set(hObject,'String',sprintf(frmt,y_min_or));    return;     end; 
         handles.y_min = y_min;
         if ~isempty(handles.y_max) && y_min >= handles.y_max
             errordlg('South Latitude >= North Latitude','Error in Latitude limits')
@@ -114,11 +128,11 @@ function edit_y_min(hObject, handles)
             % Compute Nrows, but first must recompute y_inc
             y_inc = ivan_the_terrible((handles.y_max - y_min),round(abs(str2double(nr))),1, handles.one_or_zero);
             xx = floor((handles.y_max - str2double(xx)) / (str2double(get(handles.edit_y_inc,'String')))+0.5) + handles.one_or_zero;
-            set(handles.edit_y_inc,'String',sprintf('%.12g',y_inc))
+            set(handles.edit_y_inc,'String',sprintf(frmt,y_inc))
             guidata(hObject, handles);
         elseif ~isempty(handles.y_max)      % y_max box is filled but nrows is not, so put to the default (100)
             y_inc = ivan_the_terrible((handles.y_max - y_min),100,1, handles.one_or_zero);
-            set(handles.edit_y_inc,'String',sprintf('%.12g',y_inc))
+            set(handles.edit_y_inc,'String',sprintf(frmt,y_inc))
             set(handles.edit_Nrows,'String','100')
             guidata(hObject, handles);
         end
@@ -132,13 +146,16 @@ function edit_y_max(hObject, handles)
 	y_max_or = handles.y_max_or;
 	xx = get(hObject,'String');     val = test_dms(xx);
 	if ~isempty(val)
+		% Calculate printing format
+		nDigit = round( log10(abs(y_max_or)) );		% Number of digits of the integer part
+		frmt = sprintf('%%.%dg',nDigit+8);			% it will be of the type '%.Ng'
         y_max = 0;
         if str2double(val{1}) > 0
             for i = 1:numel(val)   y_max = y_max + str2double(val{i}) / (60^(i-1));    end
         else
             for i = 1:numel(val)   y_max = y_max - abs(str2double(val{i})) / (60^(i-1));   end
         end
-        if (y_max > y_max_or);  set(hObject,'String',sprintf('%.8g',y_max_or));    return;     end; 
+        if (y_max > y_max_or);  set(hObject,'String',sprintf(frmt,y_max_or));    return;     end; 
         handles.y_max = y_max;
         if ~isempty(handles.y_min) && y_max <= handles.y_min 
             errordlg('North Latitude <= South Latitude','Error in Latitude limits')
@@ -149,11 +166,11 @@ function edit_y_max(hObject, handles)
             % Compute Nrows, but first must recompute y_inc
             y_inc = ivan_the_terrible((y_max - handles.y_min),round(abs(str2double(nr))),1, handles.one_or_zero);
             xx = floor((handles.y_min - str2double(xx)) / (str2double(get(handles.edit_y_inc,'String')))+0.5) + handles.one_or_zero;
-            set(handles.edit_y_inc,'String',sprintf('%.12g',y_inc))
+            set(handles.edit_y_inc,'String',sprintf(frmt,y_inc))
             guidata(hObject, handles);
         elseif ~isempty(handles.y_min)      % y_min box is filled but nrows is not, so put to the default (100)
             y_inc = ivan_the_terrible((y_max - handles.y_min),100,1, handles.one_or_zero);
-            set(handles.edit_y_inc,'String',sprintf('%.12g',y_inc))
+            set(handles.edit_y_inc,'String',sprintf(frmt,y_inc))
             set(handles.edit_Nrows,'String','100')
             guidata(hObject, handles);
         end
@@ -176,7 +193,10 @@ function edit_x_inc(hObject, handles)
         % Make whatever x_inc given compatible with GMT_grd_RI_verify
         x_inc = ivan_the_terrible((handles.x_max - handles.x_min), x_inc,2, handles.one_or_zero);
         if ~dms         % case of decimal unities
-            set(hObject,'String',sprintf('%.12g',x_inc))
+			% Calculate printing format
+			nDigit = round( log10(abs(x_inc)) );		% Number of digits of the integer part
+			frmt = sprintf('%%.%dg',nDigit+10);			% it will be of the type '%.Ng'
+            set(hObject,'String',sprintf(frmt,x_inc))
         else            % inc was in dd:mm or dd:mm:ss format
             ddmm = dec2deg(x_inc);
             set(hObject,'String',ddmm)
@@ -198,7 +218,10 @@ function edit_Ncols(hObject, handles)
             ddmm = dec2deg(x_inc);
             set(handles.edit_x_inc,'String',ddmm)
         else                    % x_inc was given in decimal format
-            set(handles.edit_x_inc,'String',sprintf('%.12g',x_inc));
+			% Calculate printing format
+			nDigit = round( log10(abs(x_inc)) );		% Number of digits of the integer part
+			frmt = sprintf('%%.%dg',nDigit+10);			% it will be of the type '%.Ng'
+            set(handles.edit_x_inc,'String',sprintf(frmt,x_inc));
         end
         guidata(hObject, handles);
 	end
@@ -217,7 +240,10 @@ function edit_y_inc(hObject, handles)
         % Make whatever y_inc given compatible with GMT_grd_RI_verify
         y_inc = ivan_the_terrible((handles.y_max - handles.y_min), y_inc, 2, handles.one_or_zero);
         if ~dms         % case of decimal unities
-            set(hObject,'String',sprintf('%.12g',y_inc))
+			% Calculate printing format
+			nDigit = round( log10(abs(y_inc)) );		% Number of digits of the integer part
+			frmt = sprintf('%%.%dg',nDigit+10);			% it will be of the type '%.Ng'
+            set(hObject,'String',sprintf(frmt,y_inc))
         else            % inc was in dd:mm or dd:mm:ss format
             ddmm = dec2deg(y_inc);
             set(hObject,'String',ddmm)
@@ -238,7 +264,10 @@ function edit_Nrows(hObject, handles)
             ddmm = dec2deg(y_inc);
             set(handles.edit_y_inc,'String',ddmm)
         else                    % y_inc was given in decimal format
-            set(handles.edit_y_inc,'String',sprintf('%.12g',y_inc));
+			% Calculate printing format
+			nDigit = round( log10(abs(y_inc)) );		% Number of digits of the integer part
+			frmt = sprintf('%%.%dg',nDigit+10);			% it will be of the type '%.Ng'
+            set(handles.edit_y_inc,'String',sprintf(frmt,y_inc));
         end
         guidata(hObject, handles);
 	end
