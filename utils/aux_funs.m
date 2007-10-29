b238f334c974fd7d9fd0b22027fa8e5b46d09e41
j <j@localhost>
2007-10-29 02:47:51 +0000

    New fun togCheck. switch test is done over opt(1:4). msg_dlg no more movegui(h,'center'), removed test for nargin==1, simplified case 14 to ~validGrid, removed case 2 (no use anymore). case 1 now tests if geog findFileType() returns 'dono' when extension doesn't belong to the known list Moved decodeProjectionRef() to here from grid_info()

--- utils/aux_funs.m
+++ utils/aux_funs.m
@@ -4,55 +4,59 @@ function  varargout = aux_funs(opt,varargin)
 % of the Mirone's callback functions. I puted them here to release somehow
 % the burden of the non-stop groing length of the Mirone code.
 
-switch opt
-    case 'StoreZ'
-        StoreZ(varargin{:})
-    case 'msg_dlg'
-        varargout = msg_dlg(varargin{:});
-    case 'in_map_region'
-        [varargout{1} varargout{2} varargout{3} varargout{4}] = in_map_region(varargin{:});
-    case 'cleanGRDappdata'
-        clean_GRDappdata(varargin{:})
-    case 'guessGeog'
-        varargout{1} = guessGeog(varargin{:});
-    case 'colormap_bg'
-        colormap_bg(varargin{:})
-    case 'findFileType'
-        varargout{1} = findFileType(varargin{:});
-        if (nargout == 2)       % Check if file exists
-            ind = exist(varargin{1},'file');
-            varargout{2} = (ind == 2);
-        end
-    case 'isProj'
-        varargout{1} = isProj(varargin{:});
-    case 'toProjPT'
-        toProjPT(varargin{:});
-    case 'polysplit'
-        [varargout{1} varargout{2}] = localPolysplit(varargin{:});
-    case 'adjust_lims'
-        [varargout{1} varargout{2}] = adjust_lims(varargin{:});
-    case 'axes2pix'
-        varargout{1} = axes2pix(varargin{:});
-    case 'insideRect'
-        varargout{1} = insideRect(varargin{:});
-    case 'strip_bg_color'
-        varargout{1} = strip_bg_color(varargin{:});
-    case 'help'
-        str = [varargin{1}.home_dir filesep 'doc' filesep 'MironeMan.chm'];
-        if (ispc),      dos(str);
-        else            unix(str);
-        end
-    case 'min_max_single'
-        [varargout{1} varargout{2}] = min_max_single(varargin{:});
+switch opt(1:4)
+	case 'Stor'		% 'StoreZ'
+		StoreZ(varargin{:})
+	case 'msg_'		% 'msg_dlg'
+		varargout = msg_dlg(varargin{:});
+	case 'in_m'		% 'in_map_region'
+		[varargout{1} varargout{2} varargout{3} varargout{4}] = in_map_region(varargin{:});
+	case 'clea'		% 'cleanGRDappdata'
+		clean_GRDappdata(varargin{:})
+	case 'gues'		% 'guessGeog'
+		varargout{1} = guessGeog(varargin{:});
+	case 'colo'		% 'colormap_bg'
+		colormap_bg(varargin{:})
+	case 'find'		% 'findFileType'
+		varargout{1} = findFileType(varargin{:});
+		if (nargout == 2)       % Check if file exists
+			ind = exist(varargin{1},'file');
+			varargout{2} = (ind == 2);
+		end
+	case 'appP'		% 'appProjectionRef'
+		appProjectionRef(varargin{:})
+	case 'isPr'		% 'isProj'
+		varargout{1} = isProj(varargin{:});
+	case 'toPr'		% 'toProjPT'
+		toProjPT(varargin{:});
+	case 'togC'		% 'togCheck'			% Toggle the check state of a uimenu
+		togCheck(varargin{:})
+	case 'poly'		% 'polysplit'
+		[varargout{1} varargout{2}] = localPolysplit(varargin{:});
+	case 'adju'		% 'adjust_lims'
+		[varargout{1} varargout{2}] = adjust_lims(varargin{:});
+	case 'axes'		% 'axes2pix'
+		varargout{1} = axes2pix(varargin{:});
+	case 'insi'		% 'insideRect'
+		varargout{1} = insideRect(varargin{:});
+	case 'stri'		% 'strip_bg_color'
+		varargout{1} = strip_bg_color(varargin{:});
+	case 'help'
+		str = [varargin{1}.home_dir filesep 'doc' filesep 'MironeMan.chm'];
+		if (ispc),      dos(str);
+		else            unix(str);
+		end
+	case 'min_'		% 'min_max_single'
+		[varargout{1} varargout{2}] = min_max_single(varargin{:});
 end
 
 % --------------------------------------------------------------------
 function StoreZ(handles,X,Y,Z)
-    % If grid size is not to big I'll store it
-    if (numel(Z)*4 > handles.grdMaxSize),       return;     end
-    if (~isa(Z,'single')),  setappdata(handles.figure1,'dem_z',single(Z));
-    else                    setappdata(handles.figure1,'dem_z',Z);  end
-    setappdata(handles.figure1,'dem_x',X);  setappdata(handles.figure1,'dem_y',Y);
+	% If grid size is not to big I'll store it
+	if (numel(Z)*4 > handles.grdMaxSize),		return,		end
+	if (~isa(Z,'single')),  setappdata(handles.figure1,'dem_z',single(Z));
+	else                    setappdata(handles.figure1,'dem_z',Z);  end
+	setappdata(handles.figure1,'dem_x',X);  setappdata(handles.figure1,'dem_y',Y);
 
 % --------------------------------------------------------------------
 function [x,y,indx,indy] = in_map_region(handles,x,y,tol,map_lims)
@@ -112,46 +116,50 @@ function out = findFileType(fname)
     % From the extension guess what function should be called to open this file
 	out = [];	[PATH,FNAME,EXT] = fileparts(fname);
 	if ( any(strcmpi(EXT,{'.grd' '.nc'})) )
-        out = 'gmt';        return
-	end
-	if ( any(strcmpi(EXT,{'.jpg' '.png' '.bmp' '.gif' '.pcx' '.ras' '.ppm' '.pgm' '.xwd' '.shade' '.raw' '.bin'})) )
-        out = 'generic';    return
-	end
-	if ( any(strcmpi(EXT,{'.tif' '.tiff' '.sid' '.ecw' '.jp2'})) )
-        out = 'geotif';    return
-	end
-	if ( any(strcmpi(EXT,'.mat')) )
+        out = 'gmt';
+	elseif ( any(strcmpi(EXT,{'.jpg' '.png' '.bmp' '.gif' '.pcx' '.ras' '.ppm' '.pgm' '.xwd' '.shade' '.raw' '.bin'})) )
+        out = 'generic';
+	elseif ( any(strcmpi(EXT,{'.tif' '.tiff' '.sid' '.ecw' '.jp2' '.kap' '.nos'})) )
+        out = 'geotif';
+	elseif ( any(strcmpi(EXT,'.mat')) )
         load(fname,'grd_name')
         if (exist(grd_name))    % The mat file is a Session file
             out = 'mat';
         end
-        return
-    end
-	if ( any(strcmpi(EXT,{'.n1' '.n14' '.n15' '.n16' '.n17'})) )
-        out = 'multiband';    return
+	elseif ( any(strcmpi(EXT,{'.n1' '.n14' '.n15' '.n16' '.n17'})) )
+        out = 'multiband';
+	elseif ( any(strcmpi(EXT,'.img')) )
+		out = 'envherd';
+	elseif ( any(strcmpi(EXT,'.cpt')) )
+		out = 'cpt';
+	else
+		% Before we give up with a 'dono' check if its a netCDF file.
+		% One normaly shouldn't need it if the GDAL netCDF driver
+		% (which is where the 'dono's end up) was not so broken.
+		[fid, msg] = fopen(fname, 'r');
+		ID = fread(fid,3,'*char');      ID = ID';      fclose(fid);
+		if (strcmp(ID, 'CDF')),			out = 'gmt';
+		else							out = 'dono';
+		end
 	end
-	if ( any(strcmpi(EXT,'.img')) ),    out = 'envherd';    end
 
 % --------------------------------------------------------------------
 function out = msg_dlg(in,handles)
 % Cast a window with a pre-deffined message
 out = {0};    msg = [];     h = [];
-if (nargin == 1),   handles.no_file = 0;    handles.image_type = 0;    end      % backawrd comp
-if (handles.no_file == 1)
+if (handles.no_file)
     msg = 'You didn''t even load a file. What are you expecting then?';    out = {1};
-    h = msgbox(msg,'Error');      movegui(h,'center');      return;
+    h = msgbox(msg,'Error');      return;
 end
 switch in
-    case 1,     msg = 'C''mon load a file first. It''s logic, isn''t it?';    out = {1};
-    case 2,     msg = 'This operation is deffined only for images derived from DEM grids';  out = {1};
-    case 3
+    case 1
         if (~handles.geog)
             msg = 'This operation is currently possible only for geographic type data';
             out = {1};
         end
     case 14
-        if ~(handles.image_type == 1 || handles.image_type == 4 || handles.computed_grid)
-            msg = 'This operation is deffined only for images derived from DEM grids';
+        if (~handles.validGrid)
+            msg = 'This operation is deffined only for images derived from float (eg DEMs) grids';
             out = {1};
         end
     case 5
@@ -161,7 +169,7 @@ switch in
         end
 end
 if (~isempty(msg))
-    h = msgbox(msg,'Error');      movegui(h,'center')
+    h = msgbox(msg,'Error');
 end
 axes(handles.axes1)     % This is for the GCP mode be able to plot on the Master Image
 if (~isempty(h))        % Bring the figure forward because it was hiden by the previous command
@@ -193,7 +201,7 @@ function handles = isProj(handles, opt)
         handles.is_projected = 0;
         if (ishandle(handles.Projections)),     set(handles.Projections,'Enable','off');   end
     end
-    if (handles.is_projected),      set(handles.hAxMenuLF, 'Vis', 'on', 'Separator','on')
+    if (handles.is_projected),      set(handles.hAxMenuLF, 'Vis', 'on', 'Separator','on')	% To load/not in prj coords
     else                            set(handles.hAxMenuLF, 'Vis', 'off', 'Separator','off')
     end
     
@@ -205,81 +213,102 @@ function handles = isProj(handles, opt)
     if (nargin == 2),       guidata(handles.figure1, handles);      end
 
 % ----------------------------------------------------------------------------------
+function togCheck(varargin)
+	% Toggles the check state of a uimenu. If nargin == 2, second arg is assumed to contain
+	% a handles list of others uimenus belonging to a group. In this case, if one ui is 
+	% checked on, the others will be checked off. The contrary doesn not apply.
+	h = varargin{1};	st = {'on', 'off'};
+	id = 0;
+	if (strcmp(get(h,'check'), 'on')),	id = 1;		end
+	if (nargin == 1)				% Pure toggle on/off
+		set(h,'check', st{id+1})
+	else							% "Radiobutton mode"
+		if (~id)					% IF NOT CHECKED, check arg1 and un-check the others in arg2
+			set(varargin{2}, 'check', 'off')
+			set(h, 'check', 'on')
+		end
+	end
+
+% ----------------------------------------------------------------------------------
 function toProjPT(handles)
     % The following is to deal with eventual coords display in geogs and is used in PIXVAL_STSBAR 
 
-    displayBar = findobj(handles.figure1, 'Tag', 'pixValStsBar');
+	displayBar = findobj(handles.figure1, 'Tag', 'pixValStsBar');
 	dbud = get(displayBar, 'UserData');
-    dbud.toProjPT = 0;
-    if (handles.is_projected)
-        % Fish eventual proj strings
-        projGMT = getappdata(handles.figure1,'ProjGMT');
-        projWKT = getappdata(handles.figure1,'ProjWKT');
-        proj4 = getappdata(handles.figure1,'Proj4');
-        if (~isempty(projWKT) || ~isempty(proj4))
-            % In case we have both 'projWKT' takes precedence because it came from file metadata
-            if (~isempty(projWKT)),     dbud.projStruc.SrcProjWKT = projWKT;
-            else                        dbud.projStruc.SrcProjWKT = ogrproj(proj4);
-            end
-            dbud.toProjPT = 1;
-        elseif (~isempty(projGMT))
-            lims = [getappdata(handles.axes1,'ThisImageLims')];
-            out = mapproject_m([lims(1) lims(3); lims(2) lims(4)],'-R-180/180/0/80','-I','-F',projGMT{:});    % Convert lims back to geogs
-            x_min = min(out(:,1));        x_max = max(out(:,1));
-            y_min = min(out(:,2));        y_max = max(out(:,2));
-      	    dbud.opt_R = ['-R' sprintf('%f',x_min) '/' sprintf('%f',x_max) '/' sprintf('%f',y_min) '/' sprintf('%f',y_max)];
-            dbud.projGMT = projGMT;
-            dbud.toProjPT = 2;
-        end
-    	set(displayBar, 'UserData', dbud);
+	dbud.toProjPT = 0;
+	if (handles.is_projected)
+		% Fish eventual proj strings
+		projGMT = getappdata(handles.figure1,'ProjGMT');
+		projWKT = getappdata(handles.figure1,'ProjWKT');
+		proj4 = getappdata(handles.figure1,'Proj4');
+		if (~isempty(projWKT) || ~isempty(proj4))
+			% In case we have both 'projWKT' takes precedence because it came from file metadata
+			if (~isempty(projWKT)),     dbud.projStruc.SrcProjWKT = projWKT;
+			else                        dbud.projStruc.SrcProjWKT = ogrproj(proj4);
+			end
+			dbud.toProjPT = 1;
+		elseif (~isempty(projGMT))
+			lims = [getappdata(handles.axes1,'ThisImageLims')];
+			out = mapproject_m([lims(1) lims(3); lims(2) lims(4)],'-R-180/180/0/80','-I','-F',projGMT{:});    % Convert lims back to geogs
+			x_min = min(out(:,1));        x_max = max(out(:,1));
+			y_min = min(out(:,2));        y_max = max(out(:,2));
+			dbud.opt_R = ['-R' sprintf('%f',x_min) '/' sprintf('%f',x_max) '/' sprintf('%f',y_min) '/' sprintf('%f',y_max)];
+			dbud.projGMT = projGMT;
+			dbud.toProjPT = 2;
+		end
+		set(displayBar, 'UserData', dbud);
     end
 
 % --------------------------------------------------------------------
 function [z_min,z_max] = min_max_single(Z)
-% Compute the min/max of single precision Z arrays. I need this due to (another) Matlab
-% bug that gives wrong results when the Z (single) array has NaNs. Ouput are doubles.
-z_min = double(min(Z(~isnan(Z(:)))));   z_max = double(max(Z(~isnan(Z(:)))));
+	% Compute the min/max of single precision Z arrays. I need this due to (another) Matlab
+	% bug that gives wrong results when the Z (single) array has NaNs. Ouput are doubles.
+	z_min = double(min(Z(~isnan(Z(:)))));   z_max = double(max(Z(~isnan(Z(:)))));
 
 % --------------------------------------------------------------------
 function img = strip_bg_color(handles,img)
-% Strip eventual row/columns with color equal to the Figure's background color
-bg_color = uint8(get(handles.figure1,'color')*255);
-c1 = bg_color(1);    c2 = bg_color(2);    c3 = bg_color(3);
-center_row = round(size(img,1) / 2);
-center_col = round(size(img,2) / 2);
-h_Xlabel = get(handles.axes1,'Xlabel');
-h_Ylabel = get(handles.axes1,'Ylabel');
-% Strip north
-i = 1;
-while (img(i,center_col,1) == c1 && img(i,center_col,2) == c2 && img(i,center_col,3) == c3)
-    i = i + 1;
-end;    i = i - 1;
-img(1:i,:,:) = [];      % Strip the eventual gray band
-% Strip west
-j = 0;
-while (img(center_row,end-j,1) == c1 && img(center_row,end-j,2) == c2 && img(center_row,end-j,3) == c3)
-    j = j + 1;
-end;    j = j - 1;
-img(:,end-j:end,:) = [];      % Strip the eventual gray band
-% Strip south
-i = 0;
-while (img(end-i,center_col,1) == c1 && img(end-i,center_col,2) == c2 && img(end-i,center_col,3) == c3)
-    i = i + 1;
-end;    i = i - 1;
-img(end-i:end,:,:) = [];      % Strip the eventual gray band
-% Strip east
-j = 1;
-while (img(center_row,j,1) == c1 && img(center_row,j,2) == c2 && img(center_row,j,3) == c3)
-    j = j + 1;
-end;    j = j - 1;
-img(:,1:j,:) = [];      % Strip the eventual gray band
+	% Strip eventual row/columns with color equal to the Figure's background color
+	bg_color = uint8(get(handles.figure1,'color')*255);
+	c1 = bg_color(1);    c2 = bg_color(2);    c3 = bg_color(3);
+	center_row = round(size(img,1) / 2);
+	center_col = round(size(img,2) / 2);
+	h_Xlabel = get(handles.axes1,'Xlabel');
+	h_Ylabel = get(handles.axes1,'Ylabel');
+	% Strip north
+	i = 1;
+	while (img(i,center_col,1) == c1 && img(i,center_col,2) == c2 && img(i,center_col,3) == c3)
+		i = i + 1;
+	end
+	i = i - 1;
+	img(1:i,:,:) = [];      % Strip the eventual gray band
+	% Strip west
+	j = 0;
+	while (img(center_row,end-j,1) == c1 && img(center_row,end-j,2) == c2 && img(center_row,end-j,3) == c3)
+		j = j + 1;
+	end
+	j = j - 1;
+	img(:,end-j:end,:) = [];      % Strip the eventual gray band
+	% Strip south
+	i = 0;
+	while (img(end-i,center_col,1) == c1 && img(end-i,center_col,2) == c2 && img(end-i,center_col,3) == c3)
+		i = i + 1;
+	end
+	i = i - 1;
+	img(end-i:end,:,:) = [];      % Strip the eventual gray band
+	% Strip east
+	j = 1;
+	while (img(center_row,j,1) == c1 && img(center_row,j,2) == c2 && img(center_row,j,3) == c3)
+		j = j + 1;
+	end
+	j = j - 1;
+	img(:,1:j,:) = [];      % Strip the eventual gray band
 
 % --------------------------------------------------------------------
 function [X,Y] = adjust_lims(X,Y,m,n)
-% Convert the image limits from pixel reg to grid reg
-dx = (X(2) - X(1)) / n;         dy = (Y(2) - Y(1)) / m;
-X(1) = X(1) + dx/2;             X(2) = X(2) - dx/2;
-Y(1) = Y(1) + dy/2;             Y(2) = Y(2) - dy/2;
+	% Convert the image limits from pixel reg to grid reg
+	dx = (X(2) - X(1)) / n;         dy = (Y(2) - Y(1)) / m;
+	X(1) = X(1) + dx/2;             X(2) = X(2) - dx/2;
+	Y(1) = Y(1) + dy/2;             Y(2) = Y(2) - dy/2;
 
 % --------------------------------------------------------------------
 function geog = guessGeog(lims)
@@ -297,10 +326,11 @@ function res = insideRect(rect,pt)
 
 % --------------------------------------------------------------------
 function clean_GRDappdata(handles)
-try
-    rmappdata(handles.figure1,'dem_x');     rmappdata(handles.figure1,'dem_y');     rmappdata(handles.figure1,'dem_z');
-    rmappdata(handles.figure1,'GMThead');
-end
+	% If reistering an image against a grid, those cannot be empty (shity solution)
+	try
+		rmappdata(handles.figure1,'dem_x');     rmappdata(handles.figure1,'dem_y');
+		rmappdata(handles.figure1,'dem_z');
+	end
 
 %-----------------------------------------------------------------------------------------
 function pixelx = axes2pix(dim, x, axesx)
@@ -315,13 +345,13 @@ function pixelx = axes2pix(dim, x, axesx)
 %   Copyright 1993-2002 The MathWorks, Inc.  
 %   $Revision: 5.12 $  $Date: 2002/03/15 15:57:01 $
 
-if (max(size(dim)) ~= 1);   error('First argument must be a scalar.');  end
-if (min(size(x)) > 1);      error('X must be a vector.');               end
-xfirst = x(1);      xlast = x(max(size(x)));
-if (dim == 1);      pixelx = axesx - xfirst + 1;    return;     end
-xslope = (dim - 1) / (xlast - xfirst);
-if ((xslope == 1) && (xfirst == 1));     pixelx = axesx;
-else    pixelx = xslope * (axesx - xfirst) + 1;         end
+	if (max(size(dim)) ~= 1);   error('First argument must be a scalar.');  end
+	if (min(size(x)) > 1);      error('X must be a vector.');               end
+	xfirst = x(1);      xlast = x(max(size(x)));
+	if (dim == 1);      pixelx = axesx - xfirst + 1;    return;     end
+	xslope = (dim - 1) / (xlast - xfirst);
+	if ((xslope == 1) && (xfirst == 1));     pixelx = axesx;
+	else    pixelx = xslope * (axesx - xfirst) + 1;         end
 
 % --------------------------------------------------------------------------------
 function [latcells,loncells] = localPolysplit(lat,lon)
@@ -351,10 +381,10 @@ function [latcells,loncells] = localPolysplit(lat,lon)
 	latcells = cell(N,1);       loncells = cell(N,1);
 	indx = [0; indx];
 	for k = 1:N
-        iStart = indx(k)   + 1;
-        iEnd   = indx(k+1) - 1;
-        latcells{k} = lat(iStart:iEnd);
-        loncells{k} = lon(iStart:iEnd);
+		iStart = indx(k)   + 1;
+		iEnd   = indx(k+1) - 1;
+		latcells{k} = lat(iStart:iEnd);
+		loncells{k} = lon(iStart:iEnd);
 	end
 
 % --------------------------------------------------------------------------------
@@ -380,3 +410,42 @@ function [xdata, ydata, zdata] = localRemoveExtraNanSeps(xdata, ydata, zdata)
 	% Remove the excess NaNs.
 	xdata(s) = [];      ydata(s) = [];
 	if (nargin >= 3),   zdata(s) = [];  end
+
+% -*-*-*-*-*-*-$-$-$-$-$-$-#-#-#-#-#-#-%-%-%-%-%-%-@-@-@-@-@-@-(-)-(-)-(-)-&-&-&-&-&-&-{-}-{-}-{-}-
+function appProjectionRef(handles, strWKT)
+	% If we have a WKT proj store it, otherwise clean eventual predecessors
+    if (~isempty(strWKT))					% If we have a WKT projection, store it
+		setappdata(handles.figure1,'ProjWKT',strWKT)
+        out = decodeProjectionRef(strWKT);				% Decode Proj reference string
+		if ( ~isempty(out.datum) || ~isempty(out.ellipsoid) || ~isempty(out.projection) )
+			setappdata(handles.axes1,'DatumProjInfo',out)
+		end
+    else							% Otherwise remove eventual previous one
+        if (isappdata(handles.figure1,'ProjWKT')),    rmappdata(handles.figure1,'ProjWKT'),		end
+    end
+
+% -*-*-*-*-*-*-$-$-$-$-$-$-#-#-#-#-#-#-%-%-%-%-%-%-@-@-@-@-@-@-(-)-(-)-(-)-&-&-&-&-&-&-{-}-{-}-{-}-
+function out = decodeProjectionRef(strProj)
+    ind = findstr(strProj,char(10));
+    out.datum = [];     out.ellipsoid = [];     out.projection = [];
+    if (numel(ind) <= 1),   return;    end
+    
+    ind = [0 ind length(strProj)-1];
+    for (i=1:numel(ind)-1)
+        str = strProj(ind(i)+1:ind(i+1)-1);
+        if ~isempty(findstr(str,'GEOGCS'))      % Get datum
+            xx = findstr(str,'"');
+            if (numel(xx) < 2),     continue;   end
+            out.datum = str(xx(1)+1:xx(2)-1);
+        end
+        if ~isempty(findstr(str,'SPHEROID'))      % Get ellipsoid
+            if (numel(xx) < 2),     continue;   end
+            xx = findstr(str,'"');
+            out.ellipsoid = str(xx(1)+1:xx(2)-1);
+        end
+        if ~isempty(findstr(str,'PROJECTION'))      % Get ellipsoid
+            if (numel(xx) < 2),     continue;   end
+            xx = findstr(str,'"');
+            out.projection = str(xx(1)+1:xx(2)-1);
+        end
+    end
