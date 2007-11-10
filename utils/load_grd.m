fd19c4c6bf14d37ad2024797683edd196f21d6a9
j <j@localhost>
2007-11-10 01:56:51 +0000

    Call read_gmt_type_grids instead of grdread_m()

--- utils/load_grd.m
+++ utils/load_grd.m
@@ -1,21 +1,18 @@
 function [X,Y,Z,head,m,n] = load_grd(handles, opt)
-	% Load a grid either from memory, or re-read it (GMT only) again if it is to
-	% big to fit in it (biger than handles.grdMaxSize)
+% Load a grid either from memory, or re-read it (GMT only) again if it is
+% too big to fit in it (biger than handles.grdMaxSize)
 
     % Fake image with bg_color. Nothing loadable
-	if (handles.image_type == 20),  X=[];Y=[];Z=[];head=[];   return;     end     
+	if (handles.image_type == 20),  X=[];	Y=[];	Z=[];	head=[];   return,	end     
 	
     if (nargin == 1),   opt = ' ';  end
 	X = getappdata(handles.figure1,'dem_x');    Y = getappdata(handles.figure1,'dem_y');
 	Z = getappdata(handles.figure1,'dem_z');    head = handles.head;
-	if (handles.ForceInsitu),   opt_I = '-I';   % Use only in desperate cases.
-	else                        opt_I = ' ';
-    end
 	if (nargout > 4),           [m,n] = size(Z);    end
 	err_msg = 0;
 	
 	if (isempty(X) && handles.image_type == 1 && ~handles.computed_grid)
-        [X,Y,Z,head] = grdread_m(handles.grdname,'single',opt_I);
+        [handles, X, Y, Z, head] = read_gmt_type_grids(handles,handles.grdname);
         if (nargout == 6),          [m,n] = size(Z);end
 	elseif (isempty(X) && handles.image_type == 4 && ~handles.computed_grid)
         Z = [];     % Check for this to detect a (re)-loading error
