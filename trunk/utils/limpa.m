751b0c27c086a3ba3f4819911c46bc998347a59f
j <j@127d096d-3646-0410-84c9-11b0184650a1>
2008-01-21 17:48:05 +0000

    Tirei o tsunamovie

--- trunk/utils/limpa.m
+++ trunk/utils/limpa.m
@@ -11,7 +11,7 @@ end
 hTelhometro = findobj(handles.figure1,'Label','entry_tl');
 hFloodFil = findobj(handles.figure1,'Label','entry_sh');
 hVitrinite = findobj(handles.figure1,'Label','entry_vtr');
-hTsunamovie = findobj(handles.figure1,'Label','entry_tsm');
+% hTsunamovie = findobj(handles.figure1,'Label','entry_tsm');
 
 if (mata)
     delete([hTelhometro hFloodFil hVitrinite hTsunamovie])
@@ -19,9 +19,9 @@ else
     set(hTelhometro,'Callback','telhometro(gcf)','Label','Telhometro')
     set(hFloodFil,'Callback','mirone(''Transfer_CB'',guidata(gcbo),''Shape'')','Label','Shape detector')
     set(hVitrinite,'Callback','vitrinite','Label','Vitrinite')
-    set(hTsunamovie,'Label','Make tsunami movies')
-    uimenu('Parent',hTsunamovie,'Callback','umDmovie(guidata(gcbo))','Label','1d movie')
-    uimenu('Parent',hTsunamovie,'Callback','tsunamovie(guidata(gcbo))','Label','2d movie')
+%     set(hTsunamovie,'Label','Make tsunami movies')
+%     uimenu('Parent',hTsunamovie,'Callback','umDmovie(guidata(gcbo))','Label','1d movie')
+%     uimenu('Parent',hTsunamovie,'Callback','tsunamovie(guidata(gcbo))','Label','2d movie')
 end
 
 setappdata(handles.figure1,'esDourada',~mata)   % Save goldness info
