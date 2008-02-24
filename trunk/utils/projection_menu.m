613cc04ca4710c1a1deb1b4dc507ca76a4733ef4
j <j@127d096d-3646-0410-84c9-11b0184650a1>
2008-02-24 19:41:27 +0000

    Fixed bug when parsing a GMT projection with > 2 params

--- trunk/utils/projection_menu.m
+++ trunk/utils/projection_menu.m
@@ -37,31 +37,35 @@ function projectionMenu(hFig, home_dir)
     primos = true(m,1);
     for (k=2:m),    primos(k) = ~strcmp(mainMenu{k}, mainMenu{k-1});    end
     
-    % Parse the projStr string and assign it either to a GMT type -J proj or proj4 string
-    projCOMM = cell(m,3);       % 3 for -J -C -T in case of a GMT proj 
-    for (k=1:m)
-        if (isempty(projStr{k})),   continue;       end      % The 'None' line
-        
-        if (projStr{k}(1) == '+')   % We have a Proj4 string
-            projCOMM{k,1} = projStr{k};
-            continue
-        end
-        
-        [t,r] = strtok(projStr{k});
-        projCOMM{k,1} = [t '/1'];   % Append scale
-        if (~isempty(r))            % Either a -C<...> or -T
-            projCOMM{k,2} = r;
-            [t,r] = strtok(r);
-            if (~isempty(t))
-                projCOMM{k,3} = t;   % The other (of -C<...> -T above)
-            end
+	% Parse the projStr string and assign it either to a GMT type -J proj or proj4 string
+	projCOMM = cell(m,3);       % 3 for -J -C -T in case of a GMT proj 
+	for (k=1:m)
+		if (isempty(projStr{k})),	continue,	end      % The 'None' line
+
+		if (projStr{k}(1) == '+')   % We have a Proj4 string
+			projCOMM{k,1} = projStr{k};
+			continue
+		end
+
+		[t,r] = strtok(projStr{k});
+		if ( ~strcmp(t(end-1:end),'/1') )
+			t = [t '/1'];   % Append scale
+		end
+		projCOMM{k,1} = t;
+		if (~isempty(r))            % Either a -C<...> or -T
+			j = 0;
+			while (r)
+				[t,r] = strtok(r);
+				projCOMM{k,2+j} = t;
+				j = j + 1;
+			end
         else
-            projCOMM{k,2} = '-C';    % A must have
+			projCOMM{k,2} = '-C';    % A must have
         end
     end
     
-    % We are ready -- Create the "Projections" menu
-    hProj = uimenu('Parent',hFig,'Label','Projections','Tag','Projections');
+	% We are ready -- Create the "Projections" menu
+	hProj = uimenu('Parent',hFig,'Label','Projections','Tag','Projections');
 
     hMain = zeros(1,m);    hSec  = zeros(1,m);
     for (k=1:m)
@@ -77,12 +81,12 @@ function projectionMenu(hFig, home_dir)
             end
         end
     end
+
+	hMain(hMain == 0) = [];
+	hSec(hSec == 0) = [];
     
-    ind = (hMain == 0);     hMain(ind) = [];
-    ind = (hSec == 0);      hSec(ind) = [];
-    
-    set(hMain(1), 'checked', 'on')
-    setappdata(hFig, 'ProjList', [hMain hSec])
+	set(hMain(1), 'checked', 'on')
+	setappdata(hFig, 'ProjList', [hMain hSec])
 
 % -------------------------------------------------------------------------------
 function [t,r] = getMenuLabel(s)
@@ -103,37 +107,37 @@ function s = ripblanks(s)
 % -------------------------------------------------------------------------------
 function setPRJ(obj,nikles, hFig, k, projCOMM)
     % Set the projection string in Figure's appdata and a checkmark on the selected projection
-    projList = getappdata(hFig,'ProjList');
-    handles = guidata(hFig);
+	projList = getappdata(hFig,'ProjList');
+	handles = guidata(hFig);
 %     unchk = setxor(obj,projList);
 %     set(obj,'checked','on');    set(unchk,'checked','off')
     set(projList,'checked','off');
     set(obj,'checked','on')
     if (strcmp(get(obj,'Label'),'None'))
-        setappdata(hFig,'ProjGMT','')
-        setappdata(hFig,'Proj4','')
-        handles.is_projected = 0;
-    elseif (projCOMM{k}(1) == '+')
-        setappdata(hFig,'Proj4',projCOMM{k,1})  % Here the columns 2 and 3 are empty
-        setappdata(hFig,'ProjGMT','')
-        handles.is_projected = 1;
-    elseif (projCOMM{k}(1) == '-')
-        prj = projCOMM(k,:);
-        % If don't have the third element, remove it because no empties in mapproject
-        if (isempty(prj{end})),     prj(end) = [];  end
-        setappdata(hFig,'ProjGMT',prj)
-        setappdata(hFig,'Proj4','')
-        handles.is_projected = 1;
-    else
-        set(obj,'checked','off')
-        setappdata(hFig,'ProjGMT','')
-        setappdata(hFig,'Proj4','')
-        handles.is_projected = 0;
-        errordlg(['Bad projection string: ' projCOMM{k,:}],'Error')
-    end
-    guidata(hFig, handles)
-    setAxesDefCoordIn(handles);     % Sets the value of the axes uicontextmenu that selects whether project or not
-    aux_funs('toProjPT',handles)    % Set vars to be used when PT->geog conversion, used in PIXVAL_STSBAR
-    if (isempty(getappdata(hFig,'DispInGeogs')))
-        setappdata(hFig,'DispInGeogs',0)     % We need to set it now (this is the default)
-    end
+		setappdata(hFig,'ProjGMT','')
+		setappdata(hFig,'Proj4','')
+		handles.is_projected = 0;
+	elseif (projCOMM{k}(1) == '+')
+		setappdata(hFig,'Proj4',projCOMM{k,1})  % Here the columns 2 and 3 are empty
+		setappdata(hFig,'ProjGMT','')
+		handles.is_projected = 1;
+	elseif (projCOMM{k}(1) == '-')
+		prj = projCOMM(k,:);
+		% If don't have the third element, remove it because no empties in mapproject
+		if (isempty(prj{end})),     prj(end) = [];  end
+		setappdata(hFig,'ProjGMT',prj)
+		setappdata(hFig,'Proj4','')
+		handles.is_projected = 1;
+	else
+		set(obj,'checked','off')
+		setappdata(hFig,'ProjGMT','')
+		setappdata(hFig,'Proj4','')
+		handles.is_projected = 0;
+		errordlg(['Bad projection string: ' projCOMM{k,:}],'Error')
+	end
+	guidata(hFig, handles)
+	setAxesDefCoordIn(handles);     % Sets the value of the axes uicontextmenu that selects whether project or not
+	aux_funs('toProjPT',handles)    % Set vars to be used when PT->geog conversion, used in PIXVAL_STSBAR
+	if (isempty(getappdata(hFig,'DispInGeogs')))
+		setappdata(hFig,'DispInGeogs',0)     % We need to set it now (this is the default)
+	end
