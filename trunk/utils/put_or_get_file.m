3b7ea03f81c730194b65637d45607d8498aa6bf6
j <j@127d096d-3646-0410-84c9-11b0184650a1>
2008-07-22 18:41:48 +0000

    Added a 5th arg that if provided forces 'FileName' to output with a wished extension.

--- trunk/utils/put_or_get_file.m
+++ trunk/utils/put_or_get_file.m
@@ -1,11 +1,18 @@
-function [FileName,PathName,handles] = put_or_get_file(handles,str1,str2,type)
-	% Use this function to select input or output filename
+function [FileName,PathName,handles] = put_or_get_file(handles,str1,str2,type, ext)
+% Use this function to select input or output filename
+% EXT if provided forces 'FileName' to output with that wished extension
+
 	if (strcmp(type,'get'))
 		cd(handles.last_dir)
 		[FileName,PathName] = uigetfile(str1,str2);
 	elseif (strcmp(type,'put'))
 		cd(handles.work_dir)
 		[FileName,PathName] = uiputfile(str1,str2);
+		if (nargin == 5)		% Check that 'FileName' goes with the desired extension
+			[PATH,FNAME,EXT] = fileparts(FileName);
+			if (ext(1) ~= '.'),		ext = ['.' ext];		end
+			if (isempty(EXT)),		FileName = [FNAME ext];	end
+		end
 	end
 	pause(0.01);
 	
