bc6fe7ec620f8e245488d89f0a644c9f7ade701c
j <j@127d096d-3646-0410-84c9-11b0184650a1>
2008-09-05 02:24:48 +0000

    Added more input checks

--- utils/circ_geo.m
+++ utils/circ_geo.m
@@ -7,58 +7,62 @@ function [latc,lonc] = circ_geo(lat,lon,rng,azim,np)
 % "np" specifies the number of output points [default = 180].
 % All angles are in degrees.
 
-D2R = pi/180;   npts  = 180;       az    = [];
-if (nargin < 3)
-    errordlg('Error calling circ_geo. Must give at least 3 arguments','Error')
-elseif (nargin == 4)
-    az = azim;
-elseif (nargin == 5)
-    az = azim;    npts = np;
-end
-        
-%  Allow for multiple circles starting from the same point
-if length(lat) == 1 && length(lon) == 1 && length(rng) ~= 0
-    lat = lat(ones(size(rng)));   lon = lon(ones(size(rng)));
-end
+	D2R = pi/180;   npts  = 180;       az    = [];
+	if (nargin < 3)
+		errordlg('Error calling circ_geo. Must give at least 3 arguments','Error')
+	elseif (nargin == 4)
+		az = azim;
+	elseif (nargin == 5)
+		az = azim;    npts = np;
+	end
 
-if isempty(az),    az = [0 360];    az = az(ones([size(lat,1) 1]), :);  end
-if isequal(size(az), [1 2]),		az = az(ones([size(lat,1) 1]), :);  end
+	%  Allow for multiple circles starting from the same point
+	if (numel(lat) == 1 && numel(lon) == 1 && numel(rng) > 1)
+        lat = lat(ones(size(rng)));   lon = lon(ones(size(rng)));
+	end
 
-% convert to radians
-lat = lat * D2R;    lon = lon * D2R;    az = az * D2R;    rng = rng * D2R;
+	if (isempty(az))
+		az = [0 360];			az = az(ones([size(lat,1) 1]), :);
+	elseif (isequal(size(az), [1 2]))
+		az = az(ones([size(lat,1) 1]), :);
+	end
+	if (numel(rng) == 1),		rng = ones(size(az,1), 1) * rng;	end
 
-%  Expand the azimuth inputs
-if size(az,2) == 1              % Single column azimuth inputs
-    az_neg = zeros(size(az));    az_pos = az;
-else                            % Two column azimuth inputs
-    az_neg = az(:,1);            az_pos = az(:,2);
-end
+	% convert to radians
+	lat = lat * D2R;    lon = lon * D2R;    az = az * D2R;    rng = rng * D2R;
 
-az = zeros([size(az_neg,1) npts]);
-for i = 1:size(az,1)
-    % Handle case where limits give more than half of the circle. Go clockwise from start to end.
-	if az_neg(i) > az_pos(i);     az_pos(i) = az_pos(i)+2*pi;	end
-	az(i,:) = linspace(az_neg(i),az_pos(i),real(npts));	
-end
+	%  Expand the azimuth inputs
+	if size(az,2) == 1              % Single column azimuth inputs
+		az_neg = zeros(size(az));    az_pos = az;
+	else                            % Two column azimuth inputs
+		az_neg = az(:,1);            az_pos = az(:,2);
+	end
 
-% Each circle occupies a row of the output matrices.
-lat = lat(:,ones([1,size(az,2)]) );
-lon = lon(:,ones([1,size(az,2)]) );
-rng = rng(:,ones([1,size(az,2)]) );
+	az = zeros([size(az_neg,1) npts]);
+	for i = 1:size(az,1)
+		% Handle case where limits give more than half of the circle. Go clockwise from start to end.
+		if az_neg(i) > az_pos(i);     az_pos(i) = az_pos(i)+2*pi;	end
+		az(i,:) = linspace(az_neg(i),az_pos(i),real(npts));	
+	end
 
-%  Ensure correct azimuths at either pole.
-epsilon = 1e-07;     % Set tolerance
-ind = find(lat >= pi/2-epsilon);    az(ind) = pi;    % starting at north pole
-ind = find(lat <= epsilon-pi/2);    az(ind) = 0;     % starting at south pole
+	% Each circle occupies a row of the output matrices.
+	lat = lat(:,ones([1,size(az,2)]) );
+	lon = lon(:,ones([1,size(az,2)]) );
+	rng = rng(:,ones([1,size(az,2)]) );
 
-temp1  = sin(lat).*cos(rng);        temp2  = cos(lat).*sin(rng).*cos(az);
-latc   = asin(temp1+temp2);
-temp1  = sin(rng).*sin(az);         temp2  = cos(lat).*cos(rng);
-temp3  = sin(lat).*sin(rng).*cos(az);
-lonc   = lon + atan2(temp1,temp2-temp3);
+	%  Ensure correct azimuths at either pole.
+	epsilon = 1e-07;     % Set tolerance
+	ind = find(lat >= pi/2-epsilon);    az(ind) = pi;    % starting at north pole
+	ind = find(lat <= epsilon-pi/2);    az(ind) = 0;     % starting at south pole
 
-% Truncate angles into the [-pi pi] range
-lonc = pi*((abs(lonc)/pi) - 2*ceil(((abs(lonc)/pi)-1)/2)) .* sign(lonc);
+	temp1  = sin(lat).*cos(rng);        temp2  = cos(lat).*sin(rng).*cos(az);
+	latc   = asin(temp1+temp2);
+	temp1  = sin(rng).*sin(az);         temp2  = cos(lat).*cos(rng);
+	temp3  = sin(lat).*sin(rng).*cos(az);
+	lonc   = lon + atan2(temp1,temp2-temp3);
 
-%  Convert back to degrees
-latc = latc / D2R;      lonc = lonc / D2R;
+	% Truncate angles into the [-pi pi] range
+	lonc = pi*((abs(lonc)/pi) - 2*ceil(((abs(lonc)/pi)-1)/2)) .* sign(lonc);
+
+	%  Convert back to degrees
+	latc = latc / D2R;      lonc = lonc / D2R;
