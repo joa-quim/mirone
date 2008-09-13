c4ef80c3d2b3e32c4a8d02a8f87eb1f16607059a
j <j@127d096d-3646-0410-84c9-11b0184650a1>
2008-09-13 01:37:36 +0000

    There were two functions missing

--- trunk/utils/map_funs.m
+++ trunk/utils/map_funs.m
@@ -227,3 +227,53 @@ imin = find(dist==dmin);
 i1 = I1(imin(1));  i2 = I2(imin(1));
 
 % ---------------------------------------------------------------------
+function [lat,lon] = closefaces(latin,lonin)
+%CLOSEFACES closes all faces of a polygon
+%  $Revision: 1.5 $  $Date: 2003/07/17 15:25:52 $
+
+[lat,lon] = deal(latin,lonin);
+
+if ~iscell(lat),	[lat,lon] = polysplit(lat,lon);		end
+   
+for i=1:length(lat)
+	[latface,lonface] = deal(lat{i},lon{i});
+	[latfacecell,lonfacecell] = polysplit(latface,lonface);
+	
+	for (j=1:length(latfacecell))	% close open polygons
+		if latfacecell{j}(1) ~= latfacecell{j}(end) | lonfacecell{j}(1) ~= lonfacecell{j}(end)
+			latfacecell{j}(end+1) = latfacecell{j}(1);
+			lonfacecell{j}(end+1) = lonfacecell{j}(1);
+		end	
+	end
+	[lat{i},lon{i}]=polyjoin(latfacecell,lonfacecell);
+end
+
+if ~iscell(latin)
+	[lat,lon] = polyjoin(lat,lon);
+end
+
+% ---------------------------------------------------------------------
+function [xc,yc,xh,yh,nanindx] = extractpoly(xp,yp)
+% EXTRACTPOLY  Extract contour and holes from polygon element.
+% [XC,YC,XH,YH,NANINDX] = EXTRACTPOLY(XP,YP) extracts the contour and holes data from a polygon element.
+%  Copyright 1996-2003 The MathWorks, Inc.
+%  $Revision: 1.5 $  $Date: 2003/07/17 15:25:54 $
+
+	% convert data to matrix
+	if iscell(xp),		xp = xp{1};  yp = yp{1};	end
+	
+	% append a nan at the end
+	x = [xp; nan];  y = [yp; nan];
+	indx = find(isnan(x));
+	
+	% contour
+	indxc = 1:indx(1)-1;
+	xc = x(indxc);  yc = y(indxc);
+	
+	% holes
+	xh = [];  yh = [];  nanindx = [];
+	if length(indx)>1
+		indxh = indx(1):indx(end);
+		xh = x(indxh);  yh = y(indxh);
+		nanindx = find(isnan(xh));
+	end
