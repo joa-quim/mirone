9e76ef3e978b22ef97fc7031b594835deb62605d
j <j@127d096d-3646-0410-84c9-11b0184650a1>
2008-09-15 18:32:15 +0000

    Now it returns to where it was and not to home_dir

--- utils/put_or_get_file.m
+++ utils/put_or_get_file.m
@@ -2,6 +2,8 @@ function [FileName,PathName,handles] = put_or_get_file(handles,str1,str2,type, e
 % Use this function to select input or output filename
 % EXT if provided forces 'FileName' to output with that wished extension
 
+	%return_to = handles.home_dir;		% Old behavior
+	return_to = cd;						% New behavior. Return to where it was.
 	if (strcmp(type,'get'))
 		cd(handles.last_dir)
 		[FileName,PathName] = uigetfile(str1,str2);
@@ -9,7 +11,7 @@ function [FileName,PathName,handles] = put_or_get_file(handles,str1,str2,type, e
 		cd(handles.work_dir)
 		[FileName,PathName] = uiputfile(str1,str2);
 		if (isequal(FileName,0))
-			cd(handles.home_dir);       % go home
+			cd(return_to)
 			return
 		end
 		if (nargin == 5)		% Check that 'FileName' goes with the desired extension
@@ -24,4 +26,4 @@ function [FileName,PathName,handles] = put_or_get_file(handles,str1,str2,type, e
 		handles.last_dir = PathName;
 		if (nargout < 3),	guidata(handles.figure1,handles);	end
 	end
-	cd(handles.home_dir);       % allways go home
+	cd(return_to)
