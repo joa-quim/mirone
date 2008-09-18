2031762caf0e760997a4ec9f451d0b47eafc6680
j <j@localhost>
2008-09-18 22:45:37 +0000

    Added entry to world_in_not_round_enough

--- mirone_uis.m
+++ mirone_uis.m
@@ -8,7 +8,7 @@ function [H1,handles,home_dir] = mirone_uis(home_dir)
 %#function griding_mir grdfilter_mir grdsample_mir grdtrend_mir grdgradient_mir ml_clip show_palette 
 %#function geog_calculator color_palettes diluvio fault_models tsu_funs mk_movie_from_list
 %#function mxgridtrimesh aquamoto tiles_tool zonal_integrator grdlandmask_win grdlandmask_m escadeirar
-%#function run_cmd line_operations
+%#function run_cmd line_operations world_is_not_round_enough
 
 	% The following test will tell us if we are using the compiled or the ML version
 	try
@@ -338,7 +338,7 @@ uimenu('Parent',hTL,'Call','zonal_integrator(guidata(gcbo))','Label','Zonal Inte
 uimenu('Parent',hTL,'Call','tiles_tool(guidata(gcbo))','Label','Tiling Tool','Sep','on');
 hVG(kv) = uimenu('Parent',hTL,'Call','diluvio(guidata(gcbo))','Label','Noe Diluge','Sep','on');		kv = kv + 1;
 uimenu('Parent',hTL,'Call','line_operations(guidata(gcbo))','Label','Line Operations','Sep','on','Tag','lineOP');
-% uimenu('Parent',hTL,'Call','world_is_not_round_enough(guidata(gcbo))','Label','World is not (round) enough');
+uimenu('Parent',hTL,'Call','world_is_not_round_enough(guidata(gcbo))','Label','World is not (round) enough');
 % uimenu('Parent',hTL,'Call','shape_tool(gcf)','Label','Limiares','Sep','on');
 % uimenu('Parent',hTL,'Call','autofaults(guidata(gcbo))','Label','Auto falhas','Sep','on');
 
@@ -635,7 +635,7 @@ uimenu('Parent',h,'Call','geog_calculator(guidata(gcbo),''onlyGrid'')','Label','
 h = uimenu('Parent',H1,'Label','Help','Tag','Help');
 uimenu('Parent',h,'Call','aux_funs(''help'',guidata(gcbo))','Label','Mirone Help (v1.3.0)');
 uimenu('Parent',h, 'Call', @showGDALdrivers,'Label','List GDAL formats','Sep','on')
-uimenu('Parent',h, 'Call','about_box(guidata(gcbo),''Mirone Last modified at 15 Sep 2008'',''1.4.0b'')','Label','About','Sep','on');
+uimenu('Parent',h, 'Call','about_box(guidata(gcbo),''Mirone Last modified at 18 Sep 2008'',''1.4.0b'')','Label','About','Sep','on');
 
 % --------------------------- Build HANDLES and finish things here
 	handles = guihandles(H1);
