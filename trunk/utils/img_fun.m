f71ed4544bfa16beb2e5dfd49b0c61848af00713
j <j@127d096d-3646-0410-84c9-11b0184650a1>
2008-10-15 23:10:48 +0000

    Added adaptive median filter

--- trunk/utils/img_fun.m
+++ trunk/utils/img_fun.m
@@ -60,6 +60,12 @@ switch opt
         varargout{1} = stdfilt(varargin{:});
     case 'find_holes'
         varargout{1} = find_holes(varargin{:});
+	otherwise
+		if (nargout)
+			[varargout{1:nargout}] = feval(opt, varargin{:});
+		else
+			feval(opt, varargin{:});
+		end
 end
 
 %----------------------------------------------------------------------------------
@@ -80,6 +86,48 @@ out = makeClaheImage(I, tileMappings, numTiles, selectedRange, numBins, dimTile)
 if ~isempty(noPadRect) %do we need to remove padding?
   out = out(noPadRect.ulRow:noPadRect.lrRow, noPadRect.ulCol:noPadRect.lrCol);
 end
+
+%----------------------------------------------------------------------------------
+function f = adpmedian(g, Smax)
+%ADPMEDIAN Perform adaptive median filtering.
+%   F = ADPMEDIAN(G, SMAX) performs adaptive median filtering of
+%   image G.  The median filter starts at size 3-by-3 and iterates up
+%   to size SMAX-by-SMAX. SMAX must be an odd integer greater than 1.
+
+%   Copyright 2002-2004 R. C. Gonzalez, R. E. Woods, & S. L. Eddins
+%   Digital Image Processing Using MATLAB, Prentice-Hall, 2004
+%   $Revision: 1.5 $  $Date: 2003/11/21 14:19:05 $
+
+% SMAX must be an odd, positive integer greater than 1.
+if (Smax <= 1) | (Smax/2 == round(Smax/2)) | (Smax ~= round(Smax))
+   error('SMAX must be an odd integer > 1.')
+end
+[M, N] = size(g);
+
+% Initial setup.
+f = g;		f(:) = 0;
+alreadyProcessed = false(size(g));
+
+% Begin filtering.
+for k = 3:2:Smax
+   zmin = ordfilt2(g, 1, ones(k, k), 'symmetric');
+   zmax = ordfilt2(g, k * k, ones(k, k), 'symmetric');
+   zmed = medfilt2(g, [k k], 'symmetric');
+   
+   processUsingLevelB = (zmed > zmin) & (zmax > zmed) & ~alreadyProcessed; 
+   zB = (g > zmin) & (zmax > g);
+   outputZxy  = processUsingLevelB & zB;
+   outputZmed = processUsingLevelB & ~zB;
+   f(outputZxy) = g(outputZxy);
+   f(outputZmed) = zmed(outputZmed);
+   
+   alreadyProcessed = alreadyProcessed | processUsingLevelB;
+   if all(alreadyProcessed(:)),		break,		end
+end
+
+% Output zmed for any remaining unprocessed pixels. Note that this zmed was computed
+% using a window of size Smax-by-Smax, which is the final value of k in the loop.
+f(~alreadyProcessed) = zmed(~alreadyProcessed);
 
 %-----------------------------------------------------------------------------
 function tileMappings = makeTileMappings(I, numTiles, dimTile, numBins, clipLimit,...
