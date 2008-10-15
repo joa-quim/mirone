c120c73b147e2d6f033c679e0e61371068dc0b89
j <j@127d096d-3646-0410-84c9-11b0184650a1>
2008-10-15 23:09:30 +0000

    Added, Min, Max and Adaptive median filters

--- trunk/utils/filter_funs.m
+++ trunk/utils/filter_funs.m
@@ -4,25 +4,25 @@ function img = filter_funs( handles, opt )
 
 	img = (get(handles.hImg,'CData'));
     
-switch opt
-    case 'SUSAN'
-        if (ndims(img) == 2)
-            img = susan(img,'-s','-3');
-        else
-            for (i=1:3),    img(:,:,i) = susan(img(:,:,i),'-s','-3');    end
-        end
-    case 'Median'
-        if (ndims(img) == 2)
-            img = img_fun('medfilt2',img,'indexed');
-        else				% RGB image. Do't really know if the following is correct
-			for (k = 1:3)
-            	img(:,:,k) = img_fun('medfilt2',img(:,:,k));
+	switch opt
+		case 'SUSAN'
+			for (i=1:size(img,3)),		img(:,:,i) = susan(img(:,:,i),'-s','-3');    end
+		case 'Median'
+			if (ndims(img) == 2)
+				img = img_fun('medfilt2',img,'indexed');
+			else				% RGB image. Do't really know if the following is correct
+				for (k = 1:3),			img(:,:,k) = img_fun('medfilt2',img(:,:,k));	end
 			end
-        end
-    case 'STD'
-		img = img_fun('stdfilt',img);
-    case 'range'
-		img = img_fun('rangefilt',img);
-end
-
-set(handles.hImg,'CData',img)
+		case 'Adaptive'
+			for (k = 1:size(img,3)),	img(:,:,k) = img_fun('adpmedian', img(:,:,k), 7);		end
+		case 'STD'
+			img = img_fun('stdfilt',img);
+		case 'Min'
+			for (k = 1:size(img,3)),	img(:,:,k) = ordfilt2(img(:,:,k), 1, ones(3), 'symmetric');		end
+		case 'Max'
+			for (k = 1:size(img,3)),	img(:,:,k) = ordfilt2(img(:,:,k), 9, ones(3), 'symmetric');		end
+		case 'range'
+			img = img_fun('rangefilt',img);
+	end
+	
+	set(handles.hImg,'CData',img)
