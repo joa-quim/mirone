fbabf62da4784a36364e3a9d810e669d0a995a5c
j <j@127d096d-3646-0410-84c9-11b0184650a1>
2009-12-07 02:56:25 +0000

    Updates

--- trunk/mex/copy_IPmexs.m
+++ trunk/mex/copy_IPmexs.m
@@ -1,4 +1,5 @@
 % This script will copy the MEXs from the IPT that are needed by Mirone
+% Please revise for your OS (for instance, if it is a 64 bits suffixs need to be changed accordingly)
 
 % Do private do IPT
 fs = filesep;
@@ -6,6 +7,30 @@ pato_in  = [matlabroot fs 'toolbox' fs 'images' fs 'images' fs 'private'];
 
 % Where to copy the MEXs. It must be MIRONEROOT/lib_mex
 pato_out = '/home/bagside/programs/mirone_dev/trunk/lib_mex';
+
+% The following 3 still need to be copied (either depend on IPP or not enough info to rebuild)
+ipts = { 'imfilter_mex' 'imlincombc' 'inv_piecewiselinear'};
+
+% On newer MATLAB instalations you may want to also copy the ones below as well (they might be faster)
+%	'applylutc'
+%	'bwlabel1'
+%	'bwlabel2'
+%	'cq'
+%	'ditherc'
+%	'intlutc'
+%	'grayto8'
+%	'grayto16'
+%	'imhistc'
+%	'inv_lwm'
+%	'ordf'
+%	'parityscan'
+%	'bwboundariesmex'
+%	'bwlabelnmex'
+%	'grayxform'
+%	'imreconstructmex'
+%	'morphmex'
+%	'resampsep'
+
 
 % The suffix for MEXs in this OS. You may need to addapt
 if (ispc)
@@ -17,28 +42,6 @@ else
 	suffix = '.mexglx';
 end
 
-ipts = {
-	'applylutc'
-	'bwlabel1'
-	'bwlabel2'
-	'cq'
-	'ditherc'
-	'intlutc'
-	'grayto8'
-	'grayto16'
-	'imhistc'
-	'inv_lwm'
-	'ordf'
-	'parityscan'
-	'bwboundariesmex'
-	'bwlabelnmex'
-	'grayxform'
-	'imfilter_mex'
-	'imlincombc'
-	'imreconstructmex'
-	'inv_piecewiselinear'
-	'morphmex'
-	'resampsep'};
 
 for k=1:numel(ipts)
 	copyfile([pato_in fs ipts{k} suffix], pato_out, 'f')
