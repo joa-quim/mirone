45ed74b18c4f7e6cb175c3f1999416258e85df35
j <j@localhost>
2010-01-24 02:27:21 +0000

    Use #ifndef MX_COMPAT_32 as much more robust test condition for when this file should be used.

--- mex/mwsize.h
+++ mex/mwsize.h
@@ -3,17 +3,17 @@
  * This should be harmless to ML versions where mwSize is natively defined.
  */
 
-#ifndef mwSize
+#ifndef MX_COMPAT_32
+
 typedef int mwSize;
 typedef int mwIndex;
 typedef int mwSignedIndex;
 
-# define MWSIZE_MAX    2147483647UL
-# define MWINDEX_MAX   2147483647UL
-# define MWSINDEX_MAX  2147483647L
-# define MWSINDEX_MIN -2147483647L
+#define MWSIZE_MAX    2147483647UL
+#define MWINDEX_MAX   2147483647UL
+#define MWSINDEX_MAX  2147483647L
+#define MWSINDEX_MIN -2147483647L
 #define MWSIZE_MIN    0UL
 #define MWINDEX_MIN   0UL
-
 
 #endif
