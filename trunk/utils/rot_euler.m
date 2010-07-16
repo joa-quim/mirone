da4adaffacecad4ed1d515a284ed4bcc92ba7c6d
j <j@127d096d-3646-0410-84c9-11b0184650a1>
2010-07-16 16:09:53 +0000

    Now can optionally do the calculations using geocentric latitudes

--- trunk/utils/rot_euler.m
+++ trunk/utils/rot_euler.m
@@ -1,4 +1,4 @@
-function [rlon,rlat] = rot_euler(lon,lat,p_lon,p_lat,omega,units)
+function [rlon,rlat] = rot_euler(lon,lat,p_lon,p_lat,omega,units, ecc)
 % ROT_EULER  Rotate points (or lines) about an Euler pole
 %
 %  [rlon,rlat] = ROT_EULER(lat,lon,p_lon,p_lat,omega)   # Two column vectors output
@@ -7,6 +7,12 @@ function [rlon,rlat] = rot_euler(lon,lat,p_lon,p_lat,omega,units)
 %  P_LON, P_LAT & OMEGA are the obvious parameters that define the Euler pole
 %  UNITS -> by default angles are expected to be in degrees. Use UNITS = 'radians'
 %  if input AND output angles are in radians
+%
+%	GEOCENTRIC LATITUDES.
+%	Convert to geocentric latitudes -> rotate -> convert back to geodetic latitudes
+%	(..., units) UNITS	-> Numeric value. The numeric value contain the first exccentricity
+%	(...,units, ecc)	-> Units selects either 'radians' or 'degrees' as above
+%	If ECC = -1, use the default value for the WGS84 datum
 %
 %   Based on code extracted from GMT
 
@@ -24,40 +30,65 @@ function [rlon,rlat] = rot_euler(lon,lat,p_lon,p_lat,omega,units)
 %	Contact info: w3.ualg.pt/~jluis/mirone
 % --------------------------------------------------------------------
 
-if (nargin < 5)
-    error('ROT_EULER: Incorrect number of arguments')
-elseif (nargin == 6 && strcmp(units,'radians'))
-    is_radians = 1;
-else
-    is_radians = 0;
-end
+	is_radians = false;		do_geocentric = false;
+	if (nargin < 5)
+		error('ROT_EULER: Incorrect number of arguments')
+	elseif (nargin == 6)
+		if (ischar(units))
+			if (strncmp(units,'rad',3))
+				is_radians = true;
+			end
+		else
+			ecc = units;	% Excentricity
+			if (ecc < 0)
+				ecc = 0.0818191908426215;	% WGS84
+			end
+			do_geocentric = true;
+		end
+	else
+		if (strncmp(units,'rad',3))
+			is_radians = true;
+		end
+		if (ecc < 0)
+			ecc = 0.0818191908426215;		% WGS84
+		end
+		do_geocentric = true;
+	end
 
-D2R = pi/180;			R2D = 180 / pi;
-if (~is_radians)		% Angles are in degrees. We must convert them
-	lon = lon(:) * D2R;			lat = lat(:) * D2R;
-	p_lon = p_lon * D2R;		p_lat = p_lat * D2R;
-	omega = omega * D2R;
-end
+	D2R = pi/180;			R2D = 180 / pi;
+	if (~is_radians)		% Angles are in degrees. We must convert them
+		lon = lon(:) * D2R;			lat = lat(:) * D2R;
+		p_lon = p_lon * D2R;		p_lat = p_lat * D2R;
+		omega = omega * D2R;
+	end
 
-p_sin_lat = sin(p_lat);			p_cos_lat = cos(p_lat);
-s_lat = sin(lat);				c_lat = cos(lat);
-s_lon = sin(lon - p_lon);		c_lon = cos(lon - p_lon);
-clear lon lat;
-cc = c_lat .* c_lon;
+	if (do_geocentric)		% Convert to geocentric latitudes
+		lat = atan2( (1-ecc^2)*sin(lat), cos(lat) );
+	end
 
-tlon = atan2(c_lat .* s_lon, p_sin_lat * cc - p_cos_lat * s_lat);
-s_lat = p_sin_lat * s_lat + p_cos_lat * cc;
-c_lat = sqrt(1 - s_lat .* s_lat);
+	p_sin_lat = sin(p_lat);			p_cos_lat = cos(p_lat);
+	s_lat = sin(lat);				c_lat = cos(lat);
+	s_lon = sin(lon - p_lon);		c_lon = cos(lon - p_lon);
+	clear lon lat;
+	cc = c_lat .* c_lon;
 
-s_lon = sin(tlon + omega);		c_lon = cos(tlon + omega);
-cc = c_lat .* c_lon;			clear tlon c_lon;
+	tlon = atan2(c_lat .* s_lon, p_sin_lat * cc - p_cos_lat * s_lat);
+	s_lat = p_sin_lat * s_lat + p_cos_lat * cc;
+	c_lat = sqrt(1 - s_lat .* s_lat);
 
-rlat = asin(p_sin_lat * s_lat - p_cos_lat * cc);
-rlon = p_lon + atan2(c_lat .* s_lon, p_sin_lat * cc + p_cos_lat * s_lat);
+	s_lon = sin(tlon + omega);		c_lon = cos(tlon + omega);
+	cc = c_lat .* c_lon;			clear tlon c_lon;
 
-ind = (rlon > pi);				rlon(ind) = rlon(ind) - 2*pi;
-if (~is_radians)				% User wants angles in degrees
-	rlon = rlon * R2D;			rlat = rlat * R2D;
-end
+	rlat = asin(p_sin_lat * s_lat - p_cos_lat * cc);
+	rlon = p_lon + atan2(c_lat .* s_lon, p_sin_lat * cc + p_cos_lat * s_lat);
 
-if (nargout == 1),		rlon = [rlon rlat];		end
+	if (do_geocentric)		% Convert back to geodetic latitudes
+		rlat = atan2( sin(rlat), (1-ecc^2)*cos(rlat) );
+	end
+
+	ind = (rlon > pi);				rlon(ind) = rlon(ind) - 2*pi;
+	if (~is_radians)				% User wants angles in degrees
+		rlon = rlon * R2D;			rlat = rlat * R2D;
+	end
+
+	if (nargout == 1),		rlon = [rlon rlat];		end
