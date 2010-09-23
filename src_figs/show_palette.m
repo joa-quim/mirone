f9392782eac37a6aec36561f56c5498244384da2
j <j@localhost>
2010-09-23 16:52:09 +0000

    For palette at side, restore figure's size to what it was before palette insertion. Do this by soring that info in userdata instead of recomputing what the size should be.

--- src_figs/show_palette.m
+++ src_figs/show_palette.m
@@ -19,7 +19,7 @@ function varargout = show_palette(varargin)
     tipo = varargin{2};
     
     % Some tests
-	if (handMir.no_file),     return;      end
+	if (handMir.no_file),		return,		end
 	if (~handMir.validGrid && ndims(get(handMir.hImg,'CData')) == 3)
         msgbox('True color images do not use color palettes. Bye','Warning');    return
 	end
@@ -59,23 +59,24 @@ function varargout = show_palette(varargin)
 		set(hObject,'Visible','on');
 		if (nargout),   varargout{1} = hObject;     end
 
-    elseif (strcmp(tipo,'At'))              % Create a colorbar right next to image's right side
+    elseif (strcmp(tipo,'At'))				% Create a colorbar right next to image's right side
 
+		origFigWidth = posFigParent(3);		% To store original figure's width before palette insertion
 		hUict = handMir.PalAt;
 		% If we already have a colorbar, remove it
 		if (strcmp(get(hUict,'Check'),'on'))
 			ud = get(hUict,'Userdata');
-			posFigParent(3) = posAxParent(1) + posAxParent(3) + 9;  % 9 is for the slider + 2 
+			posFigParent(3) = ud(2);		% Figure's width before palette insertion 
 			if (posFigParent(3) == screen(3)),      posFigParent(3) = posFigParent(3)-1;    end     % The elastic
 			set(handMir.figure1,'pos',posFigParent)
-			delete(ud)
+			delete(ud(1))					% Delete the color palette
 			set(hUict,'Checked','off')
 			return
 		end
 
 		% Create the color bar axes at the right side of the main image axes
-        barW = 20;      % Colorbar width
-        marg = 8;       % Margin between Image and colorbar (must be enoug for slider width)
+        barW = 20;			% Colorbar width
+        marg = 8;			% Margin between Image and colorbar (must be enoug for slider width)
         axPos = [posAxParent(1)+posAxParent(3)+marg posAxParent(2) barW min(420,posAxParent(4))];
 		hAx = axes('Units','Pixels','pos',axPos,'Parent',handMir.figure1,'vis','off');
 		image([1 10],handMir.head(5:6),(1:size(cmap,1))');
@@ -85,16 +86,16 @@ function varargout = show_palette(varargin)
 		Ylabel_pos = get(h_Ylabel,'Extent');
 
         gutterRight = posFigParent(3) - (posAxParent(1) + posAxParent(3));
-		if (gutterRight < Ylabel_pos(1)+marg)           % Grow Figure's width to acomodate the colorbar
+		if (gutterRight < Ylabel_pos(1)+marg)			% Grow Figure's width to acomodate the colorbar
 			posFigParent(3) = posFigParent(3) + (Ylabel_pos(1)+marg - gutterRight - 3); % -3 is empiric
 			if (posFigParent(3) == screen(3)),      posFigParent(3) = posFigParent(3)-1;    end     % The elastic
 			set(handMir.figure1, 'Position', posFigParent);
 		end
-		set(hUict,'Userdata',hAx)        % Save it so that we can restore upon colorbar deletion
+		set(hUict,'Userdata',[hAx origFigWidth])		% Save it so that we can restore upon colorbar deletion
 		set(hAx,'Units','normalized','Vis','on')
 		set(hUict,'Checked','on')
 
-    elseif (strcmp(tipo,'In'))                  % Create a colorbar inside the image, at it's right side
+    elseif (strcmp(tipo,'In'))					% Create a colorbar inside the image, at it's right side
 
 		hUict = handMir.PalIn;
 		% If we already have a colorbar, remove it
