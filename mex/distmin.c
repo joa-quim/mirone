b835b518763b792041b167c5fd8ab3614cf42d73
j <j@127d096d-3646-0410-84c9-11b0184650a1>
2010-12-28 01:30:58 +0000

    Improve the algorithm to be much clever in finding the closest vertex from one line to the other. With this we gained another > times 2 factor improvement in running time.

--- mex/distmin.c
+++ mex/distmin.c
@@ -1,11 +1,13 @@
 /*
  * Compute the shortest distance between each point in (lon,lat) and the
  * polyline (r_lon,r_lat) SEGLEN holds the segment length of polyline 
- * (r_lon,r_lat) corresponding to elements of DIST
+ * (r_lon,r_lat) corresponding to elements of DIST.
  *
  * This MEX is 20x faster than the corresponding matlab code (in compute_euler)
  * Equivalent Matlab call
  * [dist, segLen] = distmin(lon, lat, r_lon, r_lat, lengthsRot)
+ *
+ * NOTE: This assumes that both lines have the same "growing direction"
  *
  * Author:	Joaquim Luis
  * Date:	22-Dec-2010
@@ -14,17 +16,17 @@
 
 #include "mex.h"
 #include <math.h>
-/*#ifdef _OPENMP
+#ifdef _OPENMP
 #include <omp.h>
-#endif*/
+#endif
 
 /* --------------------------------------------------------------------------- */
 /* Matlab Gateway routine */
 
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
-	int	i, k, n_pt, n_pt_rot, ind;
-	double	min, Dsts, tmp1, tmp2, Q1[2], Q2[2], DQ[2], D1, D2;
-	double	*dist, *segLen, *lon, *lat, *r_lon, *r_lat, *lengthsRot;
+	int	i, k, n_pt, n_pt_rot, ind = 0;
+	double	min = 1e20, Dsts, tmp1, tmp2, Q1[2], Q2[2], Q3[2], DQ[2], D1, D2;
+	double	*dist, *segLen, *lon, *lat, *r_lon, *r_lat, *lengthsRot, lon0, lat0;
 
 	lon = mxGetPr(prhs[0]); 
 	lat = mxGetPr(prhs[1]); 
@@ -40,28 +42,62 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
 	plhs[1] = mxCreateDoubleMatrix (n_pt,1, mxREAL);
 	segLen = mxGetPr(plhs[1]);
 
+	/* ---- Find vertex of rotated line that is closest to first vertex on fixed line ---- */
+	for (i = 0; i < n_pt_rot; ++i) {		/* Loop over fixed line vertices */
+		tmp1 = lon[0] - r_lon[i];	tmp2 = lat[0] - r_lat[i];
+		Dsts = tmp1*tmp1 + tmp2*tmp2;
+		if (Dsts < min) {
+			min = Dsts;		ind = i;
+		}
+	}
+	if (!(ind == 0 || ind == n_pt_rot-1)) {
+		Q1[0] = r_lon[ind-1];	Q1[1] = r_lat[ind-1];
+		Q2[0] = r_lon[ind];	Q2[1] = r_lat[ind];
+		DQ[0] = Q2[0] - Q1[0];	DQ[1] = Q2[1] - Q1[1];
+		D1 = fabs(DQ[0]*(lat[0]-Q1[1]) - DQ[1]*(lon[0]-Q1[0])) / 
+			sqrt(DQ[0]*DQ[0] + DQ[1]*DQ[1]);
+		Q1[0] = r_lon[ind];	Q1[1] = r_lat[ind];
+		Q3[0] = r_lon[ind+1];	Q3[1] = r_lat[ind+1];
+		DQ[0] = Q3[0] - Q2[0];	DQ[1] = Q3[1] - Q2[1];
+		D2 = fabs(DQ[0]*(lat[0]-Q2[1]) - DQ[1]*(lon[0]-Q2[0])) / 
+			sqrt(DQ[0]*DQ[0] + DQ[1]*DQ[1]);
+
+		if (D1 < D2) {
+			dist[0] = D1;	segLen[0] = lengthsRot[ind-1];
+		}
+		else {
+			dist[0] = D2;	segLen[0] = lengthsRot[ind];
+		}
+	}
+	/* ----------------------------------------------------------------------------------- */
+
 /*#pragma omp parallel for*/
-	for (k = 0; k < n_pt; ++k) {
-		min = 1e20;	ind = -1;
-		for (i = 0; i < n_pt_rot; ++i) {
+/*#pragma omp parallel num_threads(4)*/
+
+	for (k = 1; k < n_pt; ++k) {			/* Loop over fixed line vertices */
+		min = 1e20;	/*ind = 0;*/
+		/* Now we only have to look starting at previously found index and ahead */
+		for (i = ind; i < n_pt_rot; ++i) {	/* Loop over rotated lined vertices */
 			tmp1 = lon[k] - r_lon[i];	tmp2 = lat[k] - r_lat[i];
 			Dsts = tmp1*tmp1 + tmp2*tmp2;
 			if (Dsts < min) {
 				min = Dsts;		ind = i;
 			}
+			else if (i > ind)	/* When error starts to grow again we assume to */
+				break;		/* have passed the closest vertex on fixed line */
 		}
-	
+
 		if (ind == 0 || ind == n_pt_rot-1) continue;
-	
+
 		Q1[0] = r_lon[ind-1];	Q1[1] = r_lat[ind-1];
 		Q2[0] = r_lon[ind];	Q2[1] = r_lat[ind];
 		DQ[0] = Q2[0] - Q1[0];	DQ[1] = Q2[1] - Q1[1];
 		D1 = fabs(DQ[0]*(lat[k]-Q1[1]) - DQ[1]*(lon[k]-Q1[0])) / 
 			sqrt(DQ[0]*DQ[0] + DQ[1]*DQ[1]);
 		Q1[0] = r_lon[ind];	Q1[1] = r_lat[ind];
-		Q2[0] = r_lon[ind+1];	Q2[1] = r_lat[ind+1];
-		DQ[0] = Q2[0] - Q1[0];	DQ[1] = Q2[1] - Q1[1];
-		D2 = fabs(DQ[0]*(lat[k]-Q1[1]) - DQ[1]*(lon[k]-Q1[0])) / 
+		Q3[0] = r_lon[ind+1];	Q3[1] = r_lat[ind+1];
+		DQ[0] = Q3[0] - Q2[0];	DQ[1] = Q3[1] - Q2[1];
+		D2 = fabs(DQ[0]*(lat[k]-Q2[1]) - DQ[1]*(lon[k]-Q2[0])) / 
 			sqrt(DQ[0]*DQ[0] + DQ[1]*DQ[1]);
 
 		if (D1 < D2) {
@@ -94,17 +130,3 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
 		*soma /= pesos;
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
