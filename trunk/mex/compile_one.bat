177f60d4e4df36f2bbde34bd4a2b222996f05331
j <j@127d096d-3646-0410-84c9-11b0184650a1>
2011-01-06 01:57:18 +0000

    Some refinements

--- trunk/mex/compile_one.bat
+++ trunk/mex/compile_one.bat
@@ -30,7 +30,9 @@ REM If set to "yes", linkage is done againsts ML6.5 Libs (needed in compiled ver
 SET R13="no"
 
 REM Set it to "yes" or "no" to build under 64-bits or 32-bits respectively.
-SET WIN64="no"
+SET WIN64="yes"
+
+IF %R13%=="yes" SET WIN64="no"
 
 REM The MSVC version. I use this var to select libraries also compiled with this compiler
 SET MSVC_VER="1600"
@@ -59,15 +61,15 @@ SET MEX_EXT="dll"
 ) ELSE (
 
 IF %WIN64%=="yes" (
-SET MATLIB=C:\PROGRAMS\MATLAB\R2010A\extern\lib\win64\microsoft 
-SET MATINC=C:\PROGRAMS\MATLAB\R2010A\extern\include
+SET MATLIB=C:\PROGRAMS\MATLAB\R2010B\extern\lib\win64\microsoft 
+SET MATINC=C:\PROGRAMS\MATLAB\R2010B\extern\include
 SET _MX_COMPAT=-DMX_COMPAT_32
 SET MEX_EXT="mexw64"
 
 ) ELSE (
 
-SET MATLIB=C:\PROGRAMS\MATLAB\R2009B\extern\lib\win32\microsoft 
-SET MATINC=C:\PROGRAMS\MATLAB\R2009B\extern\include
+SET MATLIB=C:\PROGRAMS\MATLAB32\R2010B\extern\lib\win32\microsoft 
+SET MATINC=C:\PROGRAMS\MATLAB32\R2010B\extern\include
 SET _MX_COMPAT=-DMX_COMPAT_32
 ) )
 
@@ -75,25 +77,32 @@ SET _MX_COMPAT=-DMX_COMPAT_32
 REM -------------- Set up libraries here -------------------------------------------------
 IF %WIN64%=="yes" (
 
-SET NETCDF_LIB=C:\progs_cygw\netcdf-3.6.3\compileds\VC10_64\lib\libnetcdf.lib
-rem SET GMT_LIB=c:\progs_cygw\GMTdev\GMT_win64\lib\gmt.lib
-rem SET GMT_MGG_LIB=c:\progs_cygw\GMTdev\GMT_win64\lib\gmt_mgg.lib
-SET GMT_LIB=c:\progs_cygw\GMTdev\GMT5\lib\gmt.lib
-SET GMT_MGG_LIB=c:\progs_cygw\GMTdev\GMT5\lib\gmt_mgg.lib
-SET GDAL_LIB=c:\programs\GDALtrunk\gdal\compileds\VC10_64\lib\gdal_i.lib
-SET CV_LIB=C:\programs\OpenCV_SVN\compileds\VC10_64\lib\cv210.lib
-SET CXCORE_LIB=C:\programs\OpenCV_SVN\compileds\VC10_64\lib\cxcore210.lib
+SET  NETCDF_LIB=C:\progs_cygw\netcdf-3.6.3\compileds\VC10_64\lib\libnetcdf.lib
+SET     GMT_LIB=c:\progs_cygw\GMTdev\GMT_win64\lib\gmt.lib
+SET GMT_MGG_LIB=c:\progs_cygw\GMTdev\GMT_win64\lib\gmt_mgg.lib
+SET    GDAL_LIB=c:\programs\GDALtrunk\gdal\compileds\VC10_64\lib\gdal_i.lib
+SET      CV_LIB=
+SET  CXCORE_LIB=C:\programs\OpenCV_SVN\compileds\VC10_64\lib\opencv_core211.lib
+SET   CVIMG_LIB=C:\programs\OpenCV_SVN\compileds\VC10_64\lib\opencv_imgproc211.lib
+SET CVCALIB_LIB=C:\programs\OpenCV_SVN\compileds\VC10_64\lib\opencv_calib3d211.lib
+SET   CVOBJ_LIB=C:\programs\OpenCV_SVN\compileds\VC10_64\lib\opencv_objdetect211.lib
+SET CVVIDEO_LIB=C:\programs\OpenCV_SVN\compileds\VC10_64\lib\opencv_video211.lib
+SET     LAS_LIB=C:\programs\compa_libs\liblas-src-1.2.1\lib\VC10_64\liblas_i.lib
 
 ) ELSE (
 
 IF %MSVC_VER%=="1600" (
-SET NETCDF_LIB=C:\progs_cygw\netcdf-3.6.3\compileds\VC10_32\lib\libnetcdf.lib
-SET GMT_LIB=c:\progs_cygw\GMTdev\GMT_win\lib\gmt.lib
+SET  NETCDF_LIB=C:\progs_cygw\netcdf-3.6.3\compileds\VC10_32\lib\libnetcdf.lib
+SET     GMT_LIB=c:\progs_cygw\GMTdev\GMT_win\lib\gmt.lib
 SET GMT_MGG_LIB=c:\progs_cygw\GMTdev\GMT_win\lib\gmt_mgg.lib
-SET GDAL_LIB=c:\programs\GDALtrunk\gdal\compileds\VC10_32\lib\gdal_i.lib
-SET CV_LIB=C:\programs\OpenCV_SVN\compileds\VC10_32\lib\cv210.lib
-SET CXCORE_LIB=C:\programs\OpenCV_SVN\compileds\VC10_32\lib\cxcore210.lib
-SET LAS_LIB=C:\programs\compa_libs\liblas-src-1.2.1\bin\lib\liblas_i.lib
+SET    GDAL_LIB=c:\programs\GDALtrunk\gdal\compileds\VC10_32\lib\gdal_i.lib
+SET      CV_LIB=C:\programs\OpenCV_SVN\compileds\VC10_32\lib\cv.lib
+SET  CXCORE_LIB=C:\programs\OpenCV_SVN\compileds\VC10_32\lib\opencv_core211.lib
+SET   CVIMG_LIB=C:\programs\OpenCV_SVN\compileds\VC10_32\lib\opencv_imgproc211.lib
+SET CVCALIB_LIB=C:\programs\OpenCV_SVN\compileds\VC10_32\lib\opencv_calib3d211.lib
+SET   CVOBJ_LIB=C:\programs\OpenCV_SVN\compileds\VC10_32\lib\opencv_objdetect211.lib
+SET CVVIDEO_LIB=C:\programs\OpenCV_SVN\compileds\VC10_32\lib\opencv_video211.lib
+SET     LAS_LIB=C:\programs\compa_libs\liblas-src-1.2.1\lib\Intel11_32\liblas_i.lib
 
 ) ELSE (
 
@@ -108,11 +117,14 @@ SET LAS_LIB=C:\programs\compa_libs\liblas-src-1.2.1\bin\lib\liblas_i.lib
 ) )
 
 SET NETCDF_INC=C:\progs_cygw\netcdf-3.6.3\include
-rem SET GMT_INC=c:\progs_cygw\GMTdev\GMT\include
-SET GMT_INC=c:\progs_cygw\GMTdev\GMT5\include
+SET GMT_INC=c:\progs_cygw\GMTdev\GMT\include
+REM SET GMT_INC=c:\progs_cygw\GMTdev\GMT5\include
 SET GMT_INC2=C:\progs_cygw\GMTdev\GMT5\src\mex
 SET GDAL_INC=c:\programs\GDALtrunk\gdal\compileds\VC10_32\include
-SET OCV_INC=C:\programs\OpenCV_SVN\compileds\VC10_32\include\opencv
+SET CV_INC=C:\programs\OpenCV_SVN\include\opencv
+SET CVInc=C:\programs\OpenCV_SVN\modules 
+SET INCAS=%INCLUDE%
+SET INCLUDE=%INCLUDE%;%CVInc%\core\include;%CVInc%\imgproc\include;%CVInc%\features2d\include;%CVInc%\calib3d\include;%CVInc%\objdetect\include;%CVInc%\video\include;%CVInc%\flann\include;%CVInc%\legacy\include
 SET LAS_INC=-IC:\programs\compa_libs\liblas-src-1.2.1\bin\include\liblas\capi -IC:\programs\compa_libs\liblas-src-1.2.1\bin\include\liblas
 REM ----------------------------------------------------------------------------
 
@@ -120,7 +132,8 @@ REM ____________________________________________________________________________
 REM ___________________ STOP EDITING HERE ______________________________________
 
 
-SET COMPFLAGS=/c /Zp8 /GR /EHs /D_CRT_SECURE_NO_DEPRECATE /D_SCL_SECURE_NO_DEPRECATE /D_SECURE_SCL=0 /DMATLAB_MEX_FILE /nologo /MD 
+SET COMPFLAGS=/c /Zp8 /GR /EHs /D_CRT_SECURE_NO_DEPRECATE /D_SCL_SECURE_NO_DEPRECATE /D_SECURE_SCL=0 /DMATLAB_MEX_FILE /nologo /MD
+SET OPTIM2=/QxSSE4.2 /Qparallel
 IF %DEBUG%=="no" SET OPTIMFLAGS=/Ox /Oy- /DNDEBUG
 IF %DEBUG%=="yes" SET OPTIMFLAGS=/Z7
 
@@ -128,7 +141,8 @@ IF %WIN64%=="yes" SET arc=X64
 IF %WIN64%=="no" SET arc=X86
 SET LINKFLAGS=/dll /export:mexFunction /LIBPATH:%MATLIB% libmx.lib libmex.lib libmat.lib /MACHINE:%arc% kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib /nologo /incremental:NO %LDEBUG% 
 
-%CC% -DWIN32 %COMPFLAGS% -I%GMT_INC% -I%GDAL_INC% -I%NETCDF_INC% -I%OCV_INC% -I%MATINC% %LAS_INC% %OPTIMFLAGS% %_MX_COMPAT% %TIMEIT% %1.c
+%CC% -DWIN32 %COMPFLAGS% -I%GMT_INC% -I%GDAL_INC% -I%NETCDF_INC% -I%CV_INC% -I%MATINC% %LAS_INC% %OPTIMFLAGS% %_MX_COMPAT% %TIMEIT% %1.c
 link  /out:"%1.%MEX_EXT%" %LINKFLAGS% %NETCDF_LIB% %GMT_LIB% %GDAL_LIB% %LAS_LIB% /implib:templib.x %1.obj
 
+SET INCLUDE=%INCAS%
 del *.obj *.exp templib.x
