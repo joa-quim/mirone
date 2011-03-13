e48800d957692e60885415b2cd963acd08c8fc2b
j <j@127d096d-3646-0410-84c9-11b0184650a1>
2011-03-13 03:46:19 +0000

    Replace License notice to LGPL

--- trunk/src_figs/parker_stuff.m
+++ trunk/src_figs/parker_stuff.m
@@ -1,16 +1,17 @@
 function varargout = parker_stuff(varargin)
 % Helper window to do Parker inversions and Reduce To the Pole 
 
-%	Copyright (c) 2004-2010 by J. Luis
+%	Copyright (c) 2004-2011 by J. Luis
 %
-%	This program is free software; you can redistribute it and/or modify
-%	it under the terms of the GNU General Public License as published by
-%	the Free Software Foundation; version 2 of the License.
-%
-%	This program is distributed in the hope that it will be useful,
-%	but WITHOUT ANY WARRANTY; without even the implied warranty of
-%	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-%	GNU General Public License for more details.
+% 	This program is part of Mirone and is free software; you can redistribute
+% 	it and/or modify it under the terms of the GNU Lesser General Public
+% 	License as published by the Free Software Foundation; either
+% 	version 2.1 of the License, or any later version.
+% 
+% 	This program is distributed in the hope that it will be useful,
+% 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+% 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+% 	Lesser General Public License for more details.
 %
 %	Contact info: w3.ualg.pt/~jluis/mirone
 % --------------------------------------------------------------------
