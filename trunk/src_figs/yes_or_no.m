1bd610402c059e3506ecc5a33fe07eab0691a8e4
j <j@127d096d-3646-0410-84c9-11b0184650a1>
2011-03-13 03:54:53 +0000

    Replace License notice to LGPL

--- trunk/src_figs/yes_or_no.m
+++ trunk/src_figs/yes_or_no.m
@@ -4,21 +4,21 @@ function varargout = yes_or_no(varargin)
 %   The form h=yes_or_no('string',str,'title','Warning') returns the answer
 %   to the question contained in the cellstring STR
 
-%	Copyright (c) 2004-2006 by J. Luis
+%	Copyright (c) 2004-2011 by J. Luis
 %
-%	This program is free software; you can redistribute it and/or modify
-%	it under the terms of the GNU General Public License as published by
-%	the Free Software Foundation; version 2 of the License.
-%
-%	This program is distributed in the hope that it will be useful,
-%	but WITHOUT ANY WARRANTY; without even the implied warranty of
-%	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-%	GNU General Public License for more details.
+% 	This program is part of Mirone and is free software; you can redistribute
+% 	it and/or modify it under the terms of the GNU Lesser General Public
+% 	License as published by the Free Software Foundation; either
+% 	version 2.1 of the License, or any later version.
+% 
+% 	This program is distributed in the hope that it will be useful,
+% 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+% 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+% 	Lesser General Public License for more details.
 %
 %	Contact info: w3.ualg.pt/~jluis/mirone
 % --------------------------------------------------------------------
 
-%if (nargin >= 4 && isstr(varargin{1}) && ~isstr(varargin{2}))
 if (nargin == 4 && isempty(varargin{3}))
     % Come here when called by one of the buttons callback
     gui_Callback = str2func(varargin{1});
