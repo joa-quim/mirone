79285e14cf9968bd59990099ef90d298846f1902
j <j@127d096d-3646-0410-84c9-11b0184650a1>
2011-03-13 15:46:30 +0000

    Replace License notice to LGPL

--- utils/ui_edit_patch_special.m
+++ utils/ui_edit_patch_special.m
@@ -4,6 +4,21 @@ function ui_edit_patch_special(varargin)
 %  ui_edit_patch_special. Double clicking on the patch displays its vertices limits.
 %  To move, click and drag on the patch. To stop "moving mode" double click again.
 
+%	Copyright (c) 2004-2011 by J. Luis
+%
+% 	This program is part of Mirone and is free software; you can redistribute
+% 	it and/or modify it under the terms of the GNU Lesser General Public
+% 	License as published by the Free Software Foundation; either
+% 	version 2.1 of the License, or any later version.
+% 
+% 	This program is distributed in the hope that it will be useful,
+% 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+% 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+% 	Lesser General Public License for more details.
+%
+%	Contact info: w3.ualg.pt/~jluis/mirone
+% --------------------------------------------------------------------
+
 if (nargin == 1 && ischar(varargin{1}))         % ... callbacks
     patchui(varargin{:});
     return
