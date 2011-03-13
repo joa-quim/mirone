c26b9ca5a460521a9191352d3a9e2339d2e3c214
j <j@127d096d-3646-0410-84c9-11b0184650a1>
2011-03-13 15:46:30 +0000

    Replace License notice to LGPL

--- trunk/utils/uirestore_fig.m
+++ trunk/utils/uirestore_fig.m
@@ -5,26 +5,39 @@ function uirestore_fig(uistate)
 %  what it was before it was suspended by a call to UISUSPEND_FIG.
 %  The input UISTATE is the structure returned by UISUSPEND_FIG which
 %  contains information about the figure's WindowButton* functions and the pointer
+
+%	Copyright (c) 2004-2011 by J. Luis
 %
-%  Joaquim Luis 25-01-07
+% 	This program is part of Mirone and is free software; you can redistribute
+% 	it and/or modify it under the terms of the GNU Lesser General Public
+% 	License as published by the Free Software Foundation; either
+% 	version 2.1 of the License, or any later version.
+% 
+% 	This program is distributed in the hope that it will be useful,
+% 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+% 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+% 	Lesser General Public License for more details.
+%
+%	Contact info: w3.ualg.pt/~jluis/mirone
+% --------------------------------------------------------------------
 
-fig = uistate.figureHandle;
+	fig = uistate.figureHandle;
 
-% No need to restore anything if the figure isn't there
-if (~ishandle(fig)),    return;     end
+	% No need to restore anything if the figure isn't there
+	if (~ishandle(fig)),	return,		end
 
-% No need to restore if the figure is being deleted
-if strcmp('on',get(fig,'BeingDeleted'))
-    return
-end
+	% No need to restore if the figure is being deleted
+	if strcmp('on',get(fig,'BeingDeleted'))
+		return
+	end
 
-set(fig, 'WindowButtonMotionFcn', uistate.WindowButtonMotionFcn)
-set(fig, 'WindowButtonDownFcn',   uistate.WindowButtonDownFcn)
-set(fig, 'WindowButtonUpFcn',     uistate.WindowButtonUpFcn);
-set(fig, 'KeyPressFcn',           uistate.KeyPressFcn)
-set(fig, 'Pointer',               uistate.Pointer)
-set(fig, 'PointerShapeCData',     uistate.PointerShapeCData)
-set(fig, 'PointerShapeHotSpot',   uistate.PointerShapeHotSpot)
+	set(fig, 'WindowButtonMotionFcn', uistate.WindowButtonMotionFcn)
+	set(fig, 'WindowButtonDownFcn',   uistate.WindowButtonDownFcn)
+	set(fig, 'WindowButtonUpFcn',     uistate.WindowButtonUpFcn);
+	set(fig, 'KeyPressFcn',           uistate.KeyPressFcn)
+	set(fig, 'Pointer',               uistate.Pointer)
+	set(fig, 'PointerShapeCData',     uistate.PointerShapeCData)
+	set(fig, 'PointerShapeHotSpot',   uistate.PointerShapeHotSpot)
 
 %     if (isfield(uistate,'docontext') && uistate.docontext)
 %         % Do not set an invalid handle or a handle that belongs to another figure. g239172
--- trunk/utils/url2image.m
+++ trunk/utils/url2image.m
@@ -107,7 +107,21 @@ function  varargout = url2image(opt, varargin)
 %
 %   AUTHOR
 %       Joaquim Luis (jluis@ualg.pt)   01-Feb-2008
+
+%	Copyright (c) 2004-2011 by J. Luis
 %
+% 	This program is part of Mirone and is free software; you can redistribute
+% 	it and/or modify it under the terms of the GNU Lesser General Public
+% 	License as published by the Free Software Foundation; either
+% 	version 2.1 of the License, or any later version.
+% 
+% 	This program is distributed in the hope that it will be useful,
+% 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+% 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+% 	Lesser General Public License for more details.
+%
+%	Contact info: w3.ualg.pt/~jluis/mirone
+% --------------------------------------------------------------------
 
 	quadkey = {'0' '1'; '2' '3'};				% Default to Virtual Earth
 	prefix = 'http://a0.ortho.tiles.virtualearth.net/tiles/a';
