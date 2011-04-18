04f5715a18a65309ade08aa16fc474cfa35bd298
j <j@127d096d-3646-0410-84c9-11b0184650a1>
2011-04-18 20:14:43 +0000

    Store number of layers in handles. Use this knowledge to call aquamoto when appropriate.

--- utils/read_grid.m
+++ utils/read_grid.m
@@ -41,6 +41,7 @@ function [Z, X, Y, srsWKT, handles, att] = read_grid(handles, fullname, tipo, op
 	if (strncmp(tipo,'GMT',3))		% GMT_relatives - Reading is done by the read_gmt_type_grids function
 		[handles, X, Y, Z, head, misc] = read_gmt_type_grids(handles, fname);
 		if (isempty(X))		return,		end
+		handles.nLayers = misc.z_dim(3);
 		if (~isempty(misc) && ~isempty(misc.srsWKT) ),		srsWKT = misc.srsWKT;	end
 	else
 		grdMaxSize = handles.grdMaxSize;
