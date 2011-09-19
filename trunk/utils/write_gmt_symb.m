a13bd6a6044e5e3bfd9a07451534d440a7a6028d
j <j@127d096d-3646-0410-84c9-11b0184650a1>
2011-09-19 23:25:46 +0000

    Draw patches in reverse order than that of fishing. This hopefully will respect stack order.

--- trunk/utils/write_gmt_symb.m
+++ trunk/utils/write_gmt_symb.m
@@ -1,4 +1,4 @@
-function writeGMTsymb(handles)
+function write_gmt_symb(handles)
 % ...
 
 	ALLlineHand = findobj(get(handles.axes1,'Child'),'Type','line');
@@ -10,53 +10,54 @@ function writeGMTsymb(handles)
 	fname = [PathName FileName];
 
 	fid = fopen(fname,'wt');
-	fprintf(fid,'#\n# Definition File created by Mirone Tech\n#\n')
+	fprintf(fid,'#\n# Definition File created by Mirone Tech\n#\n');
 
-% ------------- Search for closed polygons ----------------------------
-if (~isempty(ALLpatchHand))
-    xx = get(ALLpatchHand,'XData');     yy = get(ALLpatchHand,'YData');
-    n_patch = numel(ALLpatchHand);
-    LineStyle = get(ALLpatchHand,'LineStyle');
-    LineWidth = get(ALLpatchHand,'LineWidth');
-    if (iscell(LineWidth)),     LineWidth = cat(1,LineWidth{:});     end
-    EdgeColor = get(ALLpatchHand,'EdgeColor');
-    if (iscell(EdgeColor)),     EdgeColor = cat(1,EdgeColor{:});     end
-    FillColor = get(ALLpatchHand,'FaceColor');
-    if (iscell(FillColor))
-        resp = strmatch('none',char(FillColor{:}));
-        if (isempty(resp))
-            FillColor = cat(1,FillColor{:});
-        else
-            for (i=1:length(resp))                  % Signal down that this is a non colored polygon
-                FillColor{resp(i)} = [-1 -1 -1];    % FDS it worked. I wonder why FillColor{resp} = repmat([-1 -1 -1],length(resp),1); DOESN'T
-            end
-            FillColor = cat(1,FillColor{:});
-        end
-    else                % We have only one patch
-        xx = num2cell(xx,1);   yy = num2cell(yy,1);   % Make it a cell for reducing the head-hakes
-        resp = strmatch('none',FillColor);
-        if (~isempty(resp))
-            FillColor = [-1 -1 -1];                 % Signal down that this is a non colored polygon
-        end
-    end
+	% ------------- Search for closed polygons ----------------------------
+	if (~isempty(ALLpatchHand))
+		ALLpatchHand = ALLpatchHand(end:-1:1);	% Don't know if this always good but respects stack order
+		xx = get(ALLpatchHand,'XData');     yy = get(ALLpatchHand,'YData');
+		n_patch = numel(ALLpatchHand);
+		LineStyle = get(ALLpatchHand,'LineStyle');
+		LineWidth = get(ALLpatchHand,'LineWidth');
+		if (iscell(LineWidth)),     LineWidth = cat(1,LineWidth{:});     end
+		EdgeColor = get(ALLpatchHand,'EdgeColor');
+		if (iscell(EdgeColor)),     EdgeColor = cat(1,EdgeColor{:});     end
+		FillColor = get(ALLpatchHand,'FaceColor');
+		if (iscell(FillColor))
+			resp = strmatch('none',char(FillColor{:}));
+			if (isempty(resp))
+				FillColor = cat(1,FillColor{:});
+			else
+				for (i=1:length(resp))                  % Signal down that this is a non colored polygon
+					FillColor{resp(i)} = [-1 -1 -1];    % FDS it worked. I wonder why FillColor{resp} = repmat([-1 -1 -1],length(resp),1); DOESN'T
+				end
+				FillColor = cat(1,FillColor{:});
+			end
+		else                % We have only one patch
+			xx = num2cell(xx,1);   yy = num2cell(yy,1);   % Make it a cell for reducing the head-hakes
+			resp = strmatch('none',FillColor);
+			if (~isempty(resp))
+				FillColor = [-1 -1 -1];                 % Signal down that this is a non colored polygon
+			end
+		end
 
-    for (i = 1:n_patch)
-        cor_edge = round(EdgeColor(i,1:3) * 255);
-        cor_edge = [num2str(cor_edge(1)) '/' num2str(cor_edge(2)) '/' num2str(cor_edge(3))];
-        cor_fill = round(FillColor(i,1:3) * 255);
-        if (cor_fill(1) >= 0)       % Color filled polygon
-            cor_fill = [num2str(cor_fill(1)) '/' num2str(cor_fill(2)) '/' num2str(cor_fill(3))];
-            mlt_comm = [' -G' cor_fill ' -W' num2str(LineWidth(i)) 'p,' cor_edge];
-        else                        % No filling color
-            mlt_comm = [' -W' num2str(LineWidth(i)) 'p,' cor_edge];
-        end
+		for (i = 1:n_patch)
+			cor_edge = round(EdgeColor(i,1:3) * 255);
+			cor_edge = [num2str(cor_edge(1)) '/' num2str(cor_edge(2)) '/' num2str(cor_edge(3))];
+			cor_fill = round(FillColor(i,1:3) * 255);
+			if (cor_fill(1) >= 0)       % Color filled polygon
+				cor_fill = [num2str(cor_fill(1)) '/' num2str(cor_fill(2)) '/' num2str(cor_fill(3))];
+				mlt_comm = [' -G' cor_fill ' -W' num2str(LineWidth(i)) 'p,' cor_edge];
+			else                        % No filling color
+				mlt_comm = [' -W' num2str(LineWidth(i)) 'p,' cor_edge];
+			end
 
-		x = xx{i}(:)';			y = yy{i}(:)';			% Force row vecs
-		fprintf(fid,'%f\t%f\tM\t%s\n', x(1), y(1), mlt_comm);
-		fprintf(fid,'%f\t%f\tD\n', [x(2:end); y(2:end)]);
-		fprintf(fid,'\n#\n')
-    end
-end
+			x = xx{i}(:)';			y = yy{i}(:)';			% Force row vecs
+			fprintf(fid,'%f\t%f\tM\t%s\n', x(1), y(1), mlt_comm);
+			fprintf(fid,'%f\t%f\tD\n', [x(2:end); y(2:end)]);
+			fprintf(fid,'\n#\n');
+		end
+	end
 
 	% ------------- Search for lines or polylines ----------------------------
 	if (~isempty(ALLlineHand))      % 
