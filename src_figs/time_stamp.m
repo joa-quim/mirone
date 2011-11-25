2f44fb18db200ec273e096329de94746f8ddbd22
j <j@localhost>
2011-11-25 23:09:41 +0000

    Anticipate Octave - check the 'waitstatus' on either the ML or Octave way.

--- src_figs/time_stamp.m
+++ src_figs/time_stamp.m
@@ -20,6 +20,7 @@ function varargout = time_stamp(varargin)
 	time_stamp_LayoutFcn(hObject);
 	handles = guihandles(hObject);
 	move2side(hObject,'right')
+	handles.IAmOctave = (exist('OCTAVE_VERSION','builtin') ~= 0);	% To know if we are running under Octave
 
 	handles.command = cell(15,1);
 	handles.command{3} = '-U';
@@ -148,18 +149,18 @@ helpdlg(message,'Help on Time Stamp');
 
 % --- Executes when user attempts to close figure1.
 function figure1_CloseRequestFcn(hObject, evt)
-handles = guidata(hObject);
-if isequal(get(handles.figure1, 'waitstatus'), 'waiting')
-    % The GUI is still in UIWAIT, us UIRESUME
-    handles.output = '';        % User gave up, return nothing
-    guidata(hObject, handles);
-    uiresume(handles.figure1);
-else
-    % The GUI is no longer waiting, just close it
-    handles.output = '';        % User gave up, return nothing
-    guidata(hObject, handles);
-    delete(handles.figure1);
-end
+	handles = guidata(hObject);
+	if isequal(get(handles.figure1, 'waitstatus'), 'waiting')
+		% The GUI is still in UIWAIT, us UIRESUME
+		handles.output = '';        % User gave up, return nothing
+		guidata(hObject, handles);
+		uiresume(handles.figure1);
+	else
+		% The GUI is no longer waiting, just close it
+		handles.output = '';        % User gave up, return nothing
+		guidata(hObject, handles);
+		delete(handles.figure1);
+	end
 
 % --- Executes on key press over figure1 with no controls selected.
 function figure1_KeyPressFcn(hObject, evt)
