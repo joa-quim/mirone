0ca8db6f3676de59c28d21dba4d31949f7c4f824
j <j@127d096d-3646-0410-84c9-11b0184650a1>
2011-12-13 00:37:54 +0000

    Cosmetics

--- utils/axes2pix.m
+++ utils/axes2pix.m
@@ -10,16 +10,21 @@ function pixelx = axes2pix(dim, x, axesx)
 %   Copyright 1993-2002 The MathWorks, Inc.  
 %   $Revision: 5.12 $  $Date: 2002/03/15 15:57:01 $
 
-% Error checking on input arguments.
-error_str = nargchk(3, 3, nargin);
-if (~isempty(error_str)),   error('There must be 3 input arguments.');  end
-if (max(size(dim)) ~= 1),   error('First argument must be a scalar.');  end
-if (min(size(x)) > 1),      error('X must be a vector.');               end
+	% Error checking on input arguments.
+	error_str = nargchk(3, 3, nargin);
+	if (~isempty(error_str)),   error('There must be 3 input arguments.');  end
+	if (max(size(dim)) ~= 1),   error('First argument must be a scalar.');  end
+	if (min(size(x)) > 1),      error('X must be a vector.');               end
 
-xfirst = x(1);      xlast = x(max(size(x)));
+	xfirst = x(1);      xlast = x(max(size(x)));
 
-if (dim == 1),      pixelx = axesx - xfirst + 1;    return;     end
-xslope = (dim - 1) / (xlast - xfirst);
-if ((xslope == 1) && (xfirst == 1)),     pixelx = axesx;
-else    pixelx = xslope * (axesx - xfirst) + 1;
-end
+	if (dim == 1)
+		pixelx = axesx - xfirst + 1;
+		return
+	end
+	xslope = (dim - 1) / (xlast - xfirst);
+	if ((xslope == 1) && (xfirst == 1))
+		pixelx = axesx;
+	else
+		pixelx = xslope * (axesx - xfirst) + 1;
+	end
