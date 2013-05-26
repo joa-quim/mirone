38587902edbbb932293f2250c85c8da0daa72457
j <j@127d096d-3646-0410-84c9-11b0184650a1>
2013-05-26 23:19:32 +0000

    Add new tool to do mosaics out of the Portuguese LIDAR to be publicly released soon.

--- src_figs/cartas_militares.m
+++ src_figs/cartas_militares.m
@@ -8,7 +8,7 @@ function varargout = cartas_militares(varargin)
 % 
 % Thirth method should be prefered (more accurate, I believe)
 
-%	Copyright (c) 2004-2012 by J. Luis
+%	Copyright (c) 2004-2013 by J. Luis
 %
 % 	This program is part of Mirone and is free software; you can redistribute
 % 	it and/or modify it under the terms of the GNU Lesser General Public
@@ -37,6 +37,21 @@ function varargout = cartas_militares(varargin)
 	    path_data = [cd filesep 'data' filesep];
 	end
 
+	% Load the directory list stored in mirone_pref
+	load([path_data 'mirone_pref.mat'], 'directory_list');
+	set(handles.popup_directory_list,'String', directory_list)
+	handles.last_directories = directory_list;
+	handles.files_dir = handles.last_directories{1};
+
+	% ===== UGLY PATCH TO IMPLEMENT THE LIDAR PT MOOSAIC TOOL ====
+	if (numel(varargin) == 2)
+		lidarPT(handles);
+		set(handles.radio_inWeb,'Enable','off')
+		set(hObject,'Vis','on')
+		guidata(hObject, handles);
+		return
+	end
+
     naos = cell(19,1);				% By Columns
     naos{1} = [1:19 22:58];
     naos{2} = [1:19 28:58];
@@ -57,7 +72,7 @@ function varargout = cartas_militares(varargin)
     naos{17} = [1:47 57:58];
     naos{18} = [1:49 54:58];
     naos{19} = 1:58;
-	
+
     atlas_file = [path_data 'countries_dp5.bin'];
     paises.ct = country_select(atlas_file,'-Pportugal');
 	% Clean up the empty fields in the ct struct (given I could not do it at mex level)
@@ -107,7 +122,7 @@ function varargout = cartas_militares(varargin)
 	ABs{16,1}= [38 40 46];	ABs{16,2}= {'227A' '205A' '142A'};
 	ABs{18,1}= 49;			ABs{18,2}= {'108A'};
 	ABs{19,1}= 52;			ABs{19,2}= {'67A'};
-	
+
 	for (n=1:19)
 		xp = [(n-1) n n (n-1)] * 16000 + xMap_min; 
 		for (k = 1:numel(ABs{n,1}))
@@ -143,12 +158,6 @@ function varargout = cartas_militares(varargin)
 		'As for example at:\nhttp://www.civil.ist.utl.pt/~ruif/HRF/cartas/Cartas%%20Militares%%20GIF_25000/']);
 	set(handles.radio_inWeb,'Tooltip',msg)
 
-	% Load the directory list stored in mirone_pref
-	load([path_data 'mirone_pref.mat'], 'directory_list');
-	set(handles.popup_directory_list,'String', directory_list)
-	handles.last_directories = directory_list;
-	handles.files_dir = handles.last_directories{1};
-
 	% Initiliaze with this site. This also means that it doesn't recall changes between usage sessions
 	set(handles.edit_forWeb,'String', 'http://www.civil.ist.utl.pt/~ruif/HRF/cartas/Cartas%20Militares%20GIF_25000/')
 
@@ -156,8 +165,181 @@ function varargout = cartas_militares(varargin)
 	if (nargout),	varargout{1} = hObject;		end
 
 % -----------------------------------------------------------------------------------------
+function lidarPT(handles)
+% ...
+	load lidarPT.mat
+	x_min = double(mosaico(1,1));	x_max = double(mosaico(1,2));
+	y_min = double(mosaico(1,3));	y_max = double(mosaico(1,4));
+	x_inc = 1600;		y_inc = 1000;
+
+	nColsQ = (x_max - x_min) / x_inc + 1;		% Number of cells along X in our PT
+	X = x_min:x_inc:x_max;
+	Y = y_min:y_inc:y_max;
+	nCols = numel(X);
+	nRows = numel(Y);
+	sz =  nCols*nRows;
+
+	n_quadric = size(mosaico,1)-1;				% Number of actual sub-grids in the LIDAR2011 PT
+	ind(n_quadric) = int32(0);
+	cell_yes = false(nCols * (nRows-1),1);	% ...
+	for (k = 2:n_quadric+1)
+		col = round((double(mosaico(k,1)) - x_min) / x_inc) + 1;
+		row = round((double(mosaico(k,3)) - y_min) / y_inc ) + 1;
+		c = int32((row - 1) * nColsQ + col);
+		cell_yes(c) = true;
+		ind(k-1) = c;
+	end
+
+	[Y, X] = meshgrid (Y, X);
+	X = reshape(X, sz, 1); 
+	Y = reshape(Y, sz, 1);
+	vert = [X Y];
+	clear X Y
+	faces = zeros(nCols*(nRows-1),4);
+	j = 1;
+	for (i = 1:nCols * (nRows-1))
+		if (rem(i,nCols) ~= 0 && cell_yes(i))
+			faces(j,:) = [i, i+1, i+1+nCols, i+nCols];             
+			j = j + 1;
+		end
+	end
+	faces(j:end,:) = [];
+
+	x_lim = [x_min x_max] + [-35 30]*x_inc;
+	y_lim = [y_min y_max] + [-2 2]*y_inc;
+	set(handles.axes1,'XLim', x_lim, 'YLim', y_lim)
+	set(handles.axes1,'XTick',[],'YTick',[], 'DataAspectRatio', [1 1 1])
+
+	FV.vertices = vert;		FV.faces = faces;
+	h = patch(FV,'FaceColor','none','Parent',handles.axes1, 'Tag','LidarMatrix');
+	set(h,'ButtonDownFcn',{@bdnLidar,handles.figure1},'UserData',[x_lim y_lim x_inc y_inc])
+	setappdata(h, 'infoBB', [x_min x_max y_min y_max nColsQ])
+	setappdata(h, 'infoIND', ind)
+	setappdata(h, 'infoNOME', nomes)
+	sldT = 9;		% Slider thickness
+	H = 50;			% Distance from bottom of Fig
+	setSliders(handles, sldT, H);
+
+% -----------------------------------------------------------------------------------------
+function bdnLidar(obj,evt,hFig)
+% Create a red patch in the cell of tha main data matrix clicked by user
+	handles = guidata(hFig);
+	hQ = gcbo;
+	pt = get(handles.axes1, 'CurrentPoint');		% Current click in map coordinates
+	ud = get(hQ,'UserData');						% = [x_lim y_lim x_inc y_inc]
+	col = fix((pt(1,1) - ud(1)) / ud(5));			% col number starting at 0 from axes (image) origin
+	row = fix((pt(1,2) - ud(3)) / ud(6));			% rown ----"----
+	x1 = ud(1) + col * ud(5);	x2 = x1 + ud(5);	% coordinates of the clicked rectangle
+	y1 = ud(3) + row * ud(6);	y2 = y1 + ud(6);
+	xp = [x1 x1 x2 x2];			yp = [y1 y2 y2 y1];
+	h = patch('XData',xp,'YData',yp,'FaceColor','r','Parent',handles.axes1,'Tag','oneTile');
+
+	[info.nome, info.row_col] = findInnerLidarTiles(hQ, x1, y1);
+	set(h, 'UserData', info)
+	set(h,'ButtonDownFcn','delete(gco)')			% Kill me when I'm clicked
+
+% -----------------------------------------------------------------------------------------
+function [nome, row_col] = findInnerLidarTiles(hQ, x, y)
+% Get the grid name associated to the cell where X,Y falls and its ROW,COL address in the data matrix
+	nome = [];	row_col = [];
+	ud = get(hQ,'UserData');						% = [x_lim y_lim x_inc y_inc]
+	infoBB = getappdata(hQ, 'infoBB');				% infoBB holds the true data limits, [x_min x_max y_min y_max nColsQ]
+	col = round((x - infoBB(1)) / ud(5)) + 1;		% col number starting at 1 from data origin
+	row = round((y - infoBB(3)) / ud(6)) + 1;		% rown ----"----
+	ind = int32((row - 1) * infoBB(5) + col);		% row major linear index of clicked cell (one based)
+	infoIND = getappdata(hQ, 'infoIND');
+	infoNOME = getappdata(hQ, 'infoNOME');
+	k = find(infoIND == ind);						% Find the index of the 'ind' linear index
+	if (isempty(k)),	return,		end
+	nome = infoNOME{k+1};							% + 1 because first name is empty (the global BB)
+	row_col = [row col]; 
+
+% -----------------------------------------------------------------------------------------
+function push_lidarMosaico_CB(hObject, handles)
+% Check which tiles are selected and build a mosaic that embraces them all (even the inner non-selected ones)
+
+	hQ = findobj(handles.axes1,'Type','patch','Tag','LidarMatrix');
+	t = findobj(handles.axes1,'Type','patch','Tag','oneTile');
+	if (isempty(t)),	return,		end
+	row_min = 10000;	row_max = 0;
+	col_min = 10000;	col_max = 0;
+	z_min   = 10000;	z_max   = -1000;
+	for (k = 1:numel(t))						% Find our corners
+		ud = get(t(k), 'UserData');
+		row_min = min(row_min, ud.row_col(1));
+		row_max = max(row_max, ud.row_col(1));
+		col_min = min(col_min, ud.row_col(2));
+		col_max = max(col_max, ud.row_col(2));
+	end
+
+	infoBB = getappdata(hQ, 'infoBB');			% infoBB holds the true data limits, [x_min x_max y_min y_max nColsQ]
+
+	pato = get(handles.popup_directory_list,'String');
+	pato = pato{1};
+	if (pato(end) ~= filesep),		pato = [pato filesep];		end
+	%pato = 'C:\dat\LiDAR2011\laz\';
+
+	% Start filling our mosaic
+	nx = (col_max - col_min + 1) * 1600 / 2 + 1;% 2 is the grid step and + 1 because of grid registration
+	ny = (row_max - row_min + 1) * 1000 / 2 + 1;
+	Z = alloc_mex(ny,nx,'single',nan);
+	X0 = infoBB(1) + (col_min - 1) * 1600;
+	Y0 = infoBB(3) + (row_min - 1) * 1000;
+	got_one = false;
+	for (m = 1:(row_max - row_min + 1))
+		y = Y0 + (m - 1) * 1000 + 100;			% The 100 is just put the point well inside this cell
+		for (n = 1:(col_max - col_min + 1))
+			x = X0 + (n - 1) * 1600 + 100;
+			nome = findInnerLidarTiles(hQ, x, y);
+			if (isempty(nome)),		continue,	end
+			fname = [pato nome '-mis_orto.laz'];
+			if (exist(fname,'file') ~= 2)
+				fname = [pato nome '-mis_orto.xyz'];		% Try the XYZ version
+				if (exist(fname,'file') ~= 2)
+					fname = [pato nome '-mis_orto.asc'];	% Try the ASC version
+				end
+			end
+			if (exist(fname,'file') ~= 2),	continue,	end	% Should give a warning here
+			z = readLidarTile(fname);
+			got_one = true;
+			zzz = grdutils(z,'-L');		z_min = min(zzz(1), z_min);		z_max = max(zzz(2), z_max);
+			Z((m-1)*500+1:m*500+1, (n-1)*800+1:n*800+1) = z;
+		end
+	end
+
+	if (~got_one)
+		warndlg('Nickles de LIDAR grelhas no directório indicado. Bye Bye','Warning'),	return
+	end
+	X1 = infoBB(1) + col_max * 1600;
+	Y1 = infoBB(3) + row_max * 1000;
+	meta.X = linspace(X0, X1, nx);		meta.Y = linspace(Y0, Y1, ny);
+	meta.name = 'Mosaico LIDAR2011';	meta.geog = 0;
+	meta.head = [X0 X1 Y0 Y1 z_min z_max 0 2 2];
+	meta.srsWKT = ['PROJCS["ETRS89_Portugal_TM06",GEOGCS["GCS_ETRS89",DATUM["D_ETRS_1989",' ...
+			'SPHEROID["GRS_1980",6378137,298.257222101]],PRIMEM["Greenwich",0],UNIT["Degree",0.017453292519943295]],' ...
+			'PROJECTION["Transverse_Mercator"],PARAMETER["latitude_of_origin",39.66825833333333],' ...
+			'PARAMETER["central_meridian",-8.133108333333334],PARAMETER["scale_factor",1],'...
+			'PARAMETER["false_easting",0],PARAMETER["false_northing",0],UNIT["Meter",1]]'];
+	mirone(Z, meta)
+
+% -----------------------------------------------------------------------------------------
+function z = readLidarTile(fname)
+% Read a LIDAR_PT tile. Check if FNAME is a LASZ file
+	[PATO, NAME, EXT] = fileparts(fname);
+	if (strcmpi(EXT,'.laz'))
+		xyz = laszreader_mex(fname);
+		z = single(xyz(3,:));
+		clear xyz
+		z = reshape(z,801,501)';
+	else
+		z = gdalread(fname);
+	end
+	z(z == -999) = NaN;
+	z = flipud(z);
+
+% -----------------------------------------------------------------------------------------
 function bdnTile(obj,event,hFig)
-% Do what ever it must to get a referenced image
+% Do what ever it must to get a referenced 1:25000 image
 	handles = guidata(hFig);
 	ud = get(gcbo,'UserData');
 	proj = [];		knowLimits = false;
@@ -326,7 +508,7 @@ function [gcp, proj] = getProjFromOzi(fname)
 	elseif ( strncmp(map{9}( ind(1)+1:ind(2)-1 ),'(UTM)',5) )		% (UTM) Universal Transverse Mercator
 		proj = '+proj=utm +zone=29 +k=0.9996 +ellps=intl +towgs84=-85.858,-108.681,-120.361,0,0,0,0';
 	end
-	
+
 % -----------------------------------------------------------------------------------------
 function trans = AffineTransform(uv,xy)
 % For an affine transformation:
@@ -352,28 +534,92 @@ function trans = AffineTransform(uv,xy)
 	if rank(X) >= K
 		Tinv = X \ U;
 	else
-		msg = 'At least %d non-collinear points needed to infer %s transform.';
-		errordlg(sprintf(msg,K,'affine'),'Error');
+		errordlg(sprintf('At least %d non-collinear points needed to infer %s transform.',K,'affine'),'Error');
 	end
 
 	Tinv(:,3) = [0 0 1]';       % add third column
 	trans = inv(Tinv);
 	trans(:,3) = [0 0 1]';
 
+%--------------------------------------------------------------------------
+function setSliders(handles, sldT, H)
+% Create a pair of sliders and register them to use in 'imscroll_j'
+
+	uBak = get(handles.axes1, 'Units');		set(handles.axes1, 'Units', 'pixels')
+	axPos = get(handles.axes1,'Pos');		set(handles.axes1, 'Units', uBak)
+
+    hSliders = getappdata(handles.axes1,'SliderAxes');
+	if (isempty(hSliders))
+		sliderVer = uicontrol('Units','pixels','Style','slider','Parent',handles.figure1, 'Visible','on',...
+            'Pos',[axPos(1)+axPos(3)-sldT axPos(2) sldT axPos(4)+1],'Background',[.9 .9 .9]);
+		sliderHor = uicontrol('Units','pixels','Style','slider','Parent',handles.figure1, 'Visible','on',...
+            'Pos',[axPos(1) H axPos(3)+1 sldT],'Background',[.95 .95 .95]);
+		set(sliderHor,'Min',0,'Max',1,'Value',0,'Tag','HOR','Callback',{@slider_Cb,handles.axes1,'SetSliderHor'})
+		set(sliderVer,'Min',0,'Max',1,'Value',0,'Tag','VER','Callback',{@slider_Cb,handles.axes1,'SetSliderVer'})
+		% Register the sliders in the axe's appdata
+		drawnow			% Needed because otherwise - BUG BUG - next line would change the VER slider thickness !!!!!
+		setappdata(handles.axes1,'SliderAxes',[sliderHor sliderVer])
+		imscroll_j(handles.axes1,'ZoomSetSliders')              % ...
+	else			% We have them already. They just need to be updated
+        set(hSliders(1), 'Pos',[axPos(1)+axPos(3)+1 axPos(2) sldT axPos(4)+1],'Vis','off')
+        set(hSliders(2), 'Pos',[axPos(1) H-1 axPos(3)+1 sldT],'Vis','off')
+	end
+
+% -----------------------------------------------------------------------------------------
+function slider_Cb(obj,evt,ax,opt)
+	imscroll_j(ax,opt)
+
+% -----------------------------------------------------------------------------
+function figure1_KeyPressFcn(hObj, event)
+	handles = guidata(hObj);
+	if isequal(get(hObj,'CurrentCharacter'),'+')
+		zoom_j(hObj,2,[]);
+	elseif isequal(get(hObj,'CurrentCharacter'),'-')
+		zoom_j(hObj,0.5,[]);
+	end
+	hSliders = getappdata(handles.axes1,'SliderAxes');
+	if (~isempty(hSliders) && strcmp( get(hSliders(1),'Vis'),'on' ) )	% If (1) is visible so is the other
+		CK = get(hObj,'CurrentKey');
+		if (strcmp(CK,'rightarrow') || strcmp(CK,'leftarrow'))
+			SS = get(hSliders(1),'SliderStep');			val = get(hSliders(1),'Value');
+			if (CK(1) == 'r'),		newVal = min(val + SS(1), 1);	% I know that imscroll_j sliders are [0 1]
+			else					newVal = max(0, val - SS(1));
+			end
+			set(hSliders(1),'Value', newVal)
+			imscroll_j(handles.axes1,'SetSliderHor')
+		elseif (strcmp(CK,'uparrow') || strcmp(CK,'downarrow'))
+			SS = get(hSliders(2),'SliderStep');			val = get(hSliders(2),'Value');
+			if (CK(1) == 'u'),		newVal = min(val + SS(1), 1);	% I know that imscroll_j sliders are [0 1]
+			else					newVal = max(0, val - SS(1));
+			end
+			set(hSliders(2),'Value', newVal)
+			imscroll_j(handles.axes1,'SetSliderVer')
+		end
+	end
 
 % --- Creates and returns a handle to the GUI figure. 
 function cartas_militares_LayoutFcn(h1)
+
+	rootUnits = get(0, 'Units');			set(0, 'Units', 'pixels');
+	screenSize = get(0, 'ScreenSize');      set(0, 'Units', rootUnits);
+	FigHeight = screenSize(4) - 65;			FigWidth = round(FigHeight * 0.5);
+	AxHeight  = FigHeight - 60;
+	pos = [520 113 FigWidth FigHeight];
+
 set(h1,...
 'Color',get(0,'factoryUicontrolBackgroundColor'),...
+'KeyPressFcn',@figure1_KeyPressFcn,...
 'DoubleBuffer','on',...
 'MenuBar','none',...
 'Name','Cartas Militares',...
 'NumberTitle','off',...
-'Pos',[520 113 331 670],...
+'Pos',pos,...
 'RendererMode','manual',...
 'Resize','off',...
 'HandleVisibility','Call',...
 'Tag','figure1');
+
+	pos = [0 60 FigWidth AxHeight];
 
 axes('Parent',h1,...
 'Units','pixels',...
@@ -381,13 +627,13 @@ axes('Parent',h1,...
 'CameraPosition',[0.5 0.5 9.16025403784439],...
 'CameraPositionMode',get(0,'defaultaxesCameraPositionMode'),...
 'Color',get(0,'defaultaxesColor'),...
-'Pos',[0 50 331 622],...
+'Pos',pos,...
 'Tag','axes1');
 
 uicontrol('Parent',h1,...
 'BackgroundColor',[1 1 1],...
 'HorizontalAlignment','left',...
-'Pos',[3 3 325 22],...
+'Pos',[3 3 FigWidth-40 22],...
 'Style','edit',...
 'Tooltip','Enter web site address where we can find the image files',...
 'Tag','edit_forWeb',...
@@ -396,7 +642,7 @@ uicontrol('Parent',h1,...
 uicontrol('Parent',h1,...
 'BackgroundColor',[1 1 1],...
 'Call',@main_uiCB,...
-'Pos',[10 3 300 22],...
+'Pos',[10 3 FigWidth-40 22],...
 'Style','popupmenu',...
 'String', {' '},...
 'Tooltip','Select the directory where the image files reside',...
@@ -407,29 +653,34 @@ uicontrol('Parent',h1,...
 'Call',@main_uiCB,...
 'FontSize',10,...
 'FontWeight','bold',...
-'Pos',[309 3 21 23],...
+'Pos',[FigWidth+10-41 3 21 23],...
 'String','...',...
 'Tooltip','Select a different directory',...
 'Tag','push_change_dir');
 
-uicontrol('Parent',h1,...
+uicontrol('Parent',h1, 'Pos',[10 30 61 15],...
 'Call',@main_uiCB,...
 'FontName','Helvetica',...
-'Pos',[10 30 61 15],...
 'String','In loco',...
 'Style','radiobutton',...
 'Tooltip','When you have the files on disk ',...
 'Value',1,...
 'Tag','radio_inloco');
 
-uicontrol('Parent',h1,...
+uicontrol('Parent',h1, 'Pos',[130 30 71 15],...
 'Call',@main_uiCB,...
 'FontName','Helvetica',...
-'Pos',[130 30 71 15],...
 'String','In Web',...
 'Style','radiobutton',...
-'Tooltip','When you have get the files from Web',...
+'Tooltip','When you want to get the files from Web',...
 'Tag','radio_inWeb');
+
+uicontrol('Parent',h1, 'Position',[310 26 100 21],...
+'Call',@main_uiCB,...
+'FontSize',10,...
+'FontWeight','bold',...
+'String','Faz Mosaico',...
+'Tag','push_lidarMosaico');
 
 function main_uiCB(hObject, eventdata)
 % This function is executed by the callback and than the handles is allways updated.
