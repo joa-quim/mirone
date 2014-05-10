fc935f2bec5cd18da8a9cc60e25aec705e8f9689
j <j@127d096d-3646-0410-84c9-11b0184650a1>
2014-05-10 02:18:22 +0000

    Cosmetics

--- trunk/utils/projection_menu.m
+++ trunk/utils/projection_menu.m
@@ -11,7 +11,7 @@ function projection_menu(hFig, hProj, home_dir)
 %		hFig		The Mirone handles
 %		hProj		A GMT -J type projection info (used by grdroject)
 
-%	Copyright (c) 2004-2012 by J. Luis
+%	Copyright (c) 2004-2014 by J. Luis
 %
 % 	This program is part of Mirone and is free software; you can redistribute
 % 	it and/or modify it under the terms of the GNU Lesser General Public
@@ -26,20 +26,21 @@ function projection_menu(hFig, hProj, home_dir)
 %	Contact info: w3.ualg.pt/~jluis/mirone
 % --------------------------------------------------------------------
 
+% $Id$
+
 	if (nargin == 2)
 		setPRJ([],[], hFig, 1, hProj)
 		return
 	end
 
-	fs = filesep;
-	fid = fopen([home_dir fs 'data' fs 'SRSproj_def.txt']);
+	fid = fopen([home_dir filesep 'data' filesep 'SRSproj_def.txt']);
 	if (fid < 0),	return,		end
 
 	c = (fread(fid,'*char'))';      fclose(fid);
 	menus = strread(c,'%s','delimiter','\n');   clear c fid;
 	% Remove eventual empty lines
 	m = numel(menus);    c = false(m,1);
-	for (k=1:m)
+	for (k = 1:m)
 		if (isempty(menus{k})),     c(k) = true;    end
 	end
 	menus(c) = [];
@@ -49,9 +50,9 @@ function projection_menu(hFig, hProj, home_dir)
 	mainMenu = cell(m,1);
 	subMenu = cell(m,1);
 	projStr = cell(m,1);
-	for (k=1:m)
+	for (k = 1:m)
+		if (menus{k}(1) == '#'),	c(k) = true;	continue,	end
 		[t,r] = getMenuLabel(menus{k});
-		if (t(1) == '#'),  c(k) = true;  continue;   end
 		mainMenu{k} = t;
 		[t,r] = getMenuLabel(r);
 		if (numel(t) == 1),     subMenu{k} = [];    % We don't have a submenu
@@ -64,11 +65,11 @@ function projection_menu(hFig, hProj, home_dir)
 
 	% Detect repeated main menus which occur when we have subMenus
 	primos = true(m,1);
-	for (k=2:m),    primos(k) = ~strcmp(mainMenu{k}, mainMenu{k-1});    end
-    
+	for (k = 2:m),		primos(k) = ~strcmp(mainMenu{k}, mainMenu{k-1});    end
+
 	% Parse the projStr string and assign it either to a GMT type -J proj or proj4 string
 	projCOMM = cell(m,3);       % 3 for -J -C -T in case of a GMT proj 
-	for (k=1:m)
+	for (k = 1:m)
 		if (isempty(projStr{k})),	continue,	end      % The 'None' line
 
 		if (projStr{k}(1) == '+')   % We have a Proj4 string
@@ -94,29 +95,29 @@ function projection_menu(hFig, hProj, home_dir)
 				projCOMM{k,2+j} = t;
 				j = j + 1;
 			end
-        else
+		else
 			projCOMM{k,2} = '-C';    % A must have
 		end
 	end
 
-    hMain = zeros(1,m);    hSec  = zeros(1,m);
-    for (k=1:m)
-        if (isempty(subMenu{k}))    % No subMenu, we have than a direct projection setting
-            hMain(k) = uimenu('Parent',hProj,'Label',mainMenu{k},'Call',{@setPRJ,hFig,k,projCOMM});
-        else
-            if (primos(k))          % Parent of a Submenu. No proj string settings
-                hMain(k) = uimenu('Parent',hProj,'Label',mainMenu{k});
-                hUI = hMain(k);     % Make a copy to use on SubMenus
-                hSec(k) = uimenu('Parent',hUI,'Label',subMenu{k},'Call',{@setPRJ,hFig,k,projCOMM});
-            else                    % Child of a Parent with a submenu. We have a proj string to set
-                hSec(k) = uimenu('Parent',hUI,'Label',subMenu{k},'Call',{@setPRJ,hFig,k,projCOMM});
-            end
-        end
-    end
+	hMain = zeros(1,m);    hSec  = zeros(1,m);
+	for (k = 1:m)
+		if (isempty(subMenu{k}))    % No subMenu, we have than a direct projection setting
+			hMain(k) = uimenu('Parent',hProj,'Label',mainMenu{k},'Call',{@setPRJ,hFig,k,projCOMM});
+		else
+			if (primos(k))          % Parent of a Submenu. No proj string settings
+				hMain(k) = uimenu('Parent',hProj,'Label',mainMenu{k});
+				hUI = hMain(k);     % Make a copy to use on SubMenus
+				hSec(k) = uimenu('Parent',hUI,'Label',subMenu{k},'Call',{@setPRJ,hFig,k,projCOMM});
+			else                    % Child of a Parent with a submenu. We have a proj string to set
+				hSec(k) = uimenu('Parent',hUI,'Label',subMenu{k},'Call',{@setPRJ,hFig,k,projCOMM});
+			end
+		end
+	end
 
 	hMain(hMain == 0) = [];
 	hSec(hSec == 0) = [];
-    
+
 	set(hMain(1), 'checked', 'on')
 	setappdata(hFig, 'ProjList', [hMain hSec])
 
