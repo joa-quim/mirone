ddafa5b953bb311d786560bbfb82d0b01d7181ce
j <j@127d096d-3646-0410-84c9-11b0184650a1>
2014-05-10 15:35:19 +0000

    Let the custum_menus be fished from the OPTcontrol.txt file

--- trunk/utils/custom_menus.m
+++ trunk/utils/custom_menus.m
@@ -1,11 +1,11 @@
 function custom_menus(hFig, path_data)
-% Creates Menus from data read from the 'Custom_menu_def.txt' file
+% Creates Menus from data read from the 'OPTcontrol.txt' file under the MIR_CUSTOM_MENU keyword
 %
 %	This functions is called by mirone at each Mirone figure creation time. Than:
 %		hFig		handle of the mirone fig
 %		path_data	Mirone data dir
 %
-%	The Custom_menu_def.txt has a form of this type (example)
+%	The OPTcontrol.txt, under the MIR_CUSTOM_MENU keyword, has a form of this type (example)
 %		Geography,Basins[/sub],dir
 
 %	Copyright (c) 2004-2014 by J. Luis
@@ -25,30 +25,31 @@ function custom_menus(hFig, path_data)
 
 % $Id$
 
-	fname = [path_data 'Custom_menu_def.txt'];
+	fname = [path_data 'OPTcontrol.txt'];
 	if (exist(fname,'file') ~= 2),	return,		end
 	fid = fopen(fname);
 	if (fid < 0),	return,		end
 
 	c = (fread(fid,'*char'))';      fclose(fid);
-	menus = strread(c,'%s','delimiter','\n');   clear c fid;
-	% Remove eventual empty lines
-	m = numel(menus);    c = false(m,1);
-	for (k = 1:m)
-		if (isempty(menus{k})),     c(k) = true;    end
-	end
-	menus(c) = [];
+	lines = strread(c,'%s','delimiter','\n');   clear c fid;
 
-	%Geography,Basins[/sub],dir
+	n = 1;		menus = cell(10,1);
+	for (k = 1:numel(lines))
+		if (isempty(lines{k}) || lines{k}(1) == '#' || ~strncmp(lines{k},'MIR_CUSTOM_MENU',15))
+			continue
+		end
+		menus{n} = ddewhite(lines{k}(17:end));
+		n = n + 1;
+	end
+	menus(n:end) = [];
+
+	% Example: Geography,Basins[/sub],dir
 
 	% Parse the file contents
-	m = numel(menus);    c = false(m,1);
-	mainMenu = cell(m,1);
-	subMenu1  = cell(m,1);
-	subMenu2 = cell(m,1);
-	dataDir  = cell(m,1);
+	m = numel(menus);		c = false(m,1);
+	mainMenu = cell(m,1);	subMenu1 = cell(m,1);
+	subMenu2 = cell(m,1);	dataDir  = cell(m,1);
 	for (k = 1:m)
-		if (menus{k}(1) == '#'),	c(k) = true;	continue,	end
 		ind = strfind(menus{k}, ',');
 		mainMenu{k} = menus{k}(1:ind(1)-1);
 		subMenu1{k}  = menus{k}(ind(1)+1:ind(2)-1);	% This one must further be parsed for Basins[/sub]
@@ -58,27 +59,27 @@ function custom_menus(hFig, path_data)
 			subMenu1{k}(inds(1):end) = [];
 		end
 		dataDir{k} = menus{k}(ind(2)+1:end);
+		if (exist(dataDir{k},'dir') ~= 7),		c(k) = true;	end		% Dir does not exist. Flag for killing
 	end
-	mainMenu(c) = [];	subMenu1(c) = [];	subMenu2(c) = [];	dataDir(c) = [];   % Remove comment lines
-	m = numel(mainMenu);			% Update counting
+	mainMenu(c) = [];	subMenu1(c) = [];	subMenu2(c) = [];	dataDir(c) = [];	% Delete those of non-existing dirs
+	m = numel(mainMenu);
 
-	hSbub1 = zeros(1,m);    hSbub2  = zeros(1,m);
-	n = 1;
+	hSbub2  = zeros(1,m);
 	for (k = 1:m)
 		hThis = findobj(hFig,'Tag',mainMenu{k});
 		if (isempty(hThis))
 			% Make a new one on the main toolbar. Allow it?
 		end
-		
-		if (~hSbub1(n)),	hSbub1(n) = uimenu('Parent',hThis,'Label',['Custom -> ' subMenu1{n}]);	end
-		if (k == 1),		set(hSbub1(1), 'Sep','on'),		end		% Only first has Separator
-		if (k > 1 && ~strcmp(subMenu1{k}, subMenu1{k-1}))
-			n = n + 1;
+
+		if (k == 1)
+			hSbub1 = uimenu('Parent',hThis,'Label',['Custom -> ' subMenu1{k}], 'Sep','on');
+		elseif (~strcmp(subMenu1{k}, subMenu1{k-1}))		% MUST DO A BETTER ('GLOBAL') REPETITION SEARCHING
+			hSbub1 = uimenu('Parent',hThis,'Label',['Custom -> ' subMenu1{k}]);
 		end
 		if (isempty(subMenu2{k}))
-			set(hSbub1(k), 'Call',['hand=guidata(gcf);hand.last_dir=''',dataDir{k},''';mirone(''TransferB_CB'',hand,''guessType'')'])
+			set(hSbub1, 'Call',['hand=guidata(gcf);hand.last_dir=''',dataDir{k},''';mirone(''TransferB_CB'',hand,''guessType'')'])
 		else
-			hSbub2(k) = uimenu('Parent',hSbub1(n),'Label',subMenu2{k}, 'Call', ...
+			hSbub2(k) = uimenu('Parent',hSbub1,'Label',subMenu2{k}, 'Call', ...
 				['hand=guidata(gcf);hand.last_dir=''',dataDir{k},''';mirone(''TransferB_CB'',hand,''guessType'')']);
 		end
 	end
