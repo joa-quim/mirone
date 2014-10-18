bdbc1f5cf510d365849406fe9635f1fb4f5e3654
j <j@localhost>
2014-10-18 20:41:48 +0000

    Another (the same) typo

--- src_figs/mesher_helper.m
+++ src_figs/mesher_helper.m
@@ -198,7 +198,7 @@ function push_dataFile_CB(hObject, handles, opt)
 		end
 		if (isa(bin,'struct') || bin ~= 0)		% A binary file. For now I wont test any further
 			set(handles.check_isBinary, 'Val',1, 'Vis','on')
-			set([handle.radio_single handles.radio_double], 'Vis','on')			
+			set([handles.radio_single handles.radio_double], 'Vis','on')			
 			handles.is_binary(pos) = true;
 			update_conf(handles, 'is_binary', true)
 		elseif (n_column <= 2)
