542646a0da8dae36c62214e696a81caaac791726
j <j@localhost>
2015-07-20 14:56:47 +0000

    Renming of the GMTs transition functions

---
+++ utils/c_grdproject.m
@@ -0,0 +1,27 @@
+function [Zout, head] = c_grdproject(Zin, head, varargin)
+% Temporary function to easy up transition from GMT4 to GMT5.2
+
+% $Id$
+
+	global gmt_ver
+	
+	if (gmt_ver == 4)
+		if (nargout == 1)
+			Zout = grdproject_m(Zin, head, varargin{:});
+		else
+			[Zout, head] = grdproject_m(Zin, head, varargin{:});
+		end
+	else
+		G = fill_grid_struct(Zin, head);
+		cmd = 'grdproject';
+		for (k = 1:numel(varargin))
+			cmd = sprintf('%s %s', cmd, varargin{k});
+		end
+		gmtmex('create')
+		Z = gmtmex(cmd, G);
+		Zout = Z.z;
+		gmtmex('destroy')
+		if (nargout == 2)
+			head = Z.hdr;
+		end
+	end
---
+++ utils/c_grdread.m
@@ -0,0 +1,19 @@
+function [X, Y, Z, head] = c_grdread(fname, varargin)
+% Temporary function to easy up transition from GMT4 to GMT5.2
+
+% $Id$
+
+	global gmt_ver
+	
+	if (gmt_ver == 4)
+		[X, Y, Z, head] = grdread_m(fname, 'single', varargin{:});
+	else
+		gmtmex('create')
+		Zout = gmtmex(['read -Tg' fname]);
+		X = Zout.x;
+		Y = Zout.y;
+		Z = Zout.z;
+		head = Zout.hdr;
+		gmtmex('destroy')
+		head(10) = 0;		% Old grdread_m kept trace of data type on tenth element to preserve int16 types on output
+	end
