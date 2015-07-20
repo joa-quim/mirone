f36e6fa39dc0059fc9d03b1da21c92aacbfdd860
j <j@127d096d-3646-0410-84c9-11b0184650a1>
2015-07-20 13:28:30 +0000

    Functions to make the transition GMT4 -> GMT5.2 the smoothest possible.

---
+++ utils/_grdsample.m
@@ -0,0 +1,22 @@
+function Zout = _grdsample(Zin, head, varargin)
+% Temporary function to easy up transition from GMT4 to GMT5.2
+
+% O tsu_funs still calls grdsample directly as a system call
+
+% $Id$
+
+	global gmt_ver
+	
+	if (gmt_ver == 4)
+		Zout = grdsample_m(Zin, head, varargin{:});
+	else
+		G = fill_grid_struct(Zin, head);
+		cmd = 'grdsample';
+		for (k = 1:numel(varargin))
+			cmd = sprintf('%s %s', cmd, varargin{k});
+		end
+		gmtmex('create')
+		Zout = gmtmex(cmd, G);
+		Zout = Zout.z;
+		gmtmex('destroy')
+	end
---
+++ utils/_shoredump.m
@@ -0,0 +1,23 @@
+function out = _shoredump(varargin)
+% Temporary function to easy up transition from GMT4 to GMT5.2
+
+% $Id$
+
+	global gmt_ver
+	
+	if (gmt_ver == 4)
+		out = shoredump(varargin{:});
+	else
+		cmd = 'pscoast';
+		no_W = false;
+		for (k = 1:numel(varargin))
+			cmd = sprintf('%s %s', cmd, varargin{k});
+			if (varargin{k}(2) == 'N' || varargin{k}(2) == 'I')
+				no_W = true;
+			end
+		end
+		if (~no_W),		cmd = [cmd ' -W'];	end		% If no Rivers or Borders, than Coastlines (-W) 
+		gmtmex('create')
+		out = gmtmex(cmd, data);
+		gmtmex('destroy')
+	end
