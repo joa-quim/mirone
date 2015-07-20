43a53918e11b575647398e5ee6edcb33b2cdf2c9
j <j@127d096d-3646-0410-84c9-11b0184650a1>
2015-07-20 14:56:47 +0000

    Renming of the GMTs transition functions

---
+++ utils/c_grdlandmask.m
@@ -0,0 +1,28 @@
+function [mask, head, X, Y] = c_grdlandmask(varargin)
+% Temporary function to easy up transition from GMT4 to GMT5.2
+
+% $Id$
+
+	global gmt_ver
+	
+	if (gmt_ver == 4)
+		if (nargout == 1)
+			mask = grdlandmask_m(varargin{:});
+		elseif (nargout == 4)
+			[mask, head, X, Y] = grdlandmask_m(varargin{:});
+		else
+			error('Wrong number of output args')
+		end
+	else
+		cmd = 'grdlandmask';
+		for (k = 1:numel(varargin))
+			cmd = sprintf('%s %s', cmd, varargin{k});
+		end
+		gmtmex('create')
+		mask = gmtmex(cmd);
+		gmtmex('destroy')
+		if (nargout == 4)
+			head = mask.hdr;	X = mask.x;		Y = mask.y;
+		end
+		mask = mask.z;
+	end
