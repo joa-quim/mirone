c1f3528f5db31a604b48c9a04167bd687fe49f57
Joaquim Luis <jmfluis@gmail.com>
2015-10-04 12:10:18 +0100

    Move prototypes declaration to the header file

--- mex/mexnc4/netcdf4.c
+++ mex/mexnc4/netcdf4.c
@@ -21,13 +21,6 @@
 # include "mexnc.h"
 # include "mwsize.h"
 
-void   handle_nc_def_var_chunking ( int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[], op *opcode );
-void   handle_nc_def_var_fill     ( int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[], op *opcode );
-void   handle_nc_inq_var_chunking ( int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[], op *opcode );
-void   handle_nc_def_var_deflate ( int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[], op *opcode );
-void   handle_nc_inq_format      ( int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[], op *opcode );
-void   handle_nc_inq_var_deflate ( int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[], op *opcode );
-
 /*
  * Let each function use this over and over again.
  * */
