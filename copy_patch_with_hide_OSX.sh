aae364b7028618f76d9ae1148df873e743196ee1
Joaquim Luis <jmfluis@gmail.com>
2016-04-28 22:36:46 +0100

    Updates due to new versions names.

--- copy_patch_with_hide_OSX.sh
+++ copy_patch_with_hide_OSX.sh
@@ -18,41 +18,43 @@
 gmt_loc=/Users/j/programs/gmt5/lib
 
 # HDF5
-cp /usr/local/Cellar/hdf5/1.8.15/lib/*.10.dylib .
+cp /usr/local/opt/hdf5/lib/*.10.dylib .
 chmod +w *.dylib
 
+# Commented the _cpp ones because they are aparently not needed and are now 11 instead of 10
+
 install_name_tool -id libhdf5_hide.dylib libhdf5.10.dylib
-install_name_tool -id libhdf5_cpp_hide.dylib libhdf5_cpp.10.dylib
+#install_name_tool -id libhdf5_cpp_hide.dylib libhdf5_cpp.10.dylib
 install_name_tool -id libhdf5_hl_hide.dylib libhdf5_hl.10.dylib
-install_name_tool -id libhdf5_hl_cpp_hide.dylib libhdf5_hl_cpp.10.dylib
+#install_name_tool -id libhdf5_hl_cpp_hide.dylib libhdf5_hl_cpp.10.dylib
 
 # The order is important
-install_name_tool -change /usr/local/Cellar/hdf5/1.8.15/lib/libhdf5.10.dylib libhdf5_hide.dylib libhdf5_cpp.10.dylib
-mv libhdf5_cpp.10.dylib libhdf5_cpp_hide.dylib
-install_name_tool -change /usr/local/lib/libsz.2.dylib libsz.dylib libhdf5_cpp_hide.dylib
+#install_name_tool -change /usr/local/Cellar/hdf5/1.8.15/lib/libhdf5.10.dylib libhdf5_hide.dylib libhdf5_cpp.10.dylib
+#mv libhdf5_cpp.10.dylib libhdf5_cpp_hide.dylib
+#install_name_tool -change /usr/local/lib/libsz.2.dylib libsz.dylib libhdf5_cpp_hide.dylib
 
 mv libhdf5.10.dylib libhdf5_hide.dylib
-install_name_tool -change /usr/local/lib/libsz.2.dylib libsz.dylib libhdf5_hide.dylib
+install_name_tool -change /usr/local/opt/szip/lib/libsz.2.dylib libsz.dylib libhdf5_hide.dylib
 
-install_name_tool -change /usr/local/Cellar/hdf5/1.8.15/lib/libhdf5.10.dylib libhdf5_hide.dylib libhdf5_hl.10.dylib
+install_name_tool -change /usr/local/Cellar/hdf5/1.8.16_1/lib/libhdf5.10.dylib libhdf5_hide.dylib libhdf5_hl.10.dylib
 mv libhdf5_hl.10.dylib libhdf5_hl_hide.dylib
-install_name_tool -change /usr/local/lib/libsz.2.dylib libsz.dylib libhdf5_hl_hide.dylib
+install_name_tool -change /usr/local/opt/szip/lib/libsz.2.dylib libsz.dylib libhdf5_hl_hide.dylib
 
-install_name_tool -change /usr/local/Cellar/hdf5/1.8.15/lib/libhdf5.10.dylib libhdf5_hide.dylib         libhdf5_hl_cpp.10.dylib
-install_name_tool -change /usr/local/Cellar/hdf5/1.8.15/lib/libhdf5_cpp.10.dylib libhdf5_cpp_hide.dylib libhdf5_hl_cpp.10.dylib
-install_name_tool -change /usr/local/Cellar/hdf5/1.8.15/lib/libhdf5_hl.10.dylib libhdf5_hl_hide.dylib   libhdf5_hl_cpp.10.dylib
-mv libhdf5_hl_cpp.10.dylib libhdf5_hl_cpp_hide.dylib
-install_name_tool -change /usr/local/lib/libsz.2.dylib libsz.dylib libhdf5_hl_cpp_hide.dylib
+#install_name_tool -change /usr/local/Cellar/hdf5/1.8.15/lib/libhdf5.10.dylib libhdf5_hide.dylib         libhdf5_hl_cpp.10.dylib
+#install_name_tool -change /usr/local/Cellar/hdf5/1.8.15/lib/libhdf5_cpp.10.dylib libhdf5_cpp_hide.dylib libhdf5_hl_cpp.10.dylib
+#install_name_tool -change /usr/local/Cellar/hdf5/1.8.15/lib/libhdf5_hl.10.dylib libhdf5_hl_hide.dylib   libhdf5_hl_cpp.10.dylib
+#mv libhdf5_hl_cpp.10.dylib libhdf5_hl_cpp_hide.dylib
+#ßinstall_name_tool -change /usr/local/lib/libsz.2.dylib libsz.dylib libhdf5_hl_cpp_hide.dylib
 
 
 # NetCDF
 cp /usr/local/lib/libnetcdf.7.dylib .
 chmod +w libnetcdf.7.dylib
 install_name_tool -id libnetcdf_hide.dylib libnetcdf.7.dylib
-install_name_tool -change /usr/local/lib/libhdf5.10.dylib libhdf5_hide.dylib         libnetcdf.7.dylib
-install_name_tool -change /usr/local/lib/libhdf5_hl.10.dylib libhdf5_hl_hide.dylib   libnetcdf.7.dylib
+install_name_tool -change /usr/local/opt/hdf5/lib/libhdf5.10.dylib libhdf5_hide.dylib         libnetcdf.7.dylib
+install_name_tool -change /usr/local/opt/hdf5/lib/libhdf5_hl.10.dylib libhdf5_hl_hide.dylib   libnetcdf.7.dylib
 mv libnetcdf.7.dylib libnetcdf_hide.dylib
-install_name_tool -change /usr/local/lib/libsz.2.dylib libsz.dylib libnetcdf_hide.dylib
+install_name_tool -change /usr/local/opt/szip/lib/libsz.2.dylib libsz.dylib libnetcdf_hide.dylib
 
 # FITSIO
 cp /usr/local/lib/libcfitsio.2.3.37.dylib .
@@ -75,17 +77,19 @@ chmod +w libproj.9.dylib
 install_name_tool -id libproj_hide.dylib libproj.9.dylib
 mv libproj.9.dylib libproj_hide.dylib
 
-# LIBLWGEOM
-# Tried here to see if it solved the mystery of the compatibility versions, but doesn't. It still says
-# libproj_hide.dylib (compatibility version 10.0.0, current version 10.0.0)
-# But it should be 11 like the "libproj_hide.dylib" itself. So let this be done in copy_patch_simple.sh
-#cp /usr/local/opt/liblwgeom/lib/liblwgeom-2.1.5.dylib .
-#chmod +w liblwgeom-2.1.5.dylib
-#install_name_tool -id liblwgeom-2.dylib liblwgeom-2.1.5.dylib
-#install_name_tool -change /usr/local/lib/libgeos_c.1.dylib libgeos_c.dylib   liblwgeom-2.1.5.dylib
-#install_name_tool -change /usr/local/lib/libproj.9.dylib libproj_hide.dylib  liblwgeom-2.1.5.dylib
-#install_name_tool -change /usr/local/lib/libjson-c.2.dylib libjson-c.dylib   liblwgeom-2.1.5.dylib
-#mv liblwgeom-2.1.5.dylib liblwgeom-2.dylib
+# GEOS
+cp -f /usr/local/lib/libgeos_c.1.dylib .
+chmod +w libgeos_c.1.dylib
+install_name_tool -id libgeos_c_hide.dylib libgeos_c.1.dylib
+install_name_tool -change /usr/local/Cellar/geos/3.5.0/lib/libgeos-3.5.0.dylib libgeos-3.dylib libgeos_c.1.dylib
+mv libgeos_c.1.dylib libgeos_c_hide.dylib
+
+#XERCES
+cp /usr/local/lib/libxerces-c-3.1.dylib .
+chmod +w libxerces-c-3.1.dylib
+install_name_tool -id libxerces-c_hide.dylib libxerces-c-3.1.dylib
+install_name_tool -change /usr/local/opt/xerces-c/lib/libxerces-c-3.1.dylib libxerces-c-3.1.dylib libxerces-c.dylib 
+mv libxerces-c-3.1.dylib libxerces-c_hide.dylib
 
 
 # GDAL
@@ -95,37 +99,41 @@ install_name_tool -id libgdal.dylib libgdal.1.dylib
 install_name_tool -change /usr/local/lib/libjson-c.2.dylib     libjson-c.dylib         libgdal.1.dylib
 install_name_tool -change /usr/local/lib/libproj.9.dylib       libproj_hide.dylib      libgdal.1.dylib
 install_name_tool -change /usr/local/lib/libfreexl.1.dylib     libfreexl.dylib         libgdal.1.dylib
-install_name_tool -change /usr/local/lib/libgeos_c.1.dylib     libgeos_c.dylib         libgdal.1.dylib
-install_name_tool -change /usr/local/lib/libwebp.5.dylib       libwebp.dylib           libgdal.1.dylib
+install_name_tool -change /usr/local/opt/geos/lib/libgeos_c.1.dylib     libgeos_c_hide.dylib         libgdal.1.dylib
+install_name_tool -change /usr/local/opt/webp/lib/libwebp.6.dylib       libwebp.dylib           libgdal.1.dylib
 install_name_tool -change /usr/local/lib/libepsilon.1.dylib    libepsilon.dylib        libgdal.1.dylib
 install_name_tool -change /usr/local/lib/libodbc.2.dylib       libodbc.dylib           libgdal.1.dylib
 install_name_tool -change /usr/local/lib/libodbcinst.2.dylib   libodbcinst.dylib       libgdal.1.dylib
 install_name_tool -change /usr/local/lib/libjasper.1.dylib     libjasper.dylib         libgdal.1.dylib
-install_name_tool -change /usr/local/lib/libnetcdf.7.dylib     libnetcdf_hide.dylib    libgdal.1.dylib
-install_name_tool -change /usr/local/lib/libhdf5.10.dylib      libhdf5_hide.dylib      libgdal.1.dylib
+install_name_tool -change /usr/local/opt/netcdf/lib/libnetcdf.7.dylib     libnetcdf_hide.dylib    libgdal.1.dylib
+install_name_tool -change /usr/local/opt/hdf5/lib/libhdf5.10.dylib      libhdf5_hide.dylib      libgdal.1.dylib
 install_name_tool -change /usr/local/lib/libgif.4.dylib        libgif.dylib            libgdal.1.dylib
 install_name_tool -change /usr/local/lib/libjpeg.8.dylib       libjpeg.dylib           libgdal.1.dylib
 install_name_tool -change /usr/local/lib/libgeotiff.2.dylib    libgeotiff_hide.dylib   libgdal.1.dylib
 install_name_tool -change /usr/local/lib/libtiff.5.dylib       libtiff.dylib           libgdal.1.dylib
-install_name_tool -change /usr/local/lib/libpng16.16.dylib     libpng16.dylib          libgdal.1.dylib
+install_name_tool -change /usr/local/opt/libpng/lib/libpng16.16.dylib     libpng16.dylib          libgdal.1.dylib
 install_name_tool -change /usr/local/lib/libcfitsio.2.dylib    libcfitsio_hide.dylib   libgdal.1.dylib
 install_name_tool -change /usr/local/lib/liblzma.5.dylib       liblzma.dylib           libgdal.1.dylib
-install_name_tool -change /usr/local/lib/libdap.11.dylib       libdap.dylib            libgdal.1.dylib
-install_name_tool -change /usr/local/lib/libdapserver.7.dylib  libdapserver.dylib      libgdal.1.dylib
-install_name_tool -change /usr/local/lib/libdapclient.3.dylib  libdapclient.dylib      libgdal.1.dylib
-install_name_tool -change /usr/local/lib/libspatialite.7.dylib libspatialite.dylib     libgdal.1.dylib
+install_name_tool -change /usr/local/opt/libdap/lib/libdap.21.dylib       libdap.dylib            libgdal.1.dylib
+install_name_tool -change /usr/local/opt/libdap/lib/libdapserver.7.dylib  libdapserver.dylib      libgdal.1.dylib
+install_name_tool -change /usr/local/opt/libdap/lib/libdapclient.6.dylib  libdapclient.dylib      libgdal.1.dylib
+install_name_tool -change /usr/local/opt/libspatialite/lib/libspatialite.7.dylib libspatialite.dylib     libgdal.1.dylib
 install_name_tool -change /usr/local/opt/sqlite/lib/libsqlite3.0.dylib libsqlite3.dylib libgdal.1.dylib
-install_name_tool -change /usr/local/lib/libpcre.1.dylib       libpcre.dylib           libgdal.1.dylib
+install_name_tool -change /usr/local/opt/pcre/lib/libpcre.1.dylib       libpcre.dylib           libgdal.1.dylib
 install_name_tool -change /usr/local/opt/libxml2/lib/libxml2.2.dylib libxml2.dylib     libgdal.1.dylib
+install_name_tool -change /usr/local/opt/xerces-c/lib/libxerces-c-3.1.dylib libxerces-c_hide.dylib   libgdal.1.dylib
+install_name_tool -change /usr/local/opt/unixodbc/lib/libodbc.2.dylib libodbc.dylib             libgdal.1.dylib
+install_name_tool -change /usr/local/opt/unixodbc/lib/libodbcinst.2.dylib libodbcinst.dylib     libgdal.1.dylib
+
 mv libgdal.1.dylib libgdal.dylib
 
 # GMT
 cp ${gmt_loc}/libgmt.5.dylib .
 chmod +w libgmt.5.dylib
 install_name_tool -id libgmt.dylib libgmt.5.dylib
-install_name_tool -change /usr/local/lib/libnetcdf.7.dylib      libnetcdf_hide.dylib     libgmt.5.dylib
-install_name_tool -change /usr/local/lib/libpcre.1.dylib        libpcre.dylib            libgmt.5.dylib
-install_name_tool -change /usr/local/lib/libgdal.1.dylib        libgdal.dylib            libgmt.5.dylib
+install_name_tool -change /usr/local/opt/netcdf/lib/libnetcdf.7.dylib      libnetcdf_hide.dylib     libgmt.5.dylib
+install_name_tool -change /usr/local/opt/pcre/lib/libpcre.1.dylib        libpcre.dylib            libgmt.5.dylib
+install_name_tool -change /usr/local/opt/gdal/lib/libgdal.1.dylib        libgdal.dylib            libgmt.5.dylib
 install_name_tool -change /usr/local/lib/libfftw3f.3.dylib      libfftw3f.dylib          libgmt.5.dylib
 install_name_tool -change /usr/local/lib/libfftw3f_threads.3.dylib  libfftw3f_threads.dylib  libgmt.5.dylib
 install_name_tool -change ${gmt_loc}/libpostscriptlight.5.dylib libpostscriptlight.dylib libgmt.5.dylib
@@ -139,9 +147,9 @@ cp ${gmt_loc}/gmt/plugins/supplements.so supplements.dylib
 chmod +w supplements.dylib
 install_name_tool -change ${gmt_loc}/libgmt.5.dylib libgmt.dylib supplements.dylib
 install_name_tool -change ${gmt_loc}/libpostscriptlight.5.dylib    libpostscriptlight.dylib supplements.dylib
-install_name_tool -change /usr/local/lib/libnetcdf.7.dylib         libnetcdf_hide.dylib     supplements.dylib 
-install_name_tool -change /usr/local/lib/libgdal.1.dylib           libgdal.dylib            supplements.dylib
-install_name_tool -change /usr/local/lib/libpcre.1.dylib           libpcre.dylib            supplements.dylib 
+install_name_tool -change /usr/local/opt/netcdf/lib/libnetcdf.7.dylib      libnetcdf_hide.dylib     supplements.dylib 
+install_name_tool -change /usr/local/opt/gdal/lib/libgdal.1.dylib           libgdal.dylib            supplements.dylib
+install_name_tool -change /usr/local/opt/pcre/lib/libpcre.1.dylib          libpcre.dylib            supplements.dylib 
 install_name_tool -change /usr/local/lib/libfftw3f.3.dylib         libfftw3f.dylib          supplements.dylib
 install_name_tool -change /usr/local/lib/libfftw3f_threads.3.dylib libfftw3f_threads.dylib  supplements.dylib 
 
