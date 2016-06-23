da017c1716423e6fa566310d19a6d0c4b57e4360
j <j@localhost>
2016-06-23 00:22:14 +0000

    Check if -R is in the [0 360] interval

--- utils/c_shoredump.m
+++ utils/c_shoredump.m
@@ -11,8 +11,17 @@ function out = c_shoredump(varargin)
 	else
 		cmd = 'pscoast -M';
 		no_W = false;
+		maxLat = 0;
 		for (k = 1:numel(varargin))
-			cmd = sprintf('%s %s', cmd, varargin{k});
+			if (varargin{k}(2) == 'R')		% We must fck saddly check if -R spans on [0 360] and if yes acting accordingly.
+				ind = strfind(varargin{k}, '/');
+				maxLat = str2double(varargin{k}(ind(1)+1:ind(2)-1));
+			end
+			if (maxLat > 180)
+				cmd = sprintf('%s %s --FORMAT_GEO_OUT=+D', cmd, varargin{k});
+			else
+				cmd = sprintf('%s %s', cmd, varargin{k});
+			end
 			if (varargin{k}(2) == 'N' || varargin{k}(2) == 'I')
 				no_W = true;
 			end
