74bba6db1eb45672c77ed68df368e06d4c3dc763
Joaquim Luis <jmfluis@gmail.com>
2016-06-23 01:22:30 +0100

    Check if -R is in the [0 360] interval

--- utils/c_shoredump.m
+++ utils/c_shoredump.m
@@ -1,7 +1,7 @@
 function out = c_shoredump(varargin)
 % Temporary function to easy up transition from GMT4 to GMT5.2
 
-% $Id: c_shoredump.m 7780 2016-02-09 12:53:19Z j $
+% $Id: c_shoredump.m 7925 2016-06-23 00:22:14Z j $
 
 	global gmt_ver
 	if (isempty(gmt_ver)),		gmt_ver = 4;	end		% For example, if calls do not come via mirone.m
@@ -11,8 +11,17 @@ function out = c_shoredump(varargin)
 	else
 		cmd = 'pscoast -M';
 		no_W = false;
+		maxLat = 0;
 		for (k = 1:numel(varargin))
-			cmd = sprintf('%s %s', cmd, varargin{k});
+			if (varargin{k}(2) == 'R')		% We must fck saddly check if -R spans on [0 360] and if yes acting accordingly.
+				ind = strfind(varargin{k}, '/');
+				maxLat = str2double(varargin{k}(ind(1)+1:ind(2)-1));
+			end
+			if (maxLat > 180)
+				cmd = sprintf('%s %s --FORMAT_GEO_OUT=+D', cmd, varargin{k});
+			else
+				cmd = sprintf('%s %s', cmd, varargin{k});
+			end
 			if (varargin{k}(2) == 'N' || varargin{k}(2) == 'I')
 				no_W = true;
 			end
