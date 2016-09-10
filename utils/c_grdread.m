d3fe57de8597faf81b80b4db1f5e2670633bd559
j <j@localhost>
2016-09-10 01:28:20 +0000

    Update for latest changes in gmtmex

--- utils/c_grdread.m
+++ utils/c_grdread.m
@@ -1,9 +1,13 @@
-function [X, Y, Z, head] = c_grdread(fname, varargin)
+function [X, Y, Z, head, misc] = c_grdread(fname, varargin)
 % Temporary function to easy up transition from GMT4 to GMT5.2
+
+% MISC - (GMT5 only) - is a struct with:
+%		'desc', 'title', 'history', 'srsWKT', 'strPROJ4' fields
 
 % $Id$
 
 	global gmt_ver
+	misc = [];
 	if (isempty(gmt_ver)),		gmt_ver = 4;	end		% For example, if calls do not come via mirone.m
 	
 	if (gmt_ver == 4)
@@ -13,7 +17,7 @@ function [X, Y, Z, head] = c_grdread(fname, varargin)
 		X = Zout.x;
 		Y = Zout.y;
 		Z = Zout.z;
-		head = Zout.hdr;
+		head = [Zout.range Zout.registration Zout.inc 0];	% Old grdread_m kept trace of data type on 10th element to preserve int16 types
+		misc = struct('desc',Zout.comment,'title',Zout.title,'history',Zout.command,'srsWKT',Zout.wkt,'strPROJ4',Zout.proj4);
 		gmtmex('destroy')
-		head(10) = 0;		% Old grdread_m kept trace of data type on tenth element to preserve int16 types on output
 	end
