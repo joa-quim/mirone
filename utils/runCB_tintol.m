41cc710874e6f6a12184c48800a8cb78ceab9f22
j <j@localhost>
2016-09-28 22:07:17 +0000

    Hide the TINTOL menu when in TINTOL mode

--- utils/runCB_tintol.m
+++ utils/runCB_tintol.m
@@ -10,3 +10,9 @@ function runCB_tintol(handles)
 		handles.Seismology handles.GMT handles.GridTools], 'Vis', 'off')
 % 	set([handles.DrawGeogCirc handles.Anaglyph handles.MBplaning], 'Vis', 'off')	% Stupid compiler screws here
  	set([handles.Anaglyph handles.MBplaning], 'Vis', 'off')
+	
+	set(findobj(handles.Tsunamis, 'Label', 'TINTOL'), 'Vis','off')			% When in TINTOL we don't want to see this anymore
+	set(findobj(handles.Tsunamis, 'Label', 'Plot Stations'), 'Vis','on')	% And these were hiden waiting for showing up here
+	set(findobj(handles.Tsunamis, 'Label', 'Import Stations'), 'Vis','on')
+	set(findobj(handles.Tsunamis, 'Label', 'Swan'), 'Vis','off')
+	
