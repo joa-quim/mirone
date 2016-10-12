236f7f886145c977ab56e8b00874bd153c649402
Joaquim Luis <jmfluis@gmail.com>
2016-10-12 01:54:23 +0100

    Try to cheat the roipoly function so that it doesn't come out with a left/bottom lines of zeros when the two regions are equal.

--- utils/transplants.m
+++ utils/transplants.m
@@ -48,7 +48,7 @@ function varargout = transplants(hLine, tipo, res, handles, second_g)
 %	Contact info: w3.ualg.pt/~jluis/mirone
 % --------------------------------------------------------------------
 
-% $Id: transplants.m 9843 2016-10-03 01:29:48Z j $
+% $Id: transplants.m 9862 2016-10-12 00:53:30Z j $
 
 	if (nargin < 4)		% NOTE: in the IMPLANTGRID mode I'm not really using (YET) hLine for anything else
 		if (~ishandle(hLine) && ~strcmp(get(hLine,'type'), 'line') && ~strcmp(get(hLine,'type'), 'patch'))
@@ -318,7 +318,9 @@ function [Zhost_rect, r_c, hdr_new] = job_no_nans(handlesHost, Zhost, handlesImp
 	else
 		opt_I = sprintf('-I%.16g/%.16g',handlesImp.head(8), handlesImp.head(9));
 	end
-	mask_skirt = ~(img_fun('roipoly_j',[x_min x_max],[y_min y_max],Zhost_rect,x,y));	% Mask at the Host grid resolution
+	%mask_skirt = ~(img_fun('roipoly_j',[x_min x_max]+[-eps eps],[y_min y_max]+[-eps eps],Zhost_rect,x,y));	% Mask at the Host grid resolution
+	% Do this trick to cheat the shity roipoly that fails when both regions are equal
+	mask_skirt = ~(img_fun('roipoly_j',[x_min x_max]+[eps -eps]*1e2,[y_min y_max]+[eps -eps]*1e2,Zhost_rect,x,y));
 
 	Zhost_skirt = double(Zhost_rect(mask_skirt));		% It F... has to be
 
@@ -332,7 +334,7 @@ function [Zhost_rect, r_c, hdr_new] = job_no_nans(handlesHost, Zhost, handlesImp
 	ZZ = [Zhost_skirt(:); double(Zimp(:))];				% Add Implant data with the skirt (of width pad-1) of Host data
 	opt_C = ' ';
 
-	Zhost_rect = gmtmbgrid_m(XX,YY,ZZ(:), opt_R, opt_I, '-T0.25', '-Mz', opt_C);
+	Zhost_rect = gmtmbgrid_m(XX,YY,ZZ(:), opt_R, opt_I, '-T0.25', '-Mz', opt_C, '-T100');
 
 	% For doing this with grdsample, but I need still to calculate the apropriate new 'r_c'
 % 	opt_R = sprintf('-R%.16g/%.16g/%.16g/%.16g', Ximp(1), Ximp(end), Yimp(1), Yimp(end));
