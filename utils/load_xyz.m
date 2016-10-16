8455da66b81730738e9592fb365b0d2b2e9db8b7
Joaquim Luis <jmfluis@gmail.com>
2016-10-16 19:37:00 +0100

    Fix bug when checking if multi-segment file has only the '>' char

--- utils/load_xyz.m
+++ utils/load_xyz.m
@@ -79,7 +79,7 @@ function varargout = load_xyz(handles, opt, opt2)
 %	Contact info: w3.ualg.pt/~jluis/mirone
 % --------------------------------------------------------------------
 
-% $Id: load_xyz.m 9855 2016-10-10 18:40:03Z j $
+% $Id: load_xyz.m 9865 2016-10-16 18:36:41Z j $
 
 %	EXAMPLE CODE OF HOW TO CREATE A TEMPLATE FOR UICTX WHEN THESE ARE TOO MANY
 % 	cmenuHand = get(h, 'UIContextMenu');
@@ -773,8 +773,8 @@ function varargout = load_xyz(handles, opt, opt2)
 			if (orig_no_mseg)
 				draw_funs(hLine,'line_uicontext')		% Here hLine is actually only a scalar
 			else
-				% Sometimes we still have only '>'. If only one segment test for that case and jump if true
-				if (numel(multi_segs_str) == 1) && (numel(multi_segs_str{i}) <= 2) && strfind(multi_segs_str{i}, '>')
+				% Sometimes we still have only '>'. If only one segment, test for that case and jump if true
+				if (numel(multi_segs_str) == 1) && (numel(multi_segs_str{1}) <= 2) && strfind(multi_segs_str{1}, '>')
 					draw_funs(hLine,'isochron', '')
 				else
 					draw_funs(hLine,'isochron', multi_segs_str)
