4b89a46ae84c8dde04087f3e99cad257bc92d0a6
Joaquim Luis <jmfluis@gmail.com>
2016-11-18 01:11:14 +0000

    Add a "filter Outliers" menu to lines in 'ecran()'

--- utils/draw_funs.m
+++ utils/draw_funs.m
@@ -25,7 +25,7 @@ function varargout = draw_funs(hand, varargin)
 %	Contact info: w3.ualg.pt/~jluis/mirone
 % --------------------------------------------------------------------
 
-% $Id: draw_funs.m 9926 2016-11-15 22:59:11Z j $
+% $Id: draw_funs.m 9940 2016-11-18 01:10:35Z j $
 
 % A bit of strange tests but they are necessary for the cases when we use the new feval(fun,varargin{:}) 
 opt = varargin{1};		% function name to evaluate (new) or keyword to select one (old form)
@@ -216,6 +216,7 @@ function set_line_uicontext_XY(h, opt)
 		uimenu('Parent',h, 'Label', 'X origin only');
 		uimenu('Parent',h, 'Label', 'Y origin only');
 		uimenu('Parent',h, 'Label', 'XY origin');
+		uimenu(cmenuHand,  'Label', 'Filter Outliers', 'Sep','on');
 	else
 		ui_edit_polygon(h)			% Set edition functions
 		uimenu(cmenuHand, 'Label', 'Line length', 'Call', @show_LineLength_XY)
@@ -2938,7 +2939,7 @@ function save_GMT_DB_asc(h, fname)
 		if (isempty(getappdata(h(k), 'edited'))),	continue,	end		% Skip because it was not modified
 		GSHHS_str = getappdata(h(k),'GSHHS_str');
 		if (k == 1 && ~isempty(GSHHS_str))		% Write back the magic string that allows us to recognize these type of files
-			fprintf(fid,'# $Id: draw_funs.m 9926 2016-11-15 22:59:11Z j $\n#\n%s\n#\n', GSHHS_str);
+			fprintf(fid,'# $Id: draw_funs.m 9940 2016-11-18 01:10:35Z j $\n#\n%s\n#\n', GSHHS_str);
 		end
 		hdr = getappdata(h(k), 'LineInfo');
 		x = get(h(k), 'XData');			y = get(h(k), 'YData');
