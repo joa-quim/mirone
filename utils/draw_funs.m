bb16a327e771c3ab8ff2a58b0698c52a818a3041
j <j@localhost>
2017-01-28 12:03:43 +0000

    Store the clicked point of the set_isochrons_uicontext() cmenuHand in appdata.

--- utils/draw_funs.m
+++ utils/draw_funs.m
@@ -1219,7 +1219,7 @@ function set_isochrons_uicontext(h, data)
 	if (iscell(tag)),   tag = tag{1};   end
 
 	handles = guidata(get(h(1),'Parent'));				% Get Mirone handles
-	cmenuHand = uicontextmenu('Parent',handles.figure1);
+	cmenuHand = uicontextmenu('Parent',handles.figure1, 'Call', @store_clicked_pt);
 	set(h, 'UIContextMenu', cmenuHand);
 	cb_LineWidth = uictx_LineWidth(h);		% there are 5 cb_LineWidth outputs
 	cb_color = uictx_color(h);				% there are 9 cb_color outputs
@@ -1292,6 +1292,14 @@ function set_isochrons_uicontext(h, data)
 	uimenu(cmenuHand, 'Label', 'Euler rotation', 'Sep','on', 'Call', 'euler_stuff(gcf,gco)');
 	for (i=1:length(h)),		ui_edit_polygon(h(i)),		end		% Set edition functions
 
+% -----------------------------------------------------------------------------------------
+function store_clicked_pt(hObject, evt)
+% Store the clicked point on the base cmenuHand appdata. This is usefull when one want to know
+% the current point at the time of first clicking on figure. The axes current point prop only
+% tells the current point of last clicking, which normally was somewhere on a uictx option.
+	handles = guidata(hObject);
+	setappdata(hObject, 'clicked_pt', get(handles.axes1, 'CurrentPoint'))
+	
 % -----------------------------------------------------------------------------------------
 function set_gmtfile_uicontext(h, data)
 % h is a handle to the line of a gmtfile
