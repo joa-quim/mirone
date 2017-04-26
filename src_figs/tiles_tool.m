2eced9a0db4e36f26cb715b89f3d739556292b92
j <j@localhost>
2017-04-26 11:21:10 +0000

    Check that no non valid dirs are added to cache dirs list.

--- src_figs/tiles_tool.m
+++ src_figs/tiles_tool.m
@@ -196,7 +196,7 @@ function push_change_dir_CB(hObject, handles)
 	else            % This guy doesn't let to be compiled
 		cache_dir = uigetdir(cd, 'Select a directory');
 	end
-	if ~isempty(cache_dir)
+	if (~isempty(cache_dir) && cache_dir ~= 0)
 		popup_directory_list_CB(handles.popup_directory_list, handles, cache_dir)
 	end
 
