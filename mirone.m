5aea9ae81f9e7893c49e1d57cf4c4aa2371dccfc
j <j@localhost>
2017-07-17 19:05:32 +0000

    Force curl to nor verify certificates.

--- mirone.m
+++ mirone.m
@@ -463,6 +463,7 @@ function hObject = mirone_OpeningFcn(varargin)
 	set_gmt(['PROJ_LIB=' home_dir fsep 'data' fsep 'proj_lib']);		% For projections with GDAL
 	set_gmt(['GDAL_DATA=' home_dir fsep 'data' fsep 'gdal_data']);
 	set_gmt(['GEOTIFF_CSV=' home_dir fsep 'data' fsep 'gdal_data']);
+	set_gmt('GDAL_HTTP_UNSAFESSL=YES');				% To tell libcurl to not verify the peer  
 
  	try		custom_menus(hObject, handles.path_data),	end			% Add any custom menus as commanded by OPTcontrol.txt
 
