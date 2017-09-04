d6b0f2749010fe83981d59defd4e390469a305fb
Joaquim Luis <jmfluis@gmail.com>
2017-09-05 00:54:53 +0100

    Remove the =cf in scripts when creating new grids

--- src_figs/write_gmt_script.m
+++ src_figs/write_gmt_script.m
@@ -16,7 +16,7 @@ function varargout = write_gmt_script(varargin)
 %	Contact info: w3.ualg.pt/~jluis/mirone
 % --------------------------------------------------------------------
 
-% $Id: write_gmt_script.m 10126 2017-08-29 21:06:57Z j $
+% $Id: write_gmt_script.m 10131 2017-09-04 23:54:32Z j $
 
 	handMir = varargin{1};
 	if (handMir.no_file)     % Stupid call with nothing loaded on the Mirone window
@@ -1304,7 +1304,7 @@ function [out_msg, warn_msg_pscoast] = build_write_script(handles, dest_dir, pre
 			if (~isempty(opt_M) && handles.have_GMT5),			opt_M = ' -fg';	end
 			opt_N = '';
 			if (handMir.Illumin_type == 1),      opt_N = ' -Nt';     end
-			name_illum = [prefix '_intens.grd=cf'];
+			name_illum = [prefix '_intens.grd'];
 			script{l} = sprintf('\n%s -------- Compute the illumination grid', comm);    l=l+1;
 			script{l} = ['grdgradient ' pb 'grd' pf opt_M ' ' illumComm opt_N ' -G' name_illum ellips];    l=l+1;
 			have_gmt_illum = 1;     used_grd = true;
@@ -1711,7 +1711,7 @@ function [out_msg, warn_msg_pscoast] = build_write_script(handles, dest_dir, pre
 					ct_names{k} = get(AtlasHand(k),'UserData');
 				end
 			else                            % We have only one country
-				ct_names = {get(AtlasHand(k),'UserData')};
+				ct_names = {get(AtlasHand,'UserData')};
 			end
 			ct_names = unique(ct_names);    % Many countries have several polygons (e.g. islands).
 			name = [prefix_ddir '_country_names.txt'];
