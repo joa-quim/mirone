3f08c1b0fcabdde75c21a0662ff4611476af5047
Joaquim Luis <jmfluis@gmail.com>
2018-02-14 01:06:31 +0000

    Add 'rotate_all' case

--- utils/draw_funs.m
+++ utils/draw_funs.m
@@ -25,7 +25,7 @@ function varargout = draw_funs(hand, varargin)
 %	Contact info: w3.ualg.pt/~jluis/mirone
 % --------------------------------------------------------------------
 
-% $Id: draw_funs.m 10268 2018-02-11 00:48:53Z j $
+% $Id: draw_funs.m 10278 2018-02-14 01:05:35Z j $
 
 % A bit of strange tests but they are necessary for the cases when we use the new feval(fun,varargin{:}) 
 opt = varargin{1};		% function name to evaluate (new) or keyword to select one (old form)
@@ -1331,6 +1331,7 @@ function set_isochrons_uicontext(h, data)
 	uimenu(item, 'Label', 'Age Grid', 'Callback', {@pole2neighbor, [], 'agegrid'}, 'Sep','on');
 	uimenu(item, 'Label', 'Make Age-script', 'Callback', @make_age_script);	
 	uimenu(cmenuHand, 'Label', 'Euler rotation', 'Sep','on', 'Callback', 'euler_stuff(gcf,gco)');
+	uimenu(cmenuHand, 'Label', 'Rotate all in plate', 'Callback', 'pole2neighbor([],[], gco, ''rot_all'')');
 	for (i=1:length(h)),		ui_edit_polygon(h(i)),		end		% Set edition functions
 
 % -----------------------------------------------------------------------------------------
@@ -3097,7 +3098,7 @@ function save_GMT_DB_asc(h, fname)
 		if (isempty(getappdata(h(k), 'edited'))),	continue,	end		% Skip because it was not modified
 		GSHHS_str = getappdata(h(k),'GSHHS_str');
 		if (k == 1 && ~isempty(GSHHS_str))		% Write back the magic string that allows us to recognize these type of files
-			fprintf(fid,'# $Id: draw_funs.m 10268 2018-02-11 00:48:53Z j $\n#\n%s\n#\n', GSHHS_str);
+			fprintf(fid,'# $Id: draw_funs.m 10278 2018-02-14 01:05:35Z j $\n#\n%s\n#\n', GSHHS_str);
 		end
 		hdr = getappdata(h(k), 'LineInfo');
 		x = get(h(k), 'XData');			y = get(h(k), 'YData');
