6d671d50724cebee86d8fe622fd0d129b9cae540
Joaquim Luis <jmfluis@gmail.com>
2018-07-03 12:20:52 +0100

    Check and return silently if no input.

--- src_figs/bands_list.m
+++ src_figs/bands_list.m
@@ -16,7 +16,7 @@ function varargout = bands_list(varargin)
 %	Contact info: w3.ualg.pt/~jluis/mirone
 % --------------------------------------------------------------------
 
-% $Id: bands_list.m 11344 2018-07-02 16:44:02Z j $
+% $Id: bands_list.m 11349 2018-07-03 11:19:54Z j $
 
 	if (nargin > 1 && ischar(varargin{1}))
 		gui_CB = str2func(varargin{1});
@@ -30,7 +30,9 @@ function varargout = bands_list(varargin)
 function hObject = bands_list_OF(varargin)
 
 	if (isempty(varargin)),		return,		end
- 
+ 	bandList = getappdata(varargin{1},'BandList');
+	if (isempty(bandList)),		hObject = [];	return,		end
+
 	hObject = figure('Vis','off');
 	bands_list_LayoutFcn(hObject);
 	handles = guihandles(hObject);
@@ -42,11 +44,6 @@ function hObject = bands_list_OF(varargin)
 	handles.edit_Rband_pos = get(handles.edit_Rband,'Pos');
 
 	handles.hMirFig = varargin{1};
-	bandList = getappdata(varargin{1},'BandList');
-	if (isempty(bandList))
-		errordlg('ERROR: There is no list of bands available. Do you know what you are doing?','ERROR')
-		delete(hObject);    return
-	end
 	move2side(handles.hMirFig,hObject,'left')	% Reposition this figure
 	handles.struct_names = bandList(1);
 	set(handles.listbox1,'String',bandList(1))
