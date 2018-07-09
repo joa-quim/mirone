68e55a7d89736e7ff0874f5d56f33a27dfe5de13
j <j@localhost>
2018-07-09 10:54:46 +0000

    Was not accepting file names

--- utils/c_nearneighbor.m
+++ utils/c_nearneighbor.m
@@ -3,10 +3,11 @@ function [out, hdr] = c_nearneighbor(data, varargin)
 
 % A merda é que 'data' pode ser um Mx3 ou entao um Mx1 e os X,Y[,W] devem tar no varargin
 
-% $Id: c_nearneighbor.m 10230 2018-01-26 01:34:32Z j $
+% $Id$
 
 	% nearneighbor in GMT5 is less elastic in terms of how to swallow the data array(s)
-	if (size(data,2) == 3 || size(data,2) == 4)
+	%if (size(data,2) == 3 || size(data,2) == 4)
+	if (size(data,2) == 3 || size(data,2) == 4 || isa(data,'char'))
 		k0 = 1;
 	elseif (numel(varargin) >= 2 && isnumeric(varargin{1}) && size(varargin{1},2) == 1 && ...
 			isnumeric(varargin{2}) && size(varargin{2},2) == 1)
