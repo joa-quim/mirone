703f17f292e556d877de6bc0911c9cc49531de38
j <j@localhost>
2019-07-18 15:20:44 +0000

    Remove the DrawEulerPoleCircle_CB() function. It now lives in separate figure.

--- mirone.m
+++ mirone.m
@@ -3178,46 +3178,6 @@ function DrawClosedPolygon_CB(handles, opt)
 	end
 
 % --------------------------------------------------------------------
-function DrawEulerPoleCircle_CB(handles)
-	if (aux_funs('msg_dlg',1,handles)),		return,		end		% Test geog & no_file
-	if (strcmp(get(handles.figure1,'Pointer'), 'crosshair')),	return,		end		% Already drawing something else
-	zoom_state(handles,'maybe_off');
-	
-	out = euler_poles_selector(handles.home_dir);			% The output is a struct with fields: lon lat omega plates model
-	if isempty(out),	return,		end % User gave up
-	plon = out.lon;		plat = out.lat;
-	if (~out.absolute)
-		h_circ = uicirclegeo(plon, plat, handles.axes1);
-		set(h_circ,'Tag','CircleEuler')		% This is used by draw_funs to allow velocity computations
-		s = get(h_circ,'Userdata');
-		s.omega = out.omega;
-		if ~isempty(out.plates)			% Just in case
-			if (~strncmp('absolute',out.plates,8))			% A relative plate model
-				s.plates = [out.plates '  -- Model = ' out.model];
-			else											% An absolute plate model
-				s.plates = [out.plates(end-1:end) ' -- Model = ' out.model ' (Absolute)'];
-			end
-		else
-			s.plates = 'I''m lost';
-		end
-		set(h_circ,'Userdata',s)
-		draw_funs(h_circ,'SessionRestoreCircle')
-	else
-		pt = click_e_point(1,'crosshair');
-		if (isempty(pt)),	return,		end
-		lon = pt(1);		lat = pt(2);
-		[Vx, Vy] = draw_funs([], 'compute_EulerVel', lat, lon, plat, plon, out.omega, 'Nikles');
-		struc_arrow = struct('spacingChanged',[], 'hQuiver', [], 'hAx', handles.axes1);
-		hQuiver = draw_funs([], 'loc_quiver', struc_arrow, lon, lat, Vx, Vy);
-		x = get(hQuiver,'XData');		y = get(hQuiver,'YData');
-		set(hQuiver(1),'XData',[x{1} x{2}], 'YData',[y{1} y{2}])	% Merge the header with the "trunk"
-		delete(hQuiver(2));		hQuiver(2) = [];
-		set(hQuiver,'Tag','Seta','Userdata',1)
-		draw_funs(hQuiver,'line_uicontext')
-	end
-	zoom_state(handles,'maybe_on');
-
-% --------------------------------------------------------------------
 function DrawGeogCircle_CB(handles, opt)
 	if (handles.no_file),	return,		end
 	if (strcmp(get(handles.figure1,'Pointer'), 'crosshair')),	return,		end		% Already drawing something else
