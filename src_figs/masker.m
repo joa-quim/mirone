01e651fe54416b9beb221d4a2f8692b9d96cc4d4
Joaquim Luis <jmfluis@gmail.com>
2019-10-28 00:32:57 +0000

    Add wrapper fig to apply masks to grid/images

---
+++ src_figs/masker.m
@@ -0,0 +1,236 @@
+function varargout = masker(varargin)
+% Helper Window to run
+
+%	Copyright (c) 2004-2019 by J. Luis
+%
+%             DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+%                     Version 2, December 2004
+% 
+%  Everyone is permitted to copy and distribute verbatim or modified
+%  copies of this license document, and changing it is allowed as long
+%  as the name is changed.
+% 
+%             DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+%    TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+% 
+%   0. You just DO WHAT THE FUCK YOU WANT TO.
+%
+%	Contact info: w3.ualg.pt/~jluis/mirone
+% --------------------------------------------------------------------
+
+	if (nargin > 1 && ischar(varargin{1}))
+		gui_CB = str2func(varargin{1});
+		[varargout{1:nargout}] = feval(gui_CB,varargin{2:end});
+	else
+		h = masker_OF(varargin{:});
+		if (nargout),	varargout{1} = h;   end
+	end
+
+% ---------------------------------------------------------------------------------
+function hObject = masker_OF(varargin)
+	if (isempty(varargin)),		hObject = [];	return,		end
+	if (varargin{1}.no_file),	hObject = [];	return,		end
+ 
+	hObject = figure('Vis','off');
+	masker_LayoutFcn(hObject);
+	handles = guihandles(hObject);
+	move2side(hObject,'right')
+
+	handMir = varargin{1};
+	handles.hMirFig = handMir.figure1;
+	handles.hMirImg = handMir.hImg;
+	handles.last_dir = handMir.last_dir;
+	handles.mask = [];
+
+	[masks, handles.hMaskFigs] = fish_MirMasks(handles);
+	if (~isempty(masks))
+		set(handles.listbox, 'String', masks)
+	else								% Resize fig since no floating masks are around
+		pos = get(handles.listbox, 'Pos');		H = pos(2);
+		pos = get(handles.figure1, 'Pos');		pos(4) = H - 1;
+		set(handles.figure1, 'Pos', pos)
+	end
+
+	if (handMir.validGrid),		set(handles.check_alpha, 'Vis', 'off'),	end
+	guidata(hObject, handles);
+	set(hObject,'Visible','on');
+	if (nargin > 1),	external_drive(handles, 'masker', varargin{2:end}),	end
+
+% -----------------------------------------------------------------------------------------
+function listbox_CB(hObject, handles)
+% ...
+	val = get(hObject, 'Val');
+	if (val == 1),	return,		end
+	thisHand = guidata(handles.hMaskFigs(val-1));
+	if (thisHand.validGrid)
+		handles.mask = logical(getappdata(thisHand.figure1,'dem_z'));
+	else
+		handles.mask = logical(get(thisHand.hImg,'CData'));
+	end
+	guidata(handles.figure1, handles)
+
+% -----------------------------------------------------------------------------------------
+function edit_fname_CB(hObject, handles)
+	fname = get(hObject,'String');
+	if (isempty(fname)),	return,		end
+	push_fname_CB(handles.push_fname, handles, fname)
+
+% -----------------------------------------------------------------------------------------
+function push_fname_CB(hObject, handles, opt)
+	if (nargin == 2)
+		set(handles.edit_fname,'Str',opt)		
+		[img, handles_out, handles.att] = mirone('FileOpenNewImage_CB', handles);
+	else
+		[img, handles_out, handles.att] = mirone('FileOpenNewImage_CB', handles, opt);
+	end
+	handles.mask = logical(img);
+	handles.head = handles_out.head;
+	guidata(handles, handles.figure1)
+
+% -----------------------------------------------------------------------------------------
+function push_OK_CB(hObject, handles)
+% ...
+	if (isempty(handles.mask)),		errordlg('Yes, yes. And what about giving me a mask?','Error'),		end
+	if (get(handles.check_revert, 'Val'))
+		mask = ~handles.mask;
+	else
+		mask = handles.mask;
+	end
+	handMir = guidata(handles.hMirFig);
+	if (handMir.validGrid)
+		val = single(str2double(get(handles.edit_value, 'String')));
+		Z = getappdata(handles.hMirFig,'dem_z');
+		if (size(Z,1) ~= size(mask,1) || size(Z,2) ~= size(mask,2))
+			errordlg('Maks and target do not have the same size. Impossible to continue', 'Error')
+			return
+		end
+		Z(mask) = val;
+		tmp.head = handMir.head;
+		if (val < tmp.head(5)),	tmp.head(5) = val;	end
+		if (val > tmp.head(6)),	tmp.head(6) = val;	end
+		tmp.geog = handMir.geog;
+		name = get(handles.hMirFig, 'Name');
+ 		ind = strfind(name, '@');
+		if (~isempty(ind)),	name = ddewhite(name(1:ind(end)-1));	end
+		[pato, fname] = fileparts(name);
+		tmp.name = [fname 'Masked'];
+		mirone(Z, tmp, handles.hMirFig)
+	else
+		img = get(handMir.hImg, 'CData');
+		if (size(img,3) == 1)
+			img(mask) = 0;			% 0 means bg color
+		else
+			img(repmat(mask,[1 1 3])) = handMir.bg_color(1);
+		end
+		if (get(handles.check_alpha, 'Val'))
+			set(handles.hMirImg, 'AlphaData', mask)
+		end
+		set(handMir.hImg, 'CData', img)
+	end
+
+% -----------------------------------------------------------------------------------------
+function [masks, hFigs] = fish_MirMasks(handles)
+% Fish Mirone figs with masks and return their names, or empty if not found
+	masks = [];
+	hFigs = findobj(0,'type','figure');						% Fish all figures
+	hFigs = aux_funs('figs_XOR', handles.hMirFig, hFigs);	% Get all unique Mirone Figs
+	if (isempty(hFigs)),	return,		end
+	masks = false(1, numel(hFigs));
+	[H,W,layers] = size(get(handles.hMirImg, 'CData'));
+	for (k = 1:numel(hFigs))
+		thisHand = guidata(hFigs(k));
+		this_im = get(thisHand.hImg,'CData');
+		[H_,W_,layer] = size(this_im);
+		if (layer == 1 && W == W_ && H == H_),	masks(k) = true;	end
+	end
+	hFigs = hFigs(masks);
+	if (isempty(hFigs)),	return,		end		% None of them is a mask with the same size as IMG
+	nomes = get(hFigs,'name');
+	if (~isa(nomes,'cell')),	nomes = {nomes};	end
+	for (k = 1:numel(hFigs))
+		ind = strfind(nomes{k}, '@');
+		if (~isempty(ind)),	nomes{k} = ddewhite(nomes{k}(1:ind(end)-1));	end
+		[pato, nomes{k}] = fileparts(nomes{k});
+	end
+	masks = [{' '}; nomes];
+
+% ---------------------------------------------------
+function masker_LayoutFcn(h1)
+
+set(h1, 'Position',[520 605 321 211],...
+	'Color',get(0,'factoryUicontrolBackgroundColor'),...
+	'MenuBar','none',...
+	'Name','Masker',...
+	'NumberTitle','off',...
+	'Resize','off',...
+	'HandleVisibility','callback',...
+	'Tag','figure1');
+
+uicontrol('Parent',h1, 'Position',[11 183 190 15],...
+	'HorizontalAlignment','left',...
+	'String','Use this mask file (click to select)',...
+	'Style','text');
+
+uicontrol('Parent',h1, 'Position',[10 131 301 51],...
+	'Callback',@masker_uiCB,...
+	'String','',...
+	'Style','listbox',...
+	'Value',1,...
+	'Tag','listbox');
+
+uicontrol('Parent',h1, 'Position',[10 81 281 21],...
+	'BackgroundColor',[1 1 1],...
+	'Callback',@masker_uiCB,...
+	'HorizontalAlignment','left',...
+	'String','',...
+	'Style','edit',...
+	'Tag','edit_fname');
+
+uicontrol('Parent',h1, 'Position',[10 102 100 15],...
+	'HorizontalAlignment','left',...
+	'String','Load mask from file',...
+	'Style','text',...
+	'Tag','text_mask_file');
+
+uicontrol('Parent',h1, 'Position',[10 53 140 15],...
+	'HorizontalAlignment','left',...
+	'String','Replace masked values by:',...
+	'Style','text',...
+	'Tag','text_mask_val');
+
+uicontrol('Parent',h1, 'Position',[287 81 23 21],...
+	'Callback',@masker_uiCB,...
+	'String','...',...
+	'Tag','push_fname');
+
+uicontrol('Parent',h1, 'Position',[147 50 90 21],...
+	'BackgroundColor',[1 1 1],...
+	'String','NaN',...
+	'Style','edit',...
+	'Tooltip','All masked values in grid will be replaced by this',...
+	'Tag','edit_value');
+
+uicontrol('Parent',h1, 'Position',[10 28 78 15],...
+	'String','Revert mask',...
+	'Style','checkbox',...
+	'Tooltip','Negate mask. That is, swapp zeros and ones',...
+	'Tag','check_revert');
+
+uicontrol('Parent',h1, 'Position',[10 9 100 15],...
+	'String','Add alpha layer',...
+	'Style','checkbox',...
+	'Tooltip','Add mask to image alpha layer',...
+	'Value',1,...
+	'Tag','check_alpha');
+
+uicontrol('Parent',h1, 'Position',[250 10 60 23],...
+	'Callback',@masker_uiCB,...
+	'FontSize',10,...
+	'FontWeight','bold',...
+	'String','OK',...
+	'Tag','push_OK');
+
+
+function masker_uiCB(hObject, evt)
+% This function is executed by the callback and than the handles is allways updated.
+	feval([get(hObject,'Tag') '_CB'],hObject, guidata(hObject));
