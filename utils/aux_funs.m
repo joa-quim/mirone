4166e2b688d28af40e17b0c30abac88807f21e28
Joaquim Luis <jmfluis@gmail.com>
2019-11-03 23:57:02 +0000

    Store more metadata in Landsat8 files

--- utils/aux_funs.m
+++ utils/aux_funs.m
@@ -107,6 +107,7 @@ function check_LandSat8(handles, fname)
 		end
 	end
 	band = name(ind+2:end);
+	pars.band = str2double(band);
 	fid = fopen(t, 'rt');
 	if (fid < 0),	errordlg(['Error opening file: ' t], 'Error');	return,	end
 	s = strread(fread(fid,'*char').','%s','delimiter','\n');	fclose(fid);
@@ -114,6 +115,8 @@ function check_LandSat8(handles, fname)
 	pars.rad_mul = str2double(t(strfind(t, '=')+1:end));
 	t = s{strncmp(s, ['RADIANCE_ADD_BAND_' band], 18+numel(band))};
 	pars.rad_add = str2double(t(strfind(t, '=')+1:end));
+	t = s{strncmp(s, ['RADIANCE_MAXIMUM_BAND_' band], 22+numel(band))};
+	pars.rad_max = str2double(t(strfind(t, '=')+1:end));
 	
 	% Bands 10 & 11 have no REFLECTANCE coeffs
 	if (~strcmp(band, '10') && ~strcmp(band, '11'))
@@ -121,16 +124,20 @@ function check_LandSat8(handles, fname)
 		pars.reflect_mul = str2double(t(strfind(t, '=')+1:end));
 		t = s{strncmp(s, ['REFLECTANCE_ADD_BAND_' band], 21+numel(band))};
 		pars.reflect_add = str2double(t(strfind(t, '=')+1:end));
+		t = s{strncmp(s, ['REFLECTANCE_MAXIMUM_BAND_' band], 25+numel(band))};
+		pars.reflect_max = str2double(t(strfind(t, '=')+1:end));
 	else
-		pars.reflect_mul = 1;	pars.reflect_add = 0;
+		pars.reflect_mul = 1;	pars.reflect_add = 0;	pars.reflect_max = 0;
 	end
 	
 	t = s{strncmp(s, 'SUN_AZIMUTH', 11)};
 	pars.sun_azim = str2double(t(strfind(t, '=')+1:end));
 	t = s{strncmp(s, 'SUN_ELEVATION', 13)};
 	pars.sun_elev = str2double(t(strfind(t, '=')+1:end));
+	t = s{strncmp(s, 'EARTH_SUN_DISTANCE', 18)};
+	pars.sun_dist = str2double(t(strfind(t, '=')+1:end));
 	
-	if (str2double(band) >= 10)
+	if (pars.band >= 10)
 		t = s{strncmp(s, ['K1_CONSTANT_BAND_' band], 17+numel(band))};
 		pars.K1 = str2double(t(strfind(t, '=')+1:end));
 		t = s{strncmp(s, ['K2_CONSTANT_BAND_' band], 17+numel(band))};
