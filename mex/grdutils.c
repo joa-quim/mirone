f888550e52e821c23a4edf42d669950972f5c8a6
Joaquim Luis <jmfluis@gmail.com>
2019-11-04 00:02:08 +0000

    Do not use OMP to locate min/max (it fcks). Change order os ops when mat_mull

--- mex/grdutils.c
+++ mex/grdutils.c
@@ -52,7 +52,7 @@
 	#endif
 	#if defined(_OPENMP) && (_OPENMP > 201200)
 		#ifdef _MSC_VER
-			#define OMP_PARF_MINMAX __pragma(omp parallel for reduction(min : min_val) reduction(max : max_val))
+			#define OMP_PARF_MINMAX __pragma(omp parallel for private(i, tmp, nfound, i_min, i_max) reduction(min : min_val) reduction(max : max_val))
 		#else
 			#define OMP_PARF_MINMAX _Pragma("omp parallel for reduction(min : min_val) reduction(max : max_val)")
 		#endif
@@ -182,7 +182,7 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
 		mexPrintf ("\t   Without output do operation insitu.\n");
 		mexPrintf ("\t-L Compute min and max. Apend + (-L+) to check also for NaNs\n");
 		mexPrintf ("\t-H outputs [z_min z_max i_zmin i_zmax firstNaNind mean std]\n");
-		mexPrintf ("\t-M factor multiplies array by factor. If both -A and -M, first add then multiply\n");
+		mexPrintf ("\t-M factor multiplies array by factor. If both -A and -M, first multiply then add\n");
 		mexPrintf ("\t-N See if grid has NaNs and if yes returns its index + 1 and exit.\n");
 		mexPrintf ("\t-S Compute mean and standard deviation.\n");
 		mexPrintf ("\t-T Compute bright Temperature from Landsat8 bands 10 or 11. Where:\n");
@@ -301,7 +301,7 @@ OMP_PARF_MINMAX
 				}
 			}
 			else if (do_min_max_loc) {
-OMP_PARF_MINMAX
+//OMP_PARF_MINMAX
 				for (i = 0; i < nxy; i++) {
 					if (ISNAN_F(zdata[i])) {nfound++;	continue;}
 					tmp = (double)zdata[i];
@@ -347,7 +347,7 @@ OMP_PARF_MINMAX
 				}
 			}
 			else if (do_min_max_loc) {
-OMP_PARF_MINMAX
+//OMP_PARF_MINMAX
 				for (i = 0; i < nxy; i++) {
 					tmp = (double)dataU16[i];
 					if (tmp < min_val) {min_val = tmp;		i_min = i;}
@@ -392,7 +392,7 @@ OMP_PARF_MINMAX
 			}
 		}
 		else if (do_min_max_loc) {
-OMP_PARF_MINMAX
+//OMP_PARF_MINMAX
 			for (i = 0; i < nxy; i++) {
 				tmp = (double)data8[i];
 				if (tmp < min_val) {min_val = tmp;		i_min = i;}
@@ -477,7 +477,7 @@ void mul_add(void *array_in, void *array_out, float fac_x, float fac_a, size_t n
 OMP_PARF
 			for (i = 0; i < np; i++) {
 				if (ISNAN_F(f4_i[i])) continue;
-				f4_i[i] = (f4_i[i] + fac_a) * fac_x;
+				f4_i[i] = f4_i[i] * fac_x + fac_a;
 			}
 		}
 		else if (fac_x != 1) {		/* MUL */
@@ -499,7 +499,7 @@ OMP_PARF
 		u2_i = (unsigned short int *)array_in;		f4_o = (float *)array_out;
 		if (fac_x != 1 && fac_a != 0)	/* MULL_ADD */
 OMP_PARF
-			for (i = 0; i < np; i++) f4_o[i] = (u2_i[i] + fac_a) * fac_x;
+			for (i = 0; i < np; i++) f4_o[i] = u2_i[i] * fac_x + fac_a;
 		else if (fac_x != 1)			/* MUL */
 OMP_PARF
 			for (i = 0; i < np; i++) f4_o[i] = u2_i[i] * fac_x;
