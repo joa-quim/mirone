09c7000cc4054ba4cb0487af534e4782eed6fd2d
Joaquim Luis <jmfluis@gmail.com>
2019-11-15 17:18:06 +0000

    Add several operations grid-grid and grid-scalar. Add deepcopy option

--- mex/grdutils.c
+++ mex/grdutils.c
@@ -69,6 +69,8 @@
                     ((*(int32_T *)&(x) & 0x007fffffL) != 0x00000000L))
 
 void mul_add(void *array_in, void *array_out, float fac_x, float fac_a, size_t np, int type);
+void grid_ops(float *z1, float *z2, float *array_out, char *operator, size_t np);
+void grid_clip(float *z1, float *array_out, char *operator, float val1, float val2, size_t np);
 
 /* --------------------------------------------------------------------------- */
 /* Matlab Gateway routine */
@@ -78,17 +80,19 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
 	int n, nx, ny, nfound = 0, ngood, argc = 0, n_arg_no_char = 0;
 	int error = FALSE, ADD = FALSE, MUL = FALSE, ADD_MUL = FALSE, do_min_max = FALSE, do_std = FALSE;
 	int is_double = FALSE, is_single = FALSE, is_int16 = FALSE, is_uint8 = FALSE;
+	int have_fun_to_grd = FALSE, have_two_grds = FALSE, do_deep_copy = FALSE;
 	int is_uint16 = FALSE, report_nans = FALSE, only_report_nans = FALSE, do_cast = FALSE;
 	int i_min = 0, i_max = 0, do_min_max_loc = FALSE, report_min_max_loc_nan_mean_std = FALSE;
 	int do_shift_int8 = FALSE, insitu = FALSE, is_int8 = FALSE, show_time = FALSE, Trad = FALSE;
 	long long i;
 	size_t j, nxy;
-	char   **argv;
+	char   **argv, operator[4] ={""}, clip_op[4] = {""}, clip_val_s[64];
 	char    *data8;
 	unsigned char  *dataU8;
 	short int *data16;
 	unsigned short int *dataU16;
-	float   *zdata, *array_out, fact_x = 1, fact_a = 0, K1, K2, Ml, Al, NaN;
+	float   *zdata, *zdata2 = NULL, *array_out = NULL, fact_x = 1, fact_a = 0, K1, K2, Ml, Al, NaN;
+	float   clip_val1, clip_val2;
 	double  *z, min_val = FLT_MAX, max_val = -FLT_MAX, mean = 0., sd = 0., rms = 0., tmp;
 	clock_t tic;
 
@@ -125,6 +129,9 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
 					do_shift_int8 = TRUE;
 					if (nlhs == 0) insitu = TRUE;	/* Only allowed case */
 					break;
+				case 'D':
+					do_deep_copy = TRUE;
+					break;
 				case 'H':
 					do_min_max_loc = TRUE;
 					do_std = TRUE;
@@ -144,11 +151,36 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
 				case 'N':
 					only_report_nans = TRUE;
 					break;
+				case 'F':
+					if ((n = sscanf(&argv[i][2], "%[^/]/%f/%s", clip_op, &clip_val1, clip_val_s)) != 3) {
+						mexPrintf("GRDUTILS ERROR: -G option must pass 3 values. -Gop/val/val_rep but passed %d\n", n);
+						error++;
+					}
+					if (strcmp(clip_op, "<") && strcmp(clip_op, "<=") && strcmp(clip_op, ">") && strcmp(clip_op, ">=")) {
+						mexPrintf("GRDUTILS ERROR: -G option, invalid clipping operator %s.\n", clip_op);
+						error++;
+					}
+					if (!strcmp(clip_val_s, "NaN"))
+						clip_val2 = (float)mxGetNaN();
+					else
+						clip_val2 = (float)atof(clip_val_s);
+					break;
+				case 'O':
+					if (!strcmp(&argv[i][2], "ADD") || !strcmp(&argv[i][2], "SUB") || !strcmp(&argv[i][2], "MUL") ||
+					    !strcmp(&argv[i][2], "DIV") || !strcmp(&argv[i][2], "EXP") || !strcmp(&argv[i][2], "LOG10") ||
+						!strcmp(&argv[i][2], "LOG") || !strcmp(&argv[i][2], "POW") || !strcmp(&argv[i][2], "SIN") ||
+						!strcmp(&argv[i][2], "COS") || !strcmp(&argv[i][2], "TAN") || !strcmp(&argv[i][2], "SQRT"))
+						strcpy(operator, &argv[i][2]);
+					else {
+						mexPrintf("GRDUTILS ERROR: -O option, invalid operator name %s.\n", operator);
+						error++;
+					}
+					break;
 				case 'S':
 					do_std = TRUE;
 					break;
 				case 'T':
-					if ((n = sscanf (&argv[i][2], "%f/%f/%f/%f", &Ml, &Al, &K1, &K2)) != 4) {
+					if ((n = sscanf(&argv[i][2], "%f/%f/%f/%f", &Ml, &Al, &K1, &K2)) != 4) {
 						mexPrintf("GRDUTILS ERROR: -T option must pass 4 values. -Tx1/x2/x3/x4\n");
 						error++;
 					}
@@ -163,12 +195,15 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
 			}
 		}
 	}
+	if (error)
+		mexErrMsgTxt("\n");
+
 	if (ADD && MUL) ADD_MUL = TRUE;
 	
-	if (n_arg_no_char == 0 || error) {
+	if (n_arg_no_char == 0) {
 		mexPrintf ("grdutils - Do some usefull things on arrays that are in single precision\n\n");
-		mexPrintf ("usage: [out] = grdutils(infile, ['-A<const>'], [-C], ['-L[+]'], [-H], [-M<fact>]\n");
-		mexPrintf ("                        [-TMl/Al/K1/k2], [-N], [-S], [-c], [-t])\n");
+		mexPrintf ("usage: [out] = grdutils(infile, [grid2,] ['-A<const>'], [-C], [-D], [-F<op>/val/rep], ['-L[+]'],\n");
+		mexPrintf ("                        [-H], [-M<fact>], [-TMl/Al/K1/k2], [-N], [-O<op>], [-S], [-c], [-t])\n");
 		
 		mexPrintf ("\t<out> is a two line vector with [min,max] or [mean,std] if -L OR -S\n");
 		mexPrintf ("\t<out> is a float array when -A and/or -M and infile is a uint16 array\n");
@@ -180,10 +215,16 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
 		mexPrintf ("\t-C casts array uint8 to int8 and subtracts 128. That is [0 255] to [-128 127].\n");
 		mexPrintf ("\t-c Shift int8 arrays by -128. That is [0 .. 0 127] to [-128 127].\n");
 		mexPrintf ("\t   Without output do operation insitu.\n");
+		mexPrintf ("\t-D Makes a deep copy of the input array and returns right away.\n");
+		mexPrintf ("\t-F <op>/val/rep where op is the operator to apply to clip infile. One of <, <=, >, >=\n");
+		mexPrintf ("\t   It replaces all values op(val) by rep. Example -F</10/NaN replace all z[i] < 10 by NaN\n");
 		mexPrintf ("\t-L Compute min and max. Apend + (-L+) to check also for NaNs\n");
 		mexPrintf ("\t-H outputs [z_min z_max i_zmin i_zmax firstNaNind mean std]\n");
 		mexPrintf ("\t-M factor multiplies array by factor. If both -A and -M, first multiply then add\n");
 		mexPrintf ("\t-N See if grid has NaNs and if yes returns its index + 1 and exit.\n");
+		mexPrintf ("\t-O <op> is operator to apply between infile and grid2. One of ADD, SUB, MUL, DIV, EXP.\n");
+		mexPrintf ("\t-  LOG, LOG10, POW, SIN, COS, TAN, SQRT.\n");
+		mexPrintf ("\t   If no output required, then <op> is done insitu and result is stored in infile.\n");
 		mexPrintf ("\t-S Compute mean and standard deviation.\n");
 		mexPrintf ("\t-T Compute bright Temperature from Landsat8 bands 10 or 11. Where:\n");
 		mexPrintf ("\t   Ml = RADIANCE_MULT_BAND_X where 'X' is the band number\n");
@@ -194,12 +235,15 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
 		mexErrMsgTxt("\n");
 	}
 
-	if (show_time) tic = clock();
-	
-	if (nlhs == 0 && !(ADD + MUL) && !insitu) {
-		mexPrintf("GRDUTILS ERROR: Must provide an output.\n");
+	if (do_deep_copy) {
+		if (nlhs != 1)
+			mexErrMsgTxt("GRDUTILS ERROR: Deep copy requires an output.\n");
+		plhs[0] = mxDuplicateArray(prhs[0]);
 		return;
 	}
+
+	if (show_time) tic = clock();
+
 	if (report_min_max_loc_nan_mean_std) {	/* Just in case */
 		do_min_max = FALSE;
 		only_report_nans = FALSE;
@@ -225,6 +269,28 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
 	ny = mxGetM (prhs[0]);
 	if (!mxIsNumeric(prhs[0]) || ny < 2 || nx < 2)
 		mexErrMsgTxt("GRDUTILS ERROR: First argument must contain a decent array\n");
+
+	if (is_single && !mxIsChar(prhs[1])) {
+		if (!mxIsSingle(prhs[1]))
+			mexErrMsgTxt("When the first two args are arrays, they must be of single type.\n");
+		if (nx != mxGetN(prhs[1]) || ny != mxGetM(prhs[1]))
+			mexErrMsgTxt("When the first two args are arrays, they must have the SAME SIZE.\n");
+		if (!operator[0])
+			mexErrMsgTxt("When the first two args are arrays, MUST specify option -O.\n");
+		have_two_grds = TRUE;
+		if (nlhs == 0) insitu = TRUE;
+	}
+	else if (is_single && operator[0]) {
+		have_fun_to_grd = TRUE;			/* Apply a function to a grid */
+		if (nlhs == 0) insitu = TRUE;
+	}
+	else if (clip_op[0] && nlhs == 0)
+		insitu = TRUE;
+
+	if (nlhs == 0 && !(ADD + MUL) && !insitu) {
+		mexPrintf("GRDUTILS ERROR: Must provide an output.\n");
+		return;
+	}
 
 	nxy = (size_t)nx * ny;
 
@@ -258,8 +324,11 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
 	else {
 		if (!is_single && !is_int16 && !is_uint16 && !is_int8 && !is_uint8)
 			mexErrMsgTxt("GRDUTILS ERROR: Invalid input data type. Valid type are: Single, UInt16, Int16, UInt8 or Int8.\n");
-		if (is_single)
+		if (is_single) {
 			zdata = (float *)mxGetData(prhs[0]);
+			if (have_two_grds)
+				zdata2 = (float *)mxGetData(prhs[1]);
+		}
 		else if (is_uint16)
 			dataU16 = (unsigned short int *)mxGetData(prhs[0]);
 		else if (is_int16)
@@ -290,6 +359,17 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
 	if (is_single) {
 		if (ADD_MUL || MUL || ADD)
 			mul_add((void *)zdata, NULL, fact_x, fact_a, (size_t)nxy, 0);
+		else if (have_two_grds || have_fun_to_grd || clip_op[0]) {		/* grid algebra or clipping */
+			if (!insitu) {
+				plhs[0] = mxCreateNumericArray(mxGetNumberOfDimensions(prhs[0]),
+			  	                               mxGetDimensions(prhs[0]), mxSINGLE_CLASS, mxREAL);
+				array_out = (float *)mxGetData(plhs[0]);
+			}
+			if (have_two_grds || have_fun_to_grd)
+				grid_ops(zdata, zdata2, array_out, operator, nxy);
+			else
+				grid_clip(zdata, array_out, clip_op, clip_val1, clip_val2, nxy);
+		}
 		else {
 			if (do_min_max) {
 OMP_PARF_MINMAX
@@ -506,5 +586,149 @@ OMP_PARF
 		else							/* ADD */
 OMP_PARF
 			for (i = 0; i < np; i++) f4_o[i] = u2_i[i] + fac_a;
+	}
+}
+
+
+void grid_ops(float *z1, float *z2, float *array_out, char *operator, size_t np) {
+	/* Do a ADD, SUB, MUL, DIV operation between the two arrays z1 & z2 and store result in array_out. 
+	   If array_out is NULL, do the operation in situ and store result in z1 */
+
+	long long i;
+
+	if (!strcmp(operator, "ADD")) {
+		if (array_out)
+OMP_PARF
+			for (i = 0; i < np; i++) array_out[i] = z1[i] + z2[i];
+		else			/* In Situ */
+OMP_PARF
+			for (i = 0; i < np; i++) z1[i] += z2[i];
+	}
+	else if (!strcmp(operator, "SUB")) {
+		if (array_out)
+OMP_PARF
+			for (i = 0; i < np; i++) array_out[i] = z1[i] - z2[i];
+		else			/* In Situ */
+OMP_PARF
+			for (i = 0; i < np; i++) z1[i] -= z2[i];
+	}
+	else if (!strcmp(operator, "MUL")) {
+		if (array_out)
+OMP_PARF
+			for (i = 0; i < np; i++) array_out[i] = z1[i] * z2[i];
+		else			/* In Situ */
+OMP_PARF
+			for (i = 0; i < np; i++) z1[i] *= z2[i];
+	}
+	else if (!strcmp(operator, "DIV")) {
+		if (array_out)
+OMP_PARF
+			for (i = 0; i < np; i++) array_out[i] = z1[i] / z2[i];
+		else			/* In Situ */
+OMP_PARF
+			for (i = 0; i < np; i++) z1[i] /= z2[i];
+	}
+	else if (!strcmp(operator, "POW")) {
+		if (array_out)
+OMP_PARF
+			for (i = 0; i < np; i++) array_out[i] = (float)pow(z1[i], z2[i]);
+		else			/* In Situ */
+OMP_PARF
+			for (i = 0; i < np; i++) z1[i] = (float)pow(z1[i], z2[i]);
+	}
+	else if (!strcmp(operator, "EXP")) {
+		if (array_out)
+OMP_PARF
+			for (i = 0; i < np; i++) array_out[i] = (float)exp(z1[i]);
+		else			/* In Situ */
+OMP_PARF
+			for (i = 0; i < np; i++) z1[i] = (float)exp(z1[i]);
+	}
+	else if (!strcmp(operator, "LOG10")) {
+		if (array_out)
+OMP_PARF
+			for (i = 0; i < np; i++) array_out[i] = (float)log10(z1[i]);
+		else			/* In Situ */
+OMP_PARF
+			for (i = 0; i < np; i++) z1[i] = (float)log10(z1[i]);
+	}
+	else if (!strcmp(operator, "LOG")) {
+		if (array_out)
+OMP_PARF
+			for (i = 0; i < np; i++) array_out[i] = (float)log(z1[i]);
+		else			/* In Situ */
+OMP_PARF
+			for (i = 0; i < np; i++) z1[i] = (float)log(z1[i]);
+	}
+	else if (!strcmp(operator, "SIN")) {
+		if (array_out)
+OMP_PARF
+			for (i = 0; i < np; i++) array_out[i] = (float)sin(z1[i]);
+		else			/* In Situ */
+OMP_PARF
+			for (i = 0; i < np; i++) z1[i] = (float)sin(z1[i]);
+	}
+	else if (!strcmp(operator, "COS")) {
+		if (array_out)
+OMP_PARF
+			for (i = 0; i < np; i++) array_out[i] = (float)cos(z1[i]);
+		else			/* In Situ */
+OMP_PARF
+			for (i = 0; i < np; i++) z1[i] = (float)cos(z1[i]);
+	}
+	else if (!strcmp(operator, "SQRT")) {
+		if (array_out)
+OMP_PARF
+			for (i = 0; i < np; i++) array_out[i] = (float)sqrt(z1[i]);
+		else			/* In Situ */
+OMP_PARF
+			for (i = 0; i < np; i++) z1[i] = (float)sqrt(z1[i]);
+	}
+	else if (!strcmp(operator, "TAN")) {
+		if (array_out)
+OMP_PARF
+			for (i = 0; i < np; i++) array_out[i] = (float)tan(z1[i]);
+		else			/* In Situ */
+OMP_PARF
+			for (i = 0; i < np; i++) z1[i] = (float)tan(z1[i]);
+	}
+}
+
+
+void grid_clip(float *z1, float *array_out, char *operator, float val1, float val2, size_t np) {
+	/* Do a  array clipping */
+	long long i;
+
+	if (!strcmp(operator, "<")) {
+		if (array_out)
+OMP_PARF
+			for (i = 0; i < np; i++) array_out[i] = (z1[i] < val1) ? val2 : z1[i];
+		else			/* In Situ */
+OMP_PARF
+			for (i = 0; i < np; i++) if (z1[i] < val1) z1[i] = val2;
+	}
+	else if (!strcmp(operator, "<=")) {
+		if (array_out)
+OMP_PARF
+			for (i = 0; i < np; i++) array_out[i] = (z1[i] <= val1) ? val2 : z1[i];
+		else			/* In Situ */
+OMP_PARF
+			for (i = 0; i < np; i++) if (z1[i] <= val1) z1[i] = val2;
+	}
+	else if (!strcmp(operator, ">")) {
+		if (array_out)
+OMP_PARF
+			for (i = 0; i < np; i++) array_out[i] = (z1[i] > val1) ? val2 : z1[i];
+		else			/* In Situ */
+OMP_PARF
+			for (i = 0; i < np; i++) if (z1[i] > val1) z1[i] = val2;
+	}
+	else if (!strcmp(operator, ">=")) {
+		if (array_out)
+OMP_PARF
+			for (i = 0; i < np; i++) array_out[i] = (z1[i] >= val1) ? val2 : z1[i];
+		else			/* In Situ */
+OMP_PARF
+			for (i = 0; i < np; i++) if (z1[i] >= val1) z1[i] = val2;
 	}
 }
