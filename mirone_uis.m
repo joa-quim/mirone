1d245e10f38dd50b8ce62e01e055cce9a5fbf315
Joaquim Luis <jmfluis@gmail.com>
2021-11-08 23:51:59 +0000

    Time Stamp

--- mirone_uis.m
+++ mirone_uis.m
@@ -765,7 +765,7 @@ uimenu('Parent',h, 'Callback', 'test_bots','Label','Tests - Run some tests')
 %uimenu('Parent',h, 'Callback', 'update_gmt(guidata(gcbo))','Label','Update your GMT5','Sep','on')
 uimenu('Parent',h, 'Callback',['mirone(''FileOpenWebImage_CB'',guidata(gcbo),',...
 	' ''http://www2.clustrmaps.com/stats/maps-clusters/w3.ualg.pt-~jluis-mirone-world.jpg'',''nikles'');'],'Label','See visitors map','Sep','on');
-uimenu('Parent',h, 'Callback','about_box(guidata(gcbo),''Mirone Last modified at 1 Nov 2021'',''3.1'')','Label','About','Sep','on');
+uimenu('Parent',h, 'Callback','about_box(guidata(gcbo),''Mirone Last modified at 8 Nov 2021'',''3.1'')','Label','About','Sep','on');
 
 %% --------------------------- Build HANDLES and finish things here
 	handles = guihandles(H1);
